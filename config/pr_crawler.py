f766aa834410b592e732378546946cdf659cade0
WhiteJaeger <logerk3@gmail.com>
2020-09-09 17:22:12 +0300

    Do not use token to get files from pr

--- config/pr_crawler.py
+++ config/pr_crawler.py
@@ -30,24 +30,23 @@ def download_file(from_url, to_url):
 
 
 def main():
-    # if "TRAVIS_COMMIT" not in os.environ:
-    #     print('Need proper environment variables')
-    #     sys.exit(1)
+    if 'TRAVIS_COMMIT' not in os.environ:
+        print('Need proper environment variables')
+        sys.exit(1)
 
     current_commit_hash = os.environ.get('TRAVIS_COMMIT', 'None')
 
-    token = ''
     base_url = 'https://api.github.com/repos/fipl-hse/2020-2-level-labs'
 
-    content = get_by_url(url='{}/pulls{}?state=all'.format(base_url, token))
+    content = get_by_url(url='{}/pulls?state=all'.format(base_url))
     for pull_req in content:
         pr_id = pull_req["number"]
         files_from_pr = get_by_url(
-            url='{}/pulls/{}/files{}?state=all'.format(base_url, pr_id, token))
+            url='{}/pulls/{}/files?state=all'.format(base_url, pr_id))
         for pr_file in files_from_pr:
-            target_url = pr_file['raw_url']
-            file_name = '/'.join(target_url.split('/')[-2:])
-            source_hash = target_url.split('/')[-3]
+            target_file_url = pr_file['raw_url']
+            file_name = '/'.join(target_file_url.split('/')[-2:])
+            source_hash = target_file_url.split('/')[-3]
 
             if source_hash == current_commit_hash or file_name.endswith('_test.py'):
                 print('Ignoring file: {}'.format(file_name))
@@ -59,19 +58,8 @@ def main():
 
             os.makedirs(os.path.dirname(to_url), exist_ok=True)
 
-            download_file(from_url=target_url, to_url=to_url)
+            download_file(from_url=target_file_url, to_url=to_url)
 
 
 if __name__ == '__main__':
-    content = get_by_url('https://api.github.com/repos/fipl-hse/2020-2-level-labs/pulls/2/files')
-    first_file_url = content[0]['raw_url']
-    http = urllib3.PoolManager()
-    request = http.request('GET', first_file_url, preload_content=False)
-    os.makedirs('tmp/lab')
-    with open('tmp/lab/test.txt', 'wb') as out:
-        while True:
-            data = request.read(100)
-            if not data:
-                break
-            out.write(data)
-    request.release_conn()
+    main()
