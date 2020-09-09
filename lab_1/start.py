f0cd913201580094618947f48f6dba507dd5d45e
Alexander Demidovskij <alexander.demidovskij@intel.com>
2020-09-09 19:53:12 +0300

    added start template

---
+++ lab_1/start.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+  text = read_text('a.txt')
+  text = 'Mary...'
+  
+  # here goes your logic: calling methods from concordance.py
+  
+  concordance_realize = None
+  
+  ## DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
+  assert concordance_realize == [(), ()], 'Concordance not working'
+  
+  ### Test: a) assert concordance_realize is in file
+  ### Test: b) call start.py, expect it does not throw Error
