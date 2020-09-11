8b6a239fb01c3e5b660d394da9693414c7eae58b
WhiteJaeger <logerk3@gmail.com>
2020-09-11 19:15:10 +0300

    Export var

--- config/run-lint.sh
+++ config/run-lint.sh
@@ -1,17 +1,17 @@
+export PYTHONPATH=${PYTHONPATH}:{pwd}
+
 echo 'Running Lint Check'
 MINIMUM_LEVEL=9
 FAILED=0
 
 lint_output=$(pylint ./**/*.py)
-echo "$lint_output"
 score=$(echo "$lint_output" | grep -oP "Your code has been rated at \d+.\d+" | grep -oP "\d+")
-echo "$score"
 readarray -t score <<<"$score"
-echo "$score"
 
 if [[ $score -lt $MINIMUM_LEVEL ]]; then
   echo "Lint Check FAILED!"
   FAILED=1
+  echo ''
   echo 'Lint Check Output:'
   echo "$lint_output"
 fi
@@ -21,6 +21,7 @@ if [[ $FAILED -ne 0 ]]; then
 fi
 
 if [[ $score -ne 10 ]]; then
+  echo ''
   echo 'Lint check passed but there are things to improve:'
   echo "$lint_output"
   exit 0
