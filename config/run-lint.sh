a5d6e24077933bfa363a3742aad0c657e24b271a
WhiteJaeger <logerk3@gmail.com>
2020-09-11 19:03:28 +0300

    rm quots

--- config/run-lint.sh
+++ config/run-lint.sh
@@ -3,12 +3,13 @@ MINIMUM_LEVEL=7
 FAILED=0
 
 lint_output=$(pylint ./**/*.py)
+echo "$lint_output"
 score=$(echo "$lint_output" | grep -oP "Your code has been rated at \d+.\d+" | grep -oP "\d+")
 echo "$score"
 score=$(echo "$score" | awk '{ print $1 }')
 echo "$score"
 
-if [[ "$score" -lt "$MINIMUM_LEVEL" ]]; then
+if [[ $score -lt $MINIMUM_LEVEL ]]; then
   echo "Lint Check FAILED!"
   FAILED=1
   echo 'Lint Check Output:'
@@ -19,7 +20,7 @@ if [[ $FAILED -ne 0 ]]; then
   exit 1
 fi
 
-if [[ "$score" -ne 10 ]]; then
+if [[ $score -ne 10 ]]; then
   echo 'Lint check passed but there are things to improve:'
   echo "$lint_output"
   exit 0
