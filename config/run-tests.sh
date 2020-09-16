69594d41ae05e0f52380c4f2aa69d68a5b16e0af
WhiteJaeger <logerk3@gmail.com>
2020-09-16 17:39:47 +0300

    Try with actual tests

--- config/run-tests.sh
+++ config/run-tests.sh
@@ -10,17 +10,16 @@ for lab in $LABS; do
 	TARGET_SCORE=$(cat lab_"${lab}"/target_score.txt)
 	TARGET_TESTS=lab_"${lab}"/target_tests_"${TARGET_SCORE}".txt
 
-	cat "$TARGET_TESTS" | while read line || [[ -n $line ]]
+	cat "$TARGET_TESTS" | while read test_file_name || [[ -n $test_file_name ]]
 	do
-	  echo $line
+	  echo "Running tests from $test_file_name"
+	  if ! python3 -m unittest lab_"${lab}"/$test_file_name;  then
+    	WAS_FAILED=1
+	  fi
 	done
-
-#	if ! python3 -m unittest discover -p *_test.py -s lab_"${lab}";  then
-#    	WAS_FAILED=1
-#	fi
 done
 
 if [[ $WAS_FAILED -eq 1 ]]; then
-	echo Tests failed
+	echo "Tests failed."
 	exit 1
 fi
---
+++ lab_1/sum_test.py
@@ -0,0 +1,18 @@
+# pylint: skip-file
+"""
+Checks the ideal
+"""
+
+import unittest
+
+
+class IdealTest(unittest.TestCase):
+    """
+    Tests tokenize function
+    """
+
+    def test_ideal(self):
+        """
+        Ideal  scenario
+        """
+        self.assertEqual(2+2, 4)
