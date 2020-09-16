a55d97444c820766e506c67ae1003ba4f64d6532
WhiteJaeger <logerk3@gmail.com>
2020-09-16 20:00:30 +0300

    Split text preprocessing tests

--- config/run-lint.sh
+++ config/run-lint.sh
@@ -8,7 +8,6 @@ for lab in $LABS; do
 	echo "Running lint for lab #${lab}"
 
   TARGET_SCORE=$(cat lab_"${lab}"/target_score.txt)
-  echo $TARGET_SCORE
 
 	lint_output=$(pylint lab_"${lab}")
   python3 config/lint_level.py --lint-output "$lint_output" --lint-level $TARGET_SCORE
