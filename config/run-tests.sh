c0220e19b1e75d8e2271f72f958234f262f8024d
WhiteJaeger <logerk3@gmail.com>
2020-09-16 17:51:04 +0300

    Use pythonpath and non-local failing var

--- config/run-tests.sh
+++ config/run-tests.sh
@@ -1,3 +1,4 @@
+export PYTHONPATH="$(pwd):lab_1:$(pwd):lab_2:$(pwd):lab_3:$(pwd):lab_4:$(pwd):${PYTHONPATH}"
 echo Running tests
 WAS_FAILED=0
 LABS=$(cat config/labs.txt)
@@ -10,13 +11,13 @@ for lab in $LABS; do
 	TARGET_SCORE=$(cat lab_"${lab}"/target_score.txt)
 	TARGET_TESTS=lab_"${lab}"/target_tests_"${TARGET_SCORE}".txt
 
-	cat "$TARGET_TESTS" | while read test_file_name || [[ -n $test_file_name ]]
+	while read test_file_name || [[ -n $test_file_name ]]
 	do
 	  echo "Running tests from $test_file_name"
 	  if ! python3 -m unittest lab_"${lab}"/$test_file_name;  then
     	WAS_FAILED=1
 	  fi
-	done
+	done <<< "$(cat "$TARGET_TESTS")"
 done
 
 if [[ $WAS_FAILED -eq 1 ]]; then
