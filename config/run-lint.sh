e203c59b1fcfaa88ab4b54cd8d10f476453ecf2d
radian2pk <uraev.dima@yandex.ru>
2020-09-17 08:25:08 +0300

    repaired lint, now checking

--- config/run-lint.sh
+++ config/run-lint.sh
@@ -1,3 +1,17 @@
-PYTHONPATH="$(pwd)/lab_1:$(pwd)/lab_2:$(pwd)/lab_3:$(pwd)/lab_4:$(pwd):${PYTHONPATH}"
 
-python3 -m pylint ./**/*.py
+
+echo Running lint
+WAS_FAILED=0
+
+PYTHONPATH="$(pwd)/lab_1:$(pwd)/lab_2:$(pwd)/lab_3:$(pwd)/lab_4:$(pwd):${PYTHONPATH}"
+export PYTHONPATH
+
+if ! python3 -m pylint ./**/*.py; then
+  WAS_FAILED=1
+fi
+done
+
+if [[ $WAS_FAILED -eq 1 ]]; then
+	echo Lint failed
+	exit 1
+fi
