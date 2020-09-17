8faa6eb9790fb9cd5bd453062335e927f281ee44
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2020-09-17 19:29:53 +0300

    ...testing...

--- lab_1/main.py
+++ lab_1/main.py
@@ -9,7 +9,7 @@ def tokenize(text: str) -> list:
     Splits sentences into tokens, converts the tokens into lowercase, removes punctuation
     :param text: the initial text
     :return: a list of lowercased tokens without punctuation
-    e111. text = 'The weather is sunny, the man is happy.'
+    E.G.. text = 'The weather is sunny, the man is happy.'
     --> ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
     """
     pass
