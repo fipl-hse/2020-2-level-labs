6d113dc23a27407fcb4d3bf2e2cd07ffe1d81d18
dmitry-uraev <uraev.dima@yandex.ru>
2020-09-23 20:17:41 +0300

    renamed some files, added os  paths in start.py, checking start.py call test

--- config/check_start_content.py
+++ config/check_start_content.py
@@ -1,17 +1,15 @@
-# pylint: skip-file
-
 import argparse
 import re
 import sys
 
 
 def check_assert_is_in_file(content: str):
-    print(content)
+    # print(content)
     expected = 'assert RESULT'
-    actual = re.findall(expected, content)
-    if expected == actual or expected in content:
-        return 0
-    return 1
+    # actual = re.findall(expected, content)
+    if expected in content:
+        return True
+    return False
 
 
 if __name__ == "__main__":
@@ -20,11 +18,12 @@ if __name__ == "__main__":
     args: argparse.Namespace = parser.parse_args()
 
     result = check_assert_is_in_file(args.start_py_content)
-    if result == 1:
-        print('Make sure you made assert RESULT in start.py file')
-        sys.exit(result)
-    print('Passed')
-    sys.exit(result)
+    if result:
+        print('Passed')
+        sys.exit(0)
+    print('Make sure you made assert RESULT in start.py file')
+    sys.exit(1)
+
 
 # Test: a) assert concordance_realize is in file
 # Test: b) call start.py, expect it does not throw Error
--- lab_1/start.py
+++ lab_1/start.py
@@ -3,10 +3,11 @@ Concordance implementation starter
 """
 
 from main import read_from_file
+import os
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
-    # data = read_from_file('data.txt')
+    data = read_from_file(os.getcwd()+'/data.txt')
     stop_words = []
 
     #  here goes your logic: calling methods from concordance.py
