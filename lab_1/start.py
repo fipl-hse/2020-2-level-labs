e546273c76f2db1e72b976034beff52bf9c73f3a
dmitry-uraev <uraev.dima@yandex.ru>
2020-09-23 20:57:04 +0300

    added another solution to path problem

--- lab_1/start.py
+++ lab_1/start.py
@@ -3,11 +3,12 @@ Concordance implementation starter
 """
 
 from main import read_from_file
+from pathlib import Path
 import os
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
-    data = read_from_file(os.getcwd()+'/./data.txt')
+    data = read_from_file(next(Path(os.getcwd()).rglob('data.txt')))
     stop_words = []
 
     #  here goes your logic: calling methods from concordance.py
