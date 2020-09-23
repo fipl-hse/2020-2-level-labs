1d58df074740fe9ce6cbec30fb1fa2384ed32d9b
dmitry-uraev <uraev.dima@yandex.ru>
2020-09-23 21:03:48 +0300

    check bad case

--- lab_1/start.py
+++ lab_1/start.py
@@ -15,4 +15,4 @@ if __name__ == '__main__':
 
     RESULT = None
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    # assert RESULT == [(), ()], 'Concordance not working'
+    RESULT == [(), ()], 'Concordance not working'
