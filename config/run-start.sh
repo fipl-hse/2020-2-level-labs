550d0f1d7b6b24086f2a132b6053f9e8e2005f53
dmitry-uraev <uraev.dima@yandex.ru>
2020-09-23 19:01:38 +0300

    added another test to check start.py output

--- config/run-start.sh
+++ config/run-start.sh
@@ -2,9 +2,20 @@ export PYTHONPATH="$(pwd)/lab_1:$(pwd)/lab_2:$(pwd)/lab_3:$(pwd)/lab_4:$(pwd):${
 echo "Running start.py checks..."
 
 LABS=$(cat config/labs.txt)
+WAS_FAILED=0
 
 for lab in $LABS; do
 	echo "Running start.py checks for lab #${lab}"
+
+	if ! python3 lab_"${lab}"/start.py;  then
+    	WAS_FAILED=1
+	  fi
+	  
+	if [[ $WAS_FAILED -eq 1 ]]; then
+	echo "start.py fails while running"
+	echo "Check for start.py file for lab #${lab} failed."
+	exit 1
+  fi
 
   START_PY_FILE=$(cat lab_"${lab}"/start.py)
   python3 config/start_py_test.py --start_py_content "$START_PY_FILE"
