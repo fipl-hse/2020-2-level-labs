c5122e5f52f3ee9c380928853a6f7f60dc233031
dmitry-uraev <uraev.dima@yandex.ru>
2020-09-23 20:37:55 +0300

    added another path variant

--- lab_1/start.py
+++ lab_1/start.py
@@ -7,7 +7,7 @@ import os
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
-    data = read_from_file(os.getcwd()+'./data.txt')
+    data = read_from_file(os.getcwd()+'/./data.txt')
     stop_words = []
 
     #  here goes your logic: calling methods from concordance.py
