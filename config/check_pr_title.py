45981325c538cdbb88750429f9be56e08c7dc22f
WhiteJaeger <logerk3@gmail.com>
2020-09-24 11:58:27 +0300

    rm

--- config/check_pr_title.py
+++ config/check_pr_title.py
@@ -13,7 +13,6 @@ def is_matching_name(pr_name: str) -> bool:
     pr_name = re.search(template, pr_name)
     if not pr_name:
         print('Your Pull Request title does not confirm to the template.')
-        print(f'\nYour pull request: {pr_name}')
         print('Template: Laboratory work #1, Name Surname - 19FPL1\n')
         return False
     print('Your Pull Request name confirm to provided template!')
@@ -24,10 +23,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Checks that PR name is done using the template')
     parser.add_argument('--pr-name', type=str, help='Current PR name')
     args: argparse.Namespace = parser.parse_args()
-    print(args.pr_name)
 
     pr_name_to_check = convert_raw_pr_name(args.pr_name)
-    print(pr_name_to_check)
     if not is_matching_name(pr_name_to_check):
         sys.exit(1)
     sys.exit(0)
