fec9654365fcbd514bdce8b728b3e287d683787c
alenashuvar <alenashuvar@users.noreply.github.com>
2020-09-24 22:57:41 +0300

    'Lab_1'

--- lab_1/main.py
+++ lab_1/main.py
@@ -16,7 +16,7 @@ def tokenize(text: str) -> list:
     if type(text) != str:
         return []
     text = text.lower()
-    text = re.sub(r'[^A-Za-z0-9\s`]', '', text)
+    text = re.sub(r'[^A-Za-z0-9\s]', '', text)
     return text.split()
 
 
