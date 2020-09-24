b4180faadca95aab84fa53ae14b7a315ca704ffa
WhiteJaeger <logerk3@gmail.com>
2020-09-24 18:15:36 +0300

    checkout main

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,54 +1,128 @@
-# pylint: skip-file
 """
-Checks the first lab stop words removal functions
+Lab 1
+A concordance extraction
 """
 
-import unittest
-from main import remove_stop_words
 
-
-class RemoveStopWordsTest(unittest.TestCase):
+def tokenize(text: str) -> list:
     """
-    Tests remove stop words function
+    Splits sentences into tokens, converts the tokens into lowercase, removes punctuation
+    :param text: the initial text
+    :return: a list of lowercased tokens without punctuation
+    e.g. text = 'The weather is sunny, the man is happy.'
+    --> ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
     """
-    STOP_WORDS = ['the', 'a', 'is']
+    pass
 
-    def test_remove_stop_words_ideal(self):
-        """
-        Ideal removing stop words scenario
-        """
-        expected = ['weather', 'sunny', 'man', 'happy']
-        actual = remove_stop_words(['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy'],
-                                   RemoveStopWordsTest.STOP_WORDS)
-        self.assertEqual(expected, actual)
 
-    def test_remove_stop_words_bad_input(self):
-        """
-        Remove stop words bad input scenario
-        """
-        bad_inputs_first = [{}, (), None, 9, 9.34, True]
-        bad_inputs_second = [{}, (), None, 9, 9.34, True]
-        expected = []
-        for bad_input in range(0, 5):
-            actual_1 = remove_stop_words(bad_inputs_first[bad_input], bad_inputs_second[bad_input])
-            actual_2 = remove_stop_words([], bad_inputs_second[bad_input])
-            actual_3 = remove_stop_words(bad_inputs_first[bad_input], [])
-            self.assertEqual(expected, actual_1)
-            self.assertEqual(expected, actual_2)
-            self.assertEqual(expected, actual_3)
+def remove_stop_words(tokens: list, stop_words: list) -> list:
+    """
+    Removes stop words
+    :param tokens: a list of tokens
+    :param stop_words: a list of stop words
+    :return: a list of tokens without stop words
+    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
+    stop_words = ['the', 'is']
+    --> ['weather', 'sunny', 'man', 'happy']
+    """
+    pass
 
-    def test_remove_stop_words_no_stop_words(self):
-        """
-        Remove stop words without stop words scenario
-        """
-        expected = ['token1', 'token2']
-        actual = remove_stop_words(['token1', 'token2'], [])
-        self.assertEqual(expected, actual)
 
-    def test_remove_stop_words_all_words(self):
-        """
-        Remove stop words as the whole text scenario
-        """
-        expected = []
-        actual = remove_stop_words(['the', 'a', 'is'], RemoveStopWordsTest.STOP_WORDS)
-        self.assertEqual(expected, actual)
+def calculate_frequencies(tokens: list) -> dict:
+    """
+    Calculates frequencies of given tokens
+    :param tokens: a list of tokens without stop words
+    :return: a dictionary with frequencies
+    e.g. tokens = ['weather', 'sunny', 'man', 'happy']
+    --> {'weather': 1, 'sunny': 1, 'man': 1, 'happy': 1}
+    """
+    pass
+
+
+def get_top_n_words(freq_dict: dict, top_n: int) -> list:
+    """
+    Returns the most common words
+    :param freq_dict: a dictionary with frequencies
+    :param top_n: a number of the most common words to return
+    :return: a list of the most common words
+    e.g. tokens = ['weather', 'sunny', 'man', 'happy', 'and', 'dog', 'happy']
+    top_n = 1
+    --> ['happy']
+    """
+    pass
+
+
+def get_concordance(tokens: list, word: str, left_context_size: int, right_context_size: int) -> list:
+    """
+    Gets a concordance of a word
+    A concordance is a listing of each occurrence of a word in a text,
+    presented with the words surrounding it
+    :param tokens: a list of tokens
+    :param word: a word-base for a concordance
+    :param left_context_size: the number of words in the left context
+    :param right_context_size: the number of words in the right context
+    :return: a concordance
+    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
+                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad']
+    word = 'happy'
+    left_context_size = 2
+    right_context_size = 3
+    --> [['man', 'is', 'happy', 'the', 'dog', 'is'], ['dog', 'is', 'happy', 'but', 'the', 'cat']]
+    """
+    pass
+
+
+def get_adjacent_words(tokens: list, word: str, left_n: int, right_n: int) -> list:
+    """
+    Gets adjacent words from the left and right context
+    :param tokens: a list of tokens
+    :param word: a word-base for the search
+    :param left_n: the distance between a word and an adjacent one in the left context
+    :param right_n: the distance between a word and an adjacent one in the right context
+    :return: a list of adjacent words
+    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
+                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad']
+    word = 'happy'
+    left_n = 2
+    right_n = 3
+    --> [['man', 'is'], ['dog, 'cat']]
+    """
+    pass
+
+
+def read_from_file(path_to_file: str) -> str:
+    """
+    Opens the file and reads its content
+    :return: the initial text in string format
+    """
+    with open(path_to_file, 'r', encoding='utf-8') as fs:
+        data = fs.read()
+
+    return data
+
+
+def write_to_file(path_to_file: str, content: list):
+    """
+    Writes the result in a file
+    """
+    pass
+
+
+def sort_concordance(tokens: list, word: str, left_context_size: int, right_context_size: int, left_sort: bool) -> list:
+    """
+    Gets a concordance of a word and sorts it by either left or right context
+    :param tokens: a list of tokens
+    :param word: a word-base for a concordance
+    :param left_context_size: the number of words in the left context
+    :param right_context_size: the number of words in the right context
+    :param left_sort: if True, sort by the left context, False – by the right context
+    :return: a concordance
+    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
+                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad']
+    word = 'happy'
+    left_context_size = 2
+    right_context_size = 3
+    left_sort = True
+    --> [['dog', 'is', 'happy', 'but', 'the', 'cat'], ['man', 'is', 'happy', 'the', 'dog', 'is']]
+    """
+    pass
