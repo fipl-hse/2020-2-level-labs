f5d3e24f12f88bf953efa05897ceb9150f8caa14
WhiteJaeger <logerk3@gmail.com>
2020-09-24 18:09:36 +0300

    skip current PR

--- config/pr_crawler.py
+++ config/pr_crawler.py
@@ -53,7 +53,7 @@ def download_file(from_url: str, to_url: str) -> None:
     req.release_conn()
 
 
-def main(lab_n: int):
+def main(lab_n: int, current_pr_id: int):
     if 'TRAVIS_COMMIT' not in os.environ:
         print('Need proper environment variables')
         sys.exit(1)
@@ -65,6 +65,8 @@ def main(lab_n: int):
     content = get_by_url(url='{}/pulls?state=all'.format(base_url))
     for pull_req in content:
         pr_id = pull_req['number']
+        if current_pr_id == pr_id:  # Skip PR so that main.py from current PR is not downloaded
+            continue
         files_from_pr = get_by_url(
             url='{}/pulls/{}/files'.format(base_url, pr_id))
         for pr_file in files_from_pr:
@@ -72,11 +74,9 @@ def main(lab_n: int):
             file_name = pr_file['filename']
             if file_name != f'lab_{lab_n}/main.py':  # Skip all files except main.py for the current lab
                 continue
+
             source_hash = target_file_url.split('/')[-3]
 
-            if source_hash == current_commit_hash:
-                print('Ignoring file: {}'.format(file_name))
-                continue
             file_base_name = file_name.split('/')[1]  # expected: main.py
             to_url = os.path.join('tmp',
                                   '{}_{}.{}'.format(file_base_name.split('.')[0],
@@ -91,5 +91,10 @@ def main(lab_n: int):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get all changed files from pull requests')
     parser.add_argument('--lab', type=int, help='Lab to check')
+    parser.add_argument('--current-pr', type=int, help='Current PR id')
     args: argparse.Namespace = parser.parse_args()
-    main(args.lab)
+    print(args.current_pr)
+    print(args.current_pr)
+    print(args.current_pr)
+    print(args.current_pr)
+    main(args.lab, args.current_pr)
