6065df83f0c01f63663df1e3efd78dafe824f103
dmitry-uraev <uraev.dima@yandex.ru>
2020-09-24 19:57:21 +0300

    check pathfinding

--- lab_1/start.py
+++ lab_1/start.py
@@ -7,7 +7,8 @@ import os
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
-    data = read_from_file(os.path.dirname(os.path.abspath(__file__)) + '/data.txt')
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data = read_from_file(os.path.join(current_dir, 'data.txt'))
     stop_words = []
 
     #  here goes your logic: calling methods from concordance.py
