61e744c8bb7244f07b6c43bfc4cc5d18953f231f
katerinamerkulova <merkulova1999@mail.ru>
2020-09-25 17:58:29 +0300

    finally found a bag and fixed it

--- lab_1/start.py
+++ lab_1/start.py
@@ -28,6 +28,6 @@ if __name__ == '__main__':
     print('sort_concordance', sorted_concordance[:5])
     main.write_to_file('', sorted_concordance)
     check = (all(isinstance(s, list) for s in sorted_concordance))
-    RESULT = (isinstance(sorted_concordance, list) and check
+    RESULT = isinstance(sorted_concordance, list) and check
     #  DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Concordance not working'
