c5c736a5b9200265686763e36e317f1b103a2b72
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2020-09-25 17:32:43 +0300

    fixing start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data = main.read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = main.read_from_file('lab_1\\stop_words.txt').split('\n')
+    stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words.txt')).split('\n')
 
     #  here goes your logic: calling methods from concordance.py
     tokenized_data = main.tokenize(data)
