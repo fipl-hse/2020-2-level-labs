aabe80c30b8eaadd33b1de7794aec8a3b213773a
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2020-09-25 17:23:07 +0300

    fix linter in start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -10,21 +10,21 @@ if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data = main.read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = main.read_from_file('stop_words.txt').split('\n')  # Add lab_1\
+    stop_words = main.read_from_file('lab_1\stop_words.txt').split('\n')
 
     #  here goes your logic: calling methods from concordance.py
     tokenized_data = main.tokenize(data)
     clean_data = main.remove_stop_words(tokenized_data, stop_words)
 
-    """ Let`s get top 7 words """
+    # Let`s get top 7 words """
     top_7 = main.get_top_n_words(main.calculate_frequencies(clean_data), 7)
     print(top_7)
 
-    """ Now let`s finally get a concordance for word 'people' """
+    # Now let`s finally get a concordance for word 'people' """
     concordance = main.get_concordance(clean_data, 'people', 3, 1)
     print(concordance[:5])
 
-    """ Let`s see 1st on the left and 2nd on the right words around our target word """
+    # Let`s see 1st on the left and 2nd on the right words around our target word """
     closest_words = main.get_adjacent_words(clean_data, 'people', 1, 2)
     print(closest_words[:5])
 
