ef8546925546518c180c73d20e431dc90bb4c7f3
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2020-09-26 20:00:14 +0300

    change the target word and other small changes

--- lab_1/start.py
+++ lab_1/start.py
@@ -16,23 +16,23 @@ if __name__ == '__main__':
     tokenized_data = main.tokenize(data)
     clean_data = main.remove_stop_words(tokenized_data, stop_words)
 
-    top_13 = main.get_top_n_words(main.calculate_frequencies(clean_data), 14)
-    print(top_13[-1])
+    top_n = main.get_top_n_words(main.calculate_frequencies(clean_data), 13)
+    print(top_n[-1])
 
-    closest_words = main.get_adjacent_words(clean_data, top_13[-1], 3, 2)
+    closest_words = main.get_adjacent_words(clean_data, top_n[-1], 3, 2)
     if len(closest_words) > 0:
         print(f"Third words from the left and second words from the right for "
-              f"the word {top_13[-1]} (first 5 cases) are")
+              f"the word {top_n[-1]} (first 5 cases) are")
         for adjacent_words in closest_words[:5]:
             print('\t', adjacent_words)
 
-    sorted_concordance_left = main.sort_concordance(clean_data, top_13[-1], 2, 2, True)
+    sorted_concordance_left = main.sort_concordance(clean_data, top_n[-1], 2, 2, True)
     if len(sorted_concordance_left) > 0:
         print('Concordance sorted by the first left word (first 5 cases):')
         for concordance in sorted_concordance_left[:5]:
             print('\t', concordance)
 
-    sorted_concordance_right = main.sort_concordance(clean_data, top_13[-1], 2, 2, False)
+    sorted_concordance_right = main.sort_concordance(clean_data, top_n[-1], 2, 2, False)
     if len(sorted_concordance_right) > 0:
         print('Concordance sorted by the first right word (first 5 cases):')
         for concordance in sorted_concordance_right[:5]:
