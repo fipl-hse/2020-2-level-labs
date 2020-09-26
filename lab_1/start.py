a055532364b25eeaf64c6c3154c0d14498a3e2e7
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-09-26 21:02:40 +0300

    Updated logic
    Have not done the whole logic, have question about the interection. Should I create some logic about it or what.

--- lab_1/start.py
+++ lab_1/start.py
@@ -2,15 +2,28 @@
 Concordance implementation starter
 """
 
+from main import *
+"""
+Concordance implementation starter
+"""
+
 from main import read_from_file
+import os
+
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
-    data = read_from_file('data.txt')
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data = read_from_file(os.path.join(current_dir, 'data.txt'))
     stop_words = []
 
     #  here goes your logic: calling methods from concordance.py
+    tokens = tokenize(data) #токенезирование
+    stop_words = get_stop_words()
+    #токены без стоп слов
+    tokens_clean = remove_stop_words(tokens, stop_words)
+    #частоты токенов
+    freq_dict = calculate_frequencies(tokens_clean)
 
-    RESULT = None
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT == [(), ()], 'Concordance not working'
