6b7ac7904cccb3618f0ee0b0cb75858d69956bd1
egorova-ai19-2 <nastyaegorovasunflower@gmail.com>
2020-09-28 23:03:22 +0300

    changes in start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data = main.read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words')).split()
     tokens = main.tokenize(data)
 
     sorted_concordance = main.sort_concordance(tokens, 'happy', 7, 3, True)
