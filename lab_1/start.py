563acacf5feb160932018b0208226a864078e91b
egorova-ai19-2 <nastyaegorovasunflower@gmail.com>
2020-09-28 22:49:00 +0300

    modified start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -3,16 +3,19 @@ Concordance implementation starter
 """
 
 import os
-from main import read_from_file
+import main
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    data = read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = []
+    data = main.read_from_file(os.path.join(current_dir, 'data.txt'))
+    stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words')).split()
+    tokens = main.tokenize(data)
+
+    sorted_concordance = main.sort_concordance(tokens, 'happy', 7, 3, True)
 
     #  here goes your logic: calling methods from concordance.py
 
-    RESULT = None
+    RESULT = sorted_concordance
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT == [(), ()], 'Concordance not working'
