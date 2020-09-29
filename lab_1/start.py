fa3caa382d3d51d74d94b721d653238f6f9632d7
mgaranina <mashkaeddy6784@gmail.com>
2020-09-29 20:20:03 +0300

    changes in start.py (doesn't work properly yet)

--- lab_1/start.py
+++ lab_1/start.py
@@ -2,19 +2,40 @@
 Concordance implementation starter
 """
 
-# some changes there
 
-from main import read_from_file
+import main
 import os
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    data = read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = []
+    data = main.read_from_file(os.path.join(current_dir, 'data.txt'))
+    stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words.txt')).split()
 
     #  here goes your logic: calling methods from concordance.py
+    tokens = main.tokenize(data)
+    print('tokens:', tokens[:10], '...')
 
-    RESULT = None
+    tokens = main.remove_stop_words(tokens, stop_words)
+    print('tokens without stop words:', tokens[:10], '...')
+
+    frequency_dictionary = main.calculate_frequencies(tokens)
+    print('frequency of "time":', frequency_dictionary['time'])
+
+    top_10_words = main.get_top_n_words(frequency_dictionary, 10)
+    print('the most frequent 10 words:', top_10_words)
+
+    concordance = main.get_concordance(tokens, 'moon', 2, 3)
+    print('contexts for "moon":', concordance[:3], '...')
+
+    adjacent_words = main.get_adjacent_words(tokens, 'moon', 2, 3)
+    print('adjacent words for "moon":', adjacent_words [:3], '...')
+
+    sorted_concordance = main.sort_concordance('tokens', 'moon', 2, 3, False)
+    print('sorted concordance for "moon":', sorted_concordance[:3], '...')
+
+    main.write_to_file('report.txt', sorted_concordance)
+
+    RESULT = sorted_concordance
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT == [(), ()], 'Concordance not working'
