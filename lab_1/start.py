6bf42164c3c50de90aa00008e2f187c5db05ade0
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-09-29 17:14:09 +0300

    Update start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -18,12 +18,18 @@ if __name__ == '__main__':
     stop_words = []
 
     #  here goes your logic: calling methods from concordance.py
-    tokens = tokenize(data) #токенезирование
+    tokens = tokenize(data) # токенезирование
     stop_words = get_stop_words()
-    #токены без стоп слов
+    # токены без стоп слов
     tokens_clean = remove_stop_words(tokens, stop_words)
-    #частоты токенов
+    # частоты токенов
     freq_dict = calculate_frequencies(tokens_clean)
+    # слово для поиска
+    word = input()
+    left_size, right_size = int(input()), int(input())
 
+    result_normal_concordance = get_concordance(tokens_clean, word, left_size, right_size)
+    result_adjacent_concordance = get_adjacent_words(tokens, word, left_size, right_size)
+    result_sorted_concordance = sort_concordance(tokens, word, left_size, right_size, True)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT == [(), ()], 'Concordance not working'
