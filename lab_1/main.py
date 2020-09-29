5d543c02e4b8e9468021771ceb5ba12eedcabc9d
Alla Chudinova <typical.eurofan@gmail.com>
2020-09-29 21:45:35 +0300

    returned if smth is True statement, as it is required

--- lab_1/main.py
+++ lab_1/main.py
@@ -111,7 +111,7 @@ def get_concordance(tokens: list, word: str, left_context_size: int, right_conte
     if not isinstance(tokens, list) or not isinstance(word, str)\
             or None in tokens:
         return []
-    if left_context_size or right_context_size\
+    if left_context_size is True or right_context_size is True\
             or not isinstance(left_context_size, int) or not isinstance(right_context_size, int):
         return []
 
