c8eb230ac20503a2671e0a56d9c0ae06ebeb1ee2
katyanemzer <65305336+katyanemzer@users.noreply.github.com>
2020-09-30 23:41:57 +0300

    mod

--- lab_1/main.py
+++ lab_1/main.py
@@ -114,8 +114,7 @@ def get_concordance(tokens: list, word: str, left_context_size: int, right_conte
                 elif right_context_size > 0:
                     concordance.append(tokens[word_index:word_index + right_context_size + 1])
         return concordance
-    else:
-        return []
+    return []
 
 def get_adjacent_words(tokens: list, word: str, left_n: int, right_n: int) -> list:
     """
