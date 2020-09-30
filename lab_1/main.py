a815049cba95ea8c34ed2596bc4bd83991c4db13
Strrakhova Darya <dastrakhova@edu.hse.ru>
2020-09-30 12:40:17 +0300

    made 5 tasks

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,9 +2,24 @@
 Lab 1
 A concordance extraction
 """
+import re
 
 
-def tokenize(text: str) -> list:
+def read_from_file(path_to_file: str) -> str:
+    """
+    Opens the file and reads its content
+    :return: the initial text in string format
+    """
+    with open(path_to_file, 'r', encoding='utf-8') as fs:
+        data = fs.read()
+
+    return data
+
+
+text = read_from_file('data.txt')
+
+
+def tokenize(text:str) -> list:
     """
     Splits sentences into tokens, converts the tokens into lowercase, removes punctuation
     :param text: the initial text
@@ -12,9 +27,19 @@ def tokenize(text: str) -> list:
     e.g. text = 'The weather is sunny, the man is happy.'
     --> ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
     """
-    text = text.split()
-    return text
+    pass
+    if not isinstance(text, str):  # check type (принадлежность)
+        tokens = []
+        return tokens
+    reg = re.compile('[^a-zA-Z \n`]')
+    clean_text = reg.sub('', text)
+    clean_text = clean_text.lower()
+    tokens = clean_text.split()
+    return tokens
 
+
+stop_words = read_from_file('stop_words.txt')
+stop_words = stop_words.split()
 
 def remove_stop_words(tokens: list, stop_words: list) -> list:
     """
@@ -27,6 +52,19 @@ def remove_stop_words(tokens: list, stop_words: list) -> list:
     --> ['weather', 'sunny', 'man', 'happy']
     """
     pass
+    if not isinstance(tokens, list) or not tokens or not isinstance(stop_words, list):  #check for emptiness
+        return []
+    if (isinstance(tokens, list) and tokens) and not (isinstance(stop_words, list) and stop_words):
+        return tokens
+
+    copy_text = tokens[:]   #copy list
+    for words in tokens:
+        if words in stop_words:
+            copy_text.remove(words)
+
+    return copy_text
+
+
 
 
 def calculate_frequencies(tokens: list) -> dict:
@@ -37,7 +75,15 @@ def calculate_frequencies(tokens: list) -> dict:
     e.g. tokens = ['weather', 'sunny', 'man', 'happy']
     --> {'weather': 1, 'sunny': 1, 'man': 1, 'happy': 1}
     """
+
     pass
+    if isinstance(tokens, list) and tokens: #check
+        for words in tokens:
+            if not isinstance(words, str):  #if not word
+                return {}
+        freq_dict = {words:tokens.count(words)}
+        return freq_dict
+    return{}
 
 
 def get_top_n_words(freq_dict: dict, top_n: int) -> list:
@@ -51,6 +97,17 @@ def get_top_n_words(freq_dict: dict, top_n: int) -> list:
     --> ['happy']
     """
     pass
+    if not isinstance(freq_dict, dict) or not isinstance(top_n, int) or not freq_dict or not top_n >= 1:
+        return []
+
+    top_list = list(freq_dict.items())
+    top_list.sort(key=lambda i:i[1])    #sort by keys(2 element) for less volume-lambda
+
+    n_top_list = []
+    for f_word in top_list[:top_n]:
+        n_top_list.append(f_word[0])
+    return n_top_list
+
 
 
 def get_concordance(tokens: list, word: str, left_context_size: int, right_context_size: int) -> list:
@@ -71,6 +128,30 @@ def get_concordance(tokens: list, word: str, left_context_size: int, right_conte
     --> [['man', 'is', 'happy', 'the', 'dog', 'is'], ['dog', 'is', 'happy', 'but', 'the', 'cat']]
     """
     pass
+    if isinstance(right_context_size, bool) or isinstance(left_context_size, bool):     #check for emptiness
+        return []
+
+    if not isinstance(tokens, list) and not isinstance(word, str) \
+            and not isinstance(right_context_size, int) and not isinstance(left_context_size, int):       #check type
+        return []
+
+    part_text = []
+    for ind,elem in enumerate(tokens):  #make list with numbers & tokens
+        if elem == word:
+            if left_context_size > 0 and right_context_size > 0:
+                for i in ind:
+                    part_text.append(tokens[i - left_context_size:i+ right_context_size + 1])
+            elif left_context_size <= 0:
+                for i in ind:
+                    part_text.append(tokens[i:i + int(right_context_size) + 1])
+            elif right_context_size <= 0:
+                for i in ind:
+                    part_text.append(tokens[i - left_context_size:i + 1])
+            else:
+                part_text = []
+    return part_text
+
+
 
 
 def get_adjacent_words(tokens: list, word: str, left_n: int, right_n: int) -> list:
@@ -89,17 +170,6 @@ def get_adjacent_words(tokens: list, word: str, left_n: int, right_n: int) -> li
     --> [['man', 'is'], ['dog, 'cat']]
     """
     pass
-
-
-def read_from_file(path_to_file: str) -> str:
-    """
-    Opens the file and reads its content
-    :return: the initial text in string format
-    """
-    with open(path_to_file, 'r', encoding='utf-8') as fs:
-        data = fs.read()
-
-    return data
 
 
 def write_to_file(path_to_file: str, content: list):
---
+++ lab_1/№1 Lab.py
@@ -0,0 +1,6 @@
+import re
+
+
+t = open('data.txt')
+text = t.read()
+print (type(text))
