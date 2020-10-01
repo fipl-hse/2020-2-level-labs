5507d9a898fee843acaa16327858cbef3f20c651
katyanemzer <65305336+katyanemzer@users.noreply.github.com>
2020-10-01 22:16:28 +0300

    start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -2,8 +2,8 @@
 Concordance implementation starter
 """
 
-from main import read_from_file
 import os
+from main import read_from_file
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
@@ -13,6 +13,13 @@ if __name__ == '__main__':
 
     #  here goes your logic: calling methods from concordance.py
 
-    RESULT = None
+    RESULT = tokenize ('The weather is sunny, the man is happy.')
+    RESULT=remove_stop_words(['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy'],['the', 'is'])
+    RESULT=calculate_frequencies(['weather', 'sunny', 'man', 'happy'])
+    RESULT=get_top_n_words(['weather', 'sunny', 'man', 'happy', 'and', 'dog', 'happy'],1)
+    RESULT=get_concordance(['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
+                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad'],'happy',2,3)
+    RESULT=get_adjacent_words(['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
+                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad'],'happy',2,3)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT == [(), ()], 'Concordance not working'
+    assert RESULT == [['man', 'is'], ['dog, 'cat']], 'Concordance not working'
