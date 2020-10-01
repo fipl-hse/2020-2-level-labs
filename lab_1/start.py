b1a16d961d5cd68313ba50bb1e61edd3b3d05c7c
micefic <micefic@gmail.com>
2020-10-01 22:29:28 +0300

    fixed start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -1,6 +1,7 @@
 """
 Concordance implementation starter
 """
+
 import os
 import main
 
@@ -8,32 +9,35 @@ if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data = main.read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words.txt')).split('\n')
+    stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words.txt'))
+    stop_words = stop_words.split()
 
     #  here goes your logic: calling methods from concordance.py
     tokens = main.tokenize(data)
-    print('tokens:', tokens[:10])
+    print('tokenize:', tokens[:10])
 
-    tokens_stop_words = main.remove_stop_words(tokens, stop_words)
-    print('tokens without stop words:', tokens_stop_words[:10])
+    tokens = main.remove_stop_words(tokens, stop_words)
+    print('remove_stop_words:', tokens[:10])
 
-    tokens_frequencies = main.calculate_frequencies(tokens_stop_words[:10])
-    print('frequency of the first word:', tokens_frequencies[tokens_stop_words[0]])
+    freq_dict = main.calculate_frequencies(tokens[:10])
+    print('calculate_frequencies, first elem:', freq_dict[tokens[0]])
 
-    top = main.get_top_n_words(tokens_frequencies, 3)
-    print('Top 3 words:', top)
+    top_3_words = main.get_top_n_words(freq_dict, 3)
+    print('top 3 words:', top_3_words)
 
     concordance = main.get_concordance(tokens, 'team', 1, 2)
-    print("concordance for word 'team':", concordance[:3])
+    print('get_concordance for "team"', concordance[:3])
 
     adjacent_words = main.get_adjacent_words(tokens, 'team', 1, 2)
-    print("adjacent words for word 'team':", adjacent_words[:3])
+    print('get_adjacent_words for "team"', adjacent_words[:3])
 
-    sorts_concordance = main.sort_concordance(tokens, 'team', 1, 2, True)
-    print("sorted concordance for word 'team':", sorts_concordance[:4])
+    sorted_concordance = main.sort_concordance(tokens, 'team', 1, 2, True)
+    print('sort_concordance for "team"', sorted_concordance[:3])
 
-    main.write_to_file('report.txt', sorts_concordance)
+    main.write_to_file('report.txt', sorted_concordance)
 
-    RESULT = concordance
+    RESULT = sorted_concordance[:3]
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Concordance not working'
+    assert RESULT == [['a', 'team', 'form', 'cahl'],
+                      ['a', 'team', 'mission', 'taking'],
+                      ['a', 'team', 'touch', 'teams']], 'Concordance not working'
