627c4174372fe1ab23f60b517c25c4e3d4b0eac2
Yano Erina <blackberrycupcake@yandex.ru>
2020-10-01 21:38:19 +0300

    A little changes, a lot of failed tests

--- lab_1/main.py
+++ lab_1/main.py
@@ -6,29 +6,27 @@ A concordance extraction
 
 def tokenize(text: str) -> list:
     k = 0
-    for i in range(len(string)):  # проверка строки на корректность
-        if 48 <= ord(string[i]) <= 57 or (128 <= ord(string[i]) <= 175 or 224 <= ord(string[i]) <= 243):
+    for i in range(len(text)):  # проверка строки на корректность
+        if 48 <= ord(text[i]) <= 57 or (128 <= ord(text[i]) <= 175 or 224 <= ord(text[i]) <= 243):
             k += 1
             break
 
     if k != 0:  # если некорректные токены
         return []
     else:
-        string = string.lower()  # понизили регистр букв (сделали все строчными)
+        string = text.lower()  # понизили регистр букв (сделали все строчными)
         signs = ",;:\"\'.!?—()-><|"
-
-        for i in range(len(string)):  # цикл по длине строки
+        for i in range(len(text)):  # цикл по длине строки
             for j in range(len(signs)):  # цикл по длине строки с количеством знаков
                 if string[i] == signs[j]:  # если в введенной строке нашли знак препинания
-                    string = string[:i] + ' ' + string[
-                                                i + 1:]  # берем строку до знака препинания, прибавляем пробел и строку после знака
+                    string = string[:i] + ' ' + string[i + 1:]  # строка до знака препинания, пробел и строка после знака
         return string.split()
 
 
 def remove_stop_words(tokens: list, stop_words: list) -> list:
     k = 0
-    for i in range(len(string)):  # проверка строки на корректность
-        if 48 <= ord(' '.join(string)[i]) <= 57 or (128 <= ord(' '.join(string)[i]) <= 175 or 224 <= ord(' '.join(string)[i]) <= 243):
+    for i in range(len(tokens)):  # проверка строки на корректность
+        if 48 <= ord(' '.join(tokens)[i]) <= 57 or (128 <= ord(' '.join(tokens)[i]) <= 175 or 224 <= ord(' '.join(tokens)[i]) <= 243):
             k += 1
             break
     m = 0
@@ -43,13 +41,13 @@ def remove_stop_words(tokens: list, stop_words: list) -> list:
 
     else:  # если всё корректно
         stop_words = stop_words.split()
-        tokens = [token for token in string if token not in stop_words]
+        tokens = [token for token in tokens if token not in stop_words]
         return tokens
 
 def calculate_frequencies(tokens: list) -> dict:
     k = 0
-    for i in range(len(string)):  # проверка строки на корректность
-        if 48 <= ord(' '.join(string)[i]) <= 57 or (128 <= ord(' '.join(string)[i]) <= 175 or 224 <= ord(' '.join(string)[i]) <= 243):
+    for i in range(len(tokens)):  # проверка строки на корректность
+        if 48 <= ord(' '.join(tokens)[i]) <= 57 or (128 <= ord(' '.join(tokens)[i]) <= 175 or 224 <= ord(' '.join(tokens)[i]) <= 243):
             k += 1
             break
 
@@ -57,7 +55,7 @@ def calculate_frequencies(tokens: list) -> dict:
         return []
     else:
         dictionary = {}
-        for elem in string_new:
+        for elem in tokens:
             if elem in dictionary:
                 dictionary[elem] += 1
             else:
@@ -92,8 +90,8 @@ def get_top_n_words(freq_dict: dict, top_n: int) -> list:
 
 def get_concordance(tokens: list, word: str, left_context_size: int, right_context_size: int) -> list:
     k = 0
-    for i in range(len(string)):  # проверка строки на корректность
-        if 48 <= ord(' '.join(string)[i]) <= 57 or (128 <= ord(' '.join(string)[i]) <= 175 or 224 <= ord(' '.join(string)[i]) <= 243):
+    for i in range(len(tokens)):  # проверка строки на корректность
+        if 48 <= ord(' '.join(tokens)[i]) <= 57 or (128 <= ord(' '.join(tokens)[i]) <= 175 or 224 <= ord(' '.join(tokens)[i]) <= 243):
             k += 1
             break
     m = 0
@@ -106,53 +104,53 @@ def get_concordance(tokens: list, word: str, left_context_size: int, right_conte
     if k != 0 or m != 0 or (left_context_size == 0 and right_context_size == 0) or (i - left_context_size < 0 and i + 1 + right_context_size > len(string) - 1):  # если некорректные токены
         return []
     else:
-        for i in range(len(string)):
-            if word == string[i]:
-                if i - left_context_size > 0 and i + 1 + right_context_size < len(string) - 1:
-                    concordance.append(string[i - left_context_size:i + 1 + right_context_size])
+        for i in range(len(tokens)):
+            if word == tokens[i]:
+                if i - left_context_size > 0 and i + 1 + right_context_size < len(tokens) - 1:
+                    concordance.append(tokens[i - left_context_size:i + 1 + right_context_size])
                 elif i - left_context_size < 0:
-                    concordance.append(string[i:i + 1 + right_context_size])
-                elif i + 1 + right_context_size > len(string) - 1:
-                    concordance.append(string[i - left_context_size:i + 1])
+                    concordance.append(tokens[i:i + 1 + right_context_size])
+                elif i + 1 + right_context_size > len(tokens) - 1:
+                    concordance.append(tokens[i - left_context_size:i + 1])
     return concordance
 
 def get_adjacent_words(tokens: list, word: str, left_n: int, right_n: int) -> list:
     global i
     k = 0
-    for i in range(len(string)):  # проверка строки на корректность
-        if 48 <= ord(' '.join(string)[i]) <= 57 or (128 <= ord(' '.join(string)[i]) <= 175 or 224 <= ord(' '.join(string)[i]) <= 243):
+    for i in range(len(tokens)):  # проверка строки на корректность
+        if 48 <= ord(' '.join(tokens)[i]) <= 57 or (128 <= ord(' '.join(tokens)[i]) <= 175 or 224 <= ord(' '.join(tokens)[i]) <= 243):
             k += 1
             break
     m = 0
-    for i in range(len(word1)):  # проверка слова на корректность
-        if 48 <= ord(word1[i]) <= 57 or (128 <= ord(word1[i]) <= 175 or 224 <= ord(word1[i]) <= 243):
+    for i in range(len(word)):  # проверка слова на корректность
+        if 48 <= ord(word[i]) <= 57 or (128 <= ord(word[i]) <= 175 or 224 <= ord(word[i]) <= 243):
             m += 1
             break
 
     adjacent_words = []
-    if k != 0 or m != 0 or (left_n == 0 and right_n == 0) or (i - left_n < 0 and i + 1 + right_n > len(string) - 1):  # если некорректные токены
+    if k != 0 or m != 0 or (left_n == 0 and right_n == 0) or (i - left_n < 0 and i + 1 + right_n > len(tokens) - 1):  # если некорректные токены
         return []
     else:
-        for i in range(len(string)):
-            if word1 == string[i]:
-                if i - left_n > 0 and i + 1 + right_n < len(string) - 1:  # если не выходим за границы
-                    adjacent_words.append([string[i - left_n], string[i + right_n]])
+        for i in range(len(tokens)):
+            if word1 == tokens[i]:
+                if i - left_n > 0 and i + 1 + right_n < len(tokens) - 1:  # если не выходим за границы
+                    adjacent_words.append([tokens[i - left_n], tokens[i + right_n]])
                 elif i - left_context_size < 0:
-                    adjacent_words.append(string[i + right_n])
-                elif i + 1 + right_context_size > len(string) - 1:
-                    adjacent_words.append(string[i - left_n])
+                    adjacent_words.append(tokens[i + right_n])
+                elif i + 1 + right_context_size > len(tokens) - 1:
+                    adjacent_words.append(tokens[i - left_n])
     return adjacent_words
 
 
 def read_from_file(path_to_file: str) -> str:
-    with open(path_to_file, 'r') as report:
+    with open(path_to_file, 'r', encoding = 'utf-8') as report:
         data = report.read()
 
     return data
 
 
 def write_to_file(path_to_file: str, content: list):
-    with open(path_to_file, 'w') as report:
+    with open(path_to_file, 'r') as report:
         for i in range(len(concordance)):
             report.write(' '.join(concordance[i]))
             report.write('\n')
--- lab_1/start.py
+++ lab_1/start.py
@@ -2,17 +2,37 @@
 Concordance implementation starter
 """
 
-from main import read_from_file
 import os
+
+from main import calculate_frequencies
+from main import get_adjacent_words
+from main import get_concordance
+from main import read_from_file
+from main import remove_stop_words
+from main import tokenize
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data = read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = []
+    stop_words = read_from_file(os.path.join(current_dir, 'stop_words.txt'))
 
     #  here goes your logic: calling methods from concordance.py
+    tokens = tokenize(data)[:51]
+    print('First 50 tokens: ', tokens)
 
-    RESULT = None
+    tokens = remove_stop_words(tokens, stop_words)
+    print('Stop words are now removed: ', tokens)
+
+    frequencies = calculate_frequencies(tokens)
+    print('The frequencies of those tokens are: ', frequencies)
+
+    concordance = get_concordance(tokens, 'tired', 1, 3)
+    print('Concordance for a searched token "sick" looks like: ', concordance)
+
+    adjacent_words = get_adjacent_words(tokens, 'sick', 1, 3)
+    print('Adjacent words for token "sick" are: ', adjacent_words)
+
+    RESULT = concordance
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT == [(), ()], 'Concordance not working'
+    assert RESULT == [['feels', 'sick', 'tired', 'worn', 'sleepy']], 'Concordance not working'
