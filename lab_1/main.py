48ced8cae9c34800bbea22f2df1353f40abdc492
aphexqueen <kristinasheshmeneva@yandex.ru>
2020-10-01 21:21:16 +0300

    add functions realization

--- lab_1/main.py
+++ lab_1/main.py
@@ -12,7 +12,23 @@ def tokenize(text: str) -> list:
     e.g. text = 'The weather is sunny, the man is happy.'
     --> ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
     """
-    pass
+    tokens = []
+    if isinstance(text, str):
+        text += ' '
+        text_clean = ''
+        for symbol in text:
+            if symbol.isalpha() or symbol == ' ':
+                text_clean += symbol
+            else:
+                symbol_index = text.index(symbol)
+                if text[symbol_index + 1] == ' ':
+                    text_clean += ' '
+                elif text[symbol_index + 1].isalpha():
+                    text_clean += symbol
+        text_lower = text_clean.lower()
+        tokens = text_lower.split()
+
+    return tokens
 
 
 def remove_stop_words(tokens: list, stop_words: list) -> list:
@@ -25,7 +41,19 @@ def remove_stop_words(tokens: list, stop_words: list) -> list:
     stop_words = ['the', 'is']
     --> ['weather', 'sunny', 'man', 'happy']
     """
-    pass
+    tokens_clean = []
+    if isinstance(tokens, list):
+        if isinstance(stop_words, list):
+            for token in tokens:
+                if token not in stop_words:
+                    tokens_clean.append(token)
+                else:
+                    continue
+            return tokens_clean
+        else:
+            return tokens
+
+    return tokens_clean
 
 
 def calculate_frequencies(tokens: list) -> dict:
@@ -36,7 +64,12 @@ def calculate_frequencies(tokens: list) -> dict:
     e.g. tokens = ['weather', 'sunny', 'man', 'happy']
     --> {'weather': 1, 'sunny': 1, 'man': 1, 'happy': 1}
     """
-    pass
+    freq_dict = {}
+    if isinstance(tokens, list):
+        for token in tokens:
+            freq_dict[token] = tokens.count(token)
+
+    return freq_dict
 
 
 def get_top_n_words(freq_dict: dict, top_n: int) -> list:
@@ -49,7 +82,18 @@ def get_top_n_words(freq_dict: dict, top_n: int) -> list:
     top_n = 1
     --> ['happy']
     """
-    pass
+    top_n_words = []
+    if isinstance(freq_dict, dict) and isinstance(top_n, int):
+        if top_n <= len(freq_dict):
+            freq_list = list(freq_dict.items())
+            freq_list.sort(key=lambda num: num[1], reverse=True)
+            freq_dict_sorted = dict(freq_list)
+            top_words = []
+            for frequency, word in enumerate(freq_dict_sorted):
+                top_words.append(word)
+            top_n_words = top_words[:top_n]
+
+    return top_n_words
 
 
 def get_concordance(tokens: list, word: str, left_context_size: int, right_context_size: int) -> list:
@@ -69,7 +113,45 @@ def get_concordance(tokens: list, word: str, left_context_size: int, right_conte
     right_context_size = 3
     --> [['man', 'is', 'happy', 'the', 'dog', 'is'], ['dog', 'is', 'happy', 'but', 'the', 'cat']]
     """
-    pass
+    concordance = []
+    if isinstance(tokens, list) and isinstance(word, str) and isinstance(left_context_size, int) and isinstance(right_context_size, int):
+        word_indexes = []
+        tokens_copy = tokens.copy()
+        for token in tokens_copy:
+            if token == word:
+                token_index = tokens_copy.index(token)
+                word_indexes.append(token_index)
+                tokens_copy.pop(token_index)
+                tokens_copy.insert(token_index, '?')
+        left_limit = word_indexes[-1]
+        right_limit = len(tokens)
+        right_limit - int(word_indexes[-1])
+        left_subconcordance = []
+        right_subconcordance = []
+        if left_context_size >= 1 and left_context_size <= left_limit:
+            for index in word_indexes:
+                left_example = tokens[index - left_context_size:index]
+                left_example.append(word)
+                left_subconcordance.append(left_example)
+        if right_context_size >= 1 and right_context_size <= right_limit:
+            for index in word_indexes:
+                right_example = tokens[index + 1:index + right_context_size + 1]
+                right_example.insert(0, word)
+                right_subconcordance.append(right_example)
+        if len(left_subconcordance) == 0:
+            concordance = right_subconcordance
+        elif len(right_subconcordance) == 0:
+            concordance = left_subconcordance
+        elif len(left_subconcordance) == 0 and len(right_subconcordance) == 0:
+            pass
+        else:
+            for index in word_indexes:
+                example = tokens[index - left_context_size:index]  # left context
+                example.append(word)
+                example += tokens[index + 1:index + right_context_size + 1]  # right context
+                concordance.append(example)
+
+    return concordance
 
 
 def get_adjacent_words(tokens: list, word: str, left_n: int, right_n: int) -> list:
@@ -87,7 +169,22 @@ def get_adjacent_words(tokens: list, word: str, left_n: int, right_n: int) -> li
     right_n = 3
     --> [['man', 'is'], ['dog, 'cat']]
     """
-    pass
+    adjacent_words = []
+    if isinstance(tokens, list) and isinstance(word, str) and isinstance(left_n, int) and isinstance(right_n, int):
+        concordance = get_concordance(tokens, word, left_n, right_n)
+        if len(concordance) != 0:
+            for example in concordance:
+                if example[0] == word:
+                    right_adjacent_word = [example[right_n]]
+                    adjacent_words.append(right_adjacent_word)
+                elif example[-1] == word:
+                    left_adjacent_word = [example[0]]
+                    adjacent_words.append(left_adjacent_word)
+                else:
+                    right_and_left_adjacent_words = [example[0], example[-1]]
+                    adjacent_words.append(right_and_left_adjacent_words)
+
+    return adjacent_words
 
 
 def read_from_file(path_to_file: str) -> str:
@@ -126,5 +223,3 @@ def sort_concordance(tokens: list, word: str, left_context_size: int, right_cont
     --> [['dog', 'is', 'happy', 'but', 'the', 'cat'], ['man', 'is', 'happy', 'the', 'dog', 'is']]
     """
     pass
-
-# something new for pull request
