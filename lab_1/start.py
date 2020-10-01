9d8f8e4450e66f5b4a3053bc66049ac1b5c1779e
Ekaterina Sizova <ekaterina.esizovakate-ru@yandex.ru>
2020-10-02 02:14:25 +0300

    Changes

--- lab_1/start.py
+++ lab_1/start.py
@@ -10,27 +10,33 @@ if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data = read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = read_from_file(os.path.join(current_dir, 'stop_words.txt'))
+    stop_words = read_from_file(os.path.join(current_dir, 'stop_words.txt')).split()
 
     #  here goes your logic: calling methods from concordance.py
     tokens = main.tokenize(data)
-    print(tokens[:10])
+    print('Tokens:', tokens[:20])
 
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    tokens = main.remove_stop_words(tokens[:6], [stop_words])
-    print('Stop words have been removed: {}'. format(tokens))
+    tokens_stop_words = main.remove_stop_words(tokens, stop_words)
+    print('Tokens without stop words:', tokens_stop_words[:20])
 
-    freq_dict = main.calculate_frequencies(tokens)
-    print('The frequency dictionary of tokens is: {}'. format(freq_dict))
+    tokens = main.remove_stop_words(tokens, stop_words)
+    print('Tokens without stop words:', tokens[:10])
 
-    top_n_words = main.get_top_n_words(freq_dict, 2)
-    print('Top_n_words:', freq_dict, 2)
+    tokens_frequencies = main.calculate_frequencies(tokens_stop_words[:20])
+    print('The frequency dictionary of tokens is:', tokens[:10])
 
-    concordance = main.get_concordance(tokens, 'division', 2, 3)
-    print('The concordance for the token "division" is: {}'. format(concordance))
+    top_n_words = main.get_top_n_words(tokens_frequencies, 20)
+    print('Top 20 words:', top_n_words)
 
-    adjacent = main.get_adjacent_words(tokens, 'cat', 2, 3)
-    print('The adjacent words is: ', adjacent[:5])
+    concordance = main.get_concordance(tokens, 'task', 2, 3)
+    print("The concordance for word 'task':", concordance[:6])
+
+    adjacent = main.get_adjacent_words(tokens, 'task', 2, 3)
+    print("The adjacent for word 'task': ", adjacent)
+
+    main.write_to_file('report.txt', top_n_words)
+
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    RESULT = concordance
+    RESULT = top_n_words
     assert RESULT, 'Concordance not working'
