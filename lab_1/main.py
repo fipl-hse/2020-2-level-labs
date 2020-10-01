3e00aa5b5a9371f8926e84378fc1e512eb212978
Yano Erina <blackberrycupcake@yandex.ru>
2020-10-01 17:42:12 +0300

    Inserted the newly-written code

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,125 +5,155 @@ A concordance extraction
 
 
 def tokenize(text: str) -> list:
-    """
-    Splits sentences into tokens, converts the tokens into lowercase, removes punctuation
-    :param text: the initial text
-    :return: a list of lowercased tokens without punctuation
-    e.g. text = 'The weather is sunny, the man is happy.'
-    --> ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
-    """
-    text = text.split()
-    return text
+    k = 0
+    for i in range(len(string)):  # проверка строки на корректность
+        if 48 <= ord(string[i]) <= 57 or (128 <= ord(string[i]) <= 175 or 224 <= ord(string[i]) <= 243):
+            k += 1
+            break
+
+    if k != 0:  # если некорректные токены
+        return []
+    else:
+        string = string.lower()  # понизили регистр букв (сделали все строчными)
+        signs = ",;:\"\'.!?—()-><|"
+
+        for i in range(len(string)):  # цикл по длине строки
+            for j in range(len(signs)):  # цикл по длине строки с количеством знаков
+                if string[i] == signs[j]:  # если в введенной строке нашли знак препинания
+                    string = string[:i] + ' ' + string[
+                                                i + 1:]  # берем строку до знака препинания, прибавляем пробел и строку после знака
+        return string.split()
 
 
 def remove_stop_words(tokens: list, stop_words: list) -> list:
-    """
-    Removes stop words
-    :param tokens: a list of tokens
-    :param stop_words: a list of stop words
-    :return: a list of tokens without stop words
-    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
-    stop_words = ['the', 'is']
-    --> ['weather', 'sunny', 'man', 'happy']
-    """
-    pass
+    k = 0
+    for i in range(len(string)):  # проверка строки на корректность
+        if 48 <= ord(' '.join(string)[i]) <= 57 or (128 <= ord(' '.join(string)[i]) <= 175 or 224 <= ord(' '.join(string)[i]) <= 243):
+            k += 1
+            break
+    m = 0
+    for i in range(len(stop_words)):  # проверка стоп-слов на корректность
+        if 48 <= ord(stop_words[i]) <= 57 or (128 <= ord(stop_words[i]) <= 175 or 224 <= ord(stop_words[i]) <= 243):
+            m += 1
+            break
+    if k != 0:  # если некорректные токены
+        return []
+    elif m != 0:  # если корректные токены, но некорректные стоп-слова, то возвращаем список токенов без изменений
+        tokenize(string)
 
+    else:  # если всё корректно
+        stop_words = stop_words.split()
+        tokens = [token for token in string if token not in stop_words]
+        return tokens
 
 def calculate_frequencies(tokens: list) -> dict:
-    """
-    Calculates frequencies of given tokens
-    :param tokens: a list of tokens without stop words
-    :return: a dictionary with frequencies
-    e.g. tokens = ['weather', 'sunny', 'man', 'happy']
-    --> {'weather': 1, 'sunny': 1, 'man': 1, 'happy': 1}
-    """
-    pass
+    k = 0
+    for i in range(len(string)):  # проверка строки на корректность
+        if 48 <= ord(' '.join(string)[i]) <= 57 or (128 <= ord(' '.join(string)[i]) <= 175 or 224 <= ord(' '.join(string)[i]) <= 243):
+            k += 1
+            break
+
+    if k != 0:  # если некорректные токены
+        return []
+    else:
+        dictionary = {}
+        for elem in string_new:
+            if elem in dictionary:
+                dictionary[elem] += 1
+            else:
+                dictionary[elem] = 1
+        return dictionary
 
 
 def get_top_n_words(freq_dict: dict, top_n: int) -> list:
-    """
-    Returns the most common words
-    :param freq_dict: a dictionary with frequencies
-    :param top_n: a number of the most common words to return
-    :return: a list of the most common words
-    e.g. tokens = ['weather', 'sunny', 'man', 'happy', 'and', 'dog', 'happy']
-    top_n = 1
-    --> ['happy']
-    """
-    pass
+    k = 0
+    for i in range(len(string)):  # проверка строки на корректность
+        if 48 <= ord(' '.join(string)[i]) <= 57 or (128 <= ord(' '.join(string)[i]) <= 175 or 224 <= ord(' '.join(string)[i]) <= 243):
+            k += 1
+            break
+
+    if k != 0:  # если некорректные токены
+        return []
+    else:
+        top_n = []
+        frequency = []
+        number = 0
+        for key, value in dictionary.items():
+            frequency.append([value, key])
+        frequency.sort(reverse=True)
+        if n <= len(frequency):
+            for element in frequency:
+                if number < n:
+                    top_n.append(element[1])
+                    number += 1
+            return top_n
+
 
 
 def get_concordance(tokens: list, word: str, left_context_size: int, right_context_size: int) -> list:
-    """
-    Gets a concordance of a word
-    A concordance is a listing of each occurrence of a word in a text,
-    presented with the words surrounding it
-    :param tokens: a list of tokens
-    :param word: a word-base for a concordance
-    :param left_context_size: the number of words in the left context
-    :param right_context_size: the number of words in the right context
-    :return: a concordance
-    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
-                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad']
-    word = 'happy'
-    left_context_size = 2
-    right_context_size = 3
-    --> [['man', 'is', 'happy', 'the', 'dog', 'is'], ['dog', 'is', 'happy', 'but', 'the', 'cat']]
-    """
-    pass
+    k = 0
+    for i in range(len(string)):  # проверка строки на корректность
+        if 48 <= ord(' '.join(string)[i]) <= 57 or (128 <= ord(' '.join(string)[i]) <= 175 or 224 <= ord(' '.join(string)[i]) <= 243):
+            k += 1
+            break
+    m = 0
+    for i in range(len(word)):  # проверка слова на корректность
+        if 48 <= ord(word[i]) <= 57 or (128 <= ord(word[i]) <= 175 or 224 <= ord(word[i]) <= 243):
+            m += 1
+            break
 
+    concordance = []
+    if k != 0 or m != 0 or (left_context_size == 0 and right_context_size == 0) or (i - left_context_size < 0 and i + 1 + right_context_size > len(string) - 1):  # если некорректные токены
+        return []
+    else:
+        for i in range(len(string)):
+            if word == string[i]:
+                if i - left_context_size > 0 and i + 1 + right_context_size < len(string) - 1:
+                    concordance.append(string[i - left_context_size:i + 1 + right_context_size])
+                elif i - left_context_size < 0:
+                    concordance.append(string[i:i + 1 + right_context_size])
+                elif i + 1 + right_context_size > len(string) - 1:
+                    concordance.append(string[i - left_context_size:i + 1])
+    return concordance
 
 def get_adjacent_words(tokens: list, word: str, left_n: int, right_n: int) -> list:
-    """
-    Gets adjacent words from the left and right context
-    :param tokens: a list of tokens
-    :param word: a word-base for the search
-    :param left_n: the distance between a word and an adjacent one in the left context
-    :param right_n: the distance between a word and an adjacent one in the right context
-    :return: a list of adjacent words
-    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
-                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad']
-    word = 'happy'
-    left_n = 2
-    right_n = 3
-    --> [['man', 'is'], ['dog, 'cat']]
-    """
-    pass
+    global i
+    k = 0
+    for i in range(len(string)):  # проверка строки на корректность
+        if 48 <= ord(' '.join(string)[i]) <= 57 or (128 <= ord(' '.join(string)[i]) <= 175 or 224 <= ord(' '.join(string)[i]) <= 243):
+            k += 1
+            break
+    m = 0
+    for i in range(len(word1)):  # проверка слова на корректность
+        if 48 <= ord(word1[i]) <= 57 or (128 <= ord(word1[i]) <= 175 or 224 <= ord(word1[i]) <= 243):
+            m += 1
+            break
+
+    adjacent_words = []
+    if k != 0 or m != 0 or (left_n == 0 and right_n == 0) or (i - left_n < 0 and i + 1 + right_n > len(string) - 1):  # если некорректные токены
+        return []
+    else:
+        for i in range(len(string)):
+            if word1 == string[i]:
+                if i - left_n > 0 and i + 1 + right_n < len(string) - 1:  # если не выходим за границы
+                    adjacent_words.append([string[i - left_n], string[i + right_n]])
+                elif i - left_context_size < 0:
+                    adjacent_words.append(string[i + right_n])
+                elif i + 1 + right_context_size > len(string) - 1:
+                    adjacent_words.append(string[i - left_n])
+    return adjacent_words
 
 
 def read_from_file(path_to_file: str) -> str:
-    """
-    Opens the file and reads its content
-    :return: the initial text in string format
-    """
-    with open(path_to_file, 'r', encoding='utf-8') as fs:
-        data = fs.read()
+    with open(path_to_file, 'r') as report:
+        data = report.read()
 
     return data
 
 
 def write_to_file(path_to_file: str, content: list):
-    """
-    Writes the result in a file
-    """
-    pass
+    with open(path_to_file, 'w') as report:
+        for i in range(len(concordance)):
+            report.write(' '.join(concordance[i]))
+            report.write('\n')
 
-
-def sort_concordance(tokens: list, word: str, left_context_size: int, right_context_size: int, left_sort: bool) -> list:
-    """
-    Gets a concordance of a word and sorts it by either left or right context
-    :param tokens: a list of tokens
-    :param word: a word-base for a concordance
-    :param left_context_size: the number of words in the left context
-    :param right_context_size: the number of words in the right context
-    :param left_sort: if True, sort by the left context, False – by the right context
-    :return: a concordance
-    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
-                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad']
-    word = 'happy'
-    left_context_size = 2
-    right_context_size = 3
-    left_sort = True
-    --> [['dog', 'is', 'happy', 'but', 'the', 'cat'], ['man', 'is', 'happy', 'the', 'dog', 'is']]
-    """
-    pass
