d354f2b249a1953c10e6983c6af6ba7653b59728
TroublemakerSasha <sashalysuenko@mail.ru>
2020-10-01 12:53:32 +0300

    Add realisation

--- lab_1/main.py
+++ lab_1/main.py
@@ -12,8 +12,8 @@ def tokenize(text: str) -> list:
     e.g. text = 'The weather is sunny, the man is happy.'
     --> ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
     """
-    print('hello')
-    pass
+    text=text.split()
+    return text
 
 
 def remove_stop_words(tokens: list, stop_words: list) -> list:
