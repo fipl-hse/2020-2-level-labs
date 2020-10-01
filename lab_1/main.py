8cb7e17de8b981ffbea5ad09294b641f91f0cdd5
katherinekh <nerupineapple@gmail.com>
2020-10-01 20:22:34 +0300

    it should be the last commit..

--- lab_1/main.py
+++ lab_1/main.py
@@ -102,11 +102,9 @@ def get_concordance(tokens: list, word: str, left_context_size: int, right_conte
     """
     if not isinstance(tokens, list) or not isinstance(word, str):
         return []
-    for token in tokens:
-        if not isinstance(token, str):
-            return []
-    if (not isinstance(left_context_size, int) and not isinstance(right_context_size, int)) or \
-            (left_context_size < 1 and right_context_size < 1):
+    if not isinstance(left_context_size, int) and not isinstance(right_context_size, int):
+        return []
+    if left_context_size < 1 and right_context_size < 1:
         return []
     concordance = []
     count = 0
