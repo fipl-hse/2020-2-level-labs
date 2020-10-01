c5c06933268c73c0dc9142553b6b07361c790ef7
micefic <micefic@gmail.com>
2020-10-01 20:50:23 +0300

    fix

--- lab_1/main.py
+++ lab_1/main.py
@@ -201,6 +201,7 @@ def sort_concordance(tokens: list, word: str, left_context_size: int, right_cont
     if not isinstance(left_sort, bool) or not isinstance(tokens, list) or not isinstance(word, str) \
             or not isinstance(left_context_size, int) or not isinstance(right_context_size, int):
         return []
+
     concord = get_concordance(tokens, word, left_context_size, right_context_size)
     if left_sort and left_context_size > 0:
         return sorted(concord)
