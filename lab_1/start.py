407205ca72f921a93666f209bbf3823392f53d76
makar-fedorov <65069232+makar-fedorov@users.noreply.github.com>
2020-10-02 10:48:55 +0500

    changes in start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -15,7 +15,7 @@ from main import sort_concordance
 if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    data = read_from_file(os.path.join(current_dir, 'lab_1/data.txt'))
+    data = read_from_file(os.path.join(current_dir, 'data.txt'))
     stop_words = []
 
     #  here goes your logic: calling methods from concordance.py
