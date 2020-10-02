94e5639749d359cf5d86754c81423ee474435c87
Ekaterina Sizova <ekaterina.esizovakate-ru@yandex.ru>
2020-10-02 06:23:58 +0300

    Add

--- lab_1/start.py
+++ lab_1/start.py
@@ -17,25 +17,23 @@ if __name__ == '__main__':
     print('Tokens:', tokens[:20])
 
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    tokens_stop_words = main.remove_stop_words(tokens, stop_words)
-    print('Tokens without stop words:', tokens_stop_words[:20])
 
     tokens = main.remove_stop_words(tokens, stop_words)
-    print('Tokens without stop words:', tokens[:10])
+    print('Tokens without stop words:', {tokens[:100]})
 
-    tokens_frequencies = main.calculate_frequencies(tokens_stop_words[:20])
+    tokens_frequencies = main.calculate_frequencies(tokens)
     print('The frequency dictionary of tokens is:', tokens[:10])
 
     top_n_words = main.get_top_n_words(tokens_frequencies, 20)
     print('Top 20 words:', top_n_words)
 
-    concordance = main.get_concordance(tokens, 'task', 2, 3)
+    concordance = main.get_concordance(tokens, 'end', 2, 3)
     print("The concordance for word 'task':", concordance[:6])
 
-    adjacent = main.get_adjacent_words(tokens, 'task', 2, 3)
+    adjacent = main.get_adjacent_words(tokens, 'end', 2, 3)
     print("The adjacent for word 'task': ", adjacent)
 
-    main.write_to_file('report.txt', top_n_words)
+    main.write_to_file('tokens.txt', top_n_words)
 
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     RESULT = top_n_words
