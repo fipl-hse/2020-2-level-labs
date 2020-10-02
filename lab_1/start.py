4edc573d30584dbb9f5dd90b6b0045b3b1b18a7e
ashirka <66332800+ashirka@users.noreply.github.com>
2020-10-02 09:40:09 +0300

    Update start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -12,7 +12,28 @@ if __name__ == '__main__':
     stop_words = []
 
     #  here goes your logic: calling methods from concordance.py
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data = main.read_from_file(os.path.join(current_dir, 'data.txt'))
+    stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words.txt'))
+    
+    tokens = main.tokenize(data)
+    print('tokenize', tokens[:10])
 
-    RESULT = None
+    tokens = main.remove_stop_words(tokens, stop_words)
+    print('remove_stop_words', tokens[:10])
+
+    frequencies = main.calculate_frequencies(tokens[:5000])
+    print('calculate_frequencies', frequencies[tokens[0]])
+
+    concordance = main.get_concordance(tokens, 'name', 3, 0)
+    print('get_concordance', concordance[:5])
+
+    adjacent = main.get_adjacent_words(tokens, 'name', 2, 0)
+    print('get_adjacent_words', adjacent[:5])
+
+    sorted_concordance = main.sort_concordance(tokens, 'name', 5, 2, True)
+    print('sort_concordance', sorted_concordance[:5])
+
+    RESULT = sorted_concordance
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT == [(), ()], 'Concordance not working'
