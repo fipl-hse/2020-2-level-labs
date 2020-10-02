748b908608c69fa90706a05b65fb3aa5bf1ba0d1
makar-fedorov <65069232+makar-fedorov@users.noreply.github.com>
2020-10-02 17:11:34 +0500

    start

--- lab_1/start.py
+++ lab_1/start.py
@@ -6,11 +6,6 @@ import os
 from main import tokenize
 from main import read_from_file
 from main import sort_concordance
-'''from main import remove_stop_words
-from main import calculate_frequencies
-from main import get_top_n_words
-from main import get_concordance
-from main import get_adjacent_words'''
 
 
 if __name__ == '__main__':
@@ -21,12 +16,7 @@ if __name__ == '__main__':
 
     #  here goes your logic: calling methods from concordance.py
     tokens = tokenize(data)
-    '''stop_words = read_from_file(os.path.join(current_dir, 'stop_words.txt')).split()
-    clean_text_list = remove_stop_words(tokens, stop_words)
-    frequencies_dict = calculate_frequencies(tokens)
-    top_n_words = get_top_n_words(frequencies_dict, 5)
-    concordance = get_concordance(tokens, 'sodium', 2, 2)
-    adjacent_words = get_adjacent_words(tokens, 'sodium', 2, 2)'''
+
 
     RESULT = sort_concordance(tokens, 'sodium', 1, 1, True)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
