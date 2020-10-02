495c34c987a1c80180d9a46480428f8ff513879b
marina-kaz <isthismeowgoingfrommyheart@gmail.com>
2020-10-02 10:55:08 +0300

    almost there

--- lab_1/start.py
+++ lab_1/start.py
@@ -4,44 +4,45 @@ Concordance implementation starter
 
 
 import os
-from main import *
+import main
+
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    data = read_from_file(os.path.join(current_dir, 'data.txt'))
+    data = main.read_from_file(os.path.join(current_dir, 'data.txt'))
     stop_words = []
     print('What does this program do?')
     print(f'It reads texts: {data[:50]}...')
-    tokens = remove_stop_words(tokenize(data), stop_words)
+    tokens = main.remove_stop_words(main.tokenize(data), stop_words)
     print(f'It tokenizes it: {tokens[:100]}')
-    frequencies = calculate_frequencies(tokens)
+    frequencies = main.calculate_frequencies(tokens)
     print(f'It also count how many times a word appears in text. '
           f'For instance, the word "year" appears '
           f'there {frequencies["year"]} times')
-    top = get_top_n_words(frequencies, 20)
+    top = main.get_top_n_words(frequencies, 20)
     print(f'It can also provide you with as many top-frequent words as you '
           f'wish. For example, here are the first 20: {", ".join(top)}.')
-    concordance = sort_concordance(tokens, 'year', 3, 5, True)
-    contexts = []
+    concordance = main.sort_concordance(tokens, 'year', 3, 5, True)
+    CONTEXTS = []
     for example in concordance[:10]:
-        context = " ".join(example)
-        contexts.append(context)
-    contexts = "\n".join(contexts)
+        CONTEXT = " ".join(example)
+        CONTEXTS.append(CONTEXT)
+    CONTEXTS = "\n".join(CONTEXTS)
     print(f'Finally, you can access 10 first contexts in which a certain '
           f'word appeard using this prog. Here is all the contexts for '
-          f'word "year":\n{contexts}')
+          f'word "year":\n{CONTEXTS}')
     print('What do we mean by "first"? Well, they are all sorted!' 
           'Notice that they appear in a alphabetical order.'
           ' And even reversed.')
-    adj_words = get_adjacent_words(tokens, 'year', 3, 5)
-    pairs = []
+    adj_words = main.get_adjacent_words(tokens, 'year', 3, 5)
+    PAIRS = []
     for words in adj_words[:10]:
-        pair = " ".join(words)
-        pairs.append(pair)
-    pairs = "\n".join(pairs)
-    print(f'We can also get adjacted words! Here are some of them for "year":\n{pairs}')
-    RESULT = sort_concordance(tokens, 'end', 3, 5, True)[:10]
+        PAIR = " ".join(words)
+        PAIRS.append(PAIR)
+    PAIRS = "\n".join(PAIRS)
+    print(f'We can also get adjacted words! Here are some of them for "year":\n{PAIRS}')
+    RESULT = main.sort_concordance(tokens, 'end', 3, 5, True)[:10]
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT == [['an', 'unusually', 'early', 'end', 'to', 'his', 'career', 'in', 'the'],
                       ['and', 'sometimes', 'they', 'end', 'them', 'literally', 'with', 'carved', 'wisps'],
