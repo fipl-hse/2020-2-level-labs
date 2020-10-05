32aefecb7842144edf4ae032196e250fa41429d1
TroublemakerSasha <sashalysuenko@mail.ru>
2020-10-05 11:29:49 +0300

    The end

--- lab_1/main.py
+++ lab_1/main.py
@@ -9,8 +9,8 @@ def tokenize(text: str) -> list:
         for character in token:
             if character.isalpha():
                 word += character
-             if word:
-                 clean_tokens.append(word)
+            if word:
+                clean_tokens.append(word)
     return clean_tokens
 
 def remove_stop_words(tokens: list, stop_words: list) -> list:
