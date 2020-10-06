7cf069c6363fd43ef447864451cffeb9d929e9f7
alenashuvar <alenashuvar@users.noreply.github.com>
2020-10-06 11:00:15 +0300

    Some changes in start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -20,18 +20,18 @@ if __name__ == '__main__':
     cleaned_tokens = main.remove_stop_words(tokens, list_stop_words)
     print(f"First 15 tokens without stop words: {cleaned_tokens[:15]}")
 
-    tokens_frequencies = main.calculate_frequencies(cleaned_tokens)
+    tokens_frequencies = main.calculate_frequencies(cleaned_tokens[:3000])
     third_token = cleaned_tokens[2]
     print(f"Frequency of the third word: {tokens_frequencies[third_token]}")
 
-    top_7_words = main.get_top_n_words(tokens_frequencies, 7)
-    print(f"Top 7 words: {top_7_words}")
+    top_5_words = main.get_top_n_words(tokens_frequencies, 5)
+    print(f"Top 5 words: {top_5_words}")
 
     family_concordance = main.get_concordance(cleaned_tokens, "family", 2, 3)
     print(f"Part of concordance for 'family': {family_concordance[:4]}")
 
     family_adjacent_words = main.get_adjacent_words(cleaned_tokens, 'family', 2, 2)
-    print(f"Part of adjacent words for 'family': {family_adjacent_words[:4]}")
+    print(f"Part of adjacent words (left = 2, right = 3) for 'family': {family_adjacent_words[:4]}")
 
     family_sorted_concordance = main.sort_concordance(cleaned_tokens, 'family', 1, 2, False)
     print(f"Part of sorted concordance for 'family': {family_sorted_concordance[:4]}")
@@ -40,4 +40,7 @@ if __name__ == '__main__':
 
     RESULT = family_sorted_concordance
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Concordance not working'
+    assert [['woman', 'family', 'assist', 'a'], ['pythoninae', 'family', 'boidae', 'boas'],
+            ['recommendation', 'family', 'cautioned', 'heisenberg'],
+            ['reverted', 'family', 'childless', 'children']], 'Concordance not working'
+
