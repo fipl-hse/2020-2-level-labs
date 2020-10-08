6a254be6844e114f237e4a6b8c0eaa6c16db1677
vchistyakova <65254510+vchistyakova@users.noreply.github.com>
2020-10-08 23:05:54 +0300

    add final changes in start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -1,18 +1,34 @@
 """
 Concordance implementation starter
 """
-
-from main import read_from_file
 import os
+import main
+from main import read_from_file
+
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data = read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = []
+    stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words.txt')).split('\n')
+
+    tokens = main.tokenize(data)
+    print('tokens', tokens[:5])
+
+    tokens = main.remove_stop_words(tokens, stop_words)
+    print('tokens without stop words', tokens[:5])
+
+    frequency = main.calculate_frequencies(tokens)
+    print('frequency for the fifth word', frequency[tokens[4]])
+
+    top_5 = main.get_top_n_words(frequency, 5)
+    print('top five words', top_5)
+
+    concordance_moose = main.get_concordance(tokens, 'moose', 1, 4)
+    print('concordance for "moose"', concordance_moose[:5])
 
     #  here goes your logic: calling methods from concordance.py
 
-    RESULT = None
+    RESULT = frequency
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Concordance not working'
