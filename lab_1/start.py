7866186e635e14354f30a15dd02f8706edbe3ba0
OlesyaGanina <65288843+OlesyaGanina@users.noreply.github.com>
2020-10-08 21:29:41 +0300

    Update start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -2,14 +2,14 @@
 Concordance implementation starter
 """
 import os
-from main import read_from_file
+import main
 
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data = read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = ['the', 'of']
+    stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words.txt'))
     #  here goes your logic: calling methods from concordance.py
 
     tokens = main.tokenize(data)
