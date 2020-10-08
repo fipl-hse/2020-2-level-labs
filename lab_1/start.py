3cfbfa5b83e03ab22a44dc1d41f072ae82df972e
AnastasiaZheleznyakova <66490710+AnastasiaZheleznyakova@users.noreply.github.com>
2020-10-08 21:24:57 +0300

    Update start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -1,18 +1,26 @@
-"""
+ """
 Concordance implementation starter
 """
 
-from main import read_from_file
+from main import *
 import os
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data = read_from_file(os.path.join(current_dir, 'data.txt'))
-    stop_words = []
-
-    #  here goes your logic: calling methods from concordance.py
-
-    RESULT = None
+    stop_words = read_from_file(os.path.join(current_dir, 'stop_words.txt')).split()
+    tokens = tokenize(data)
+    tokens_no_stop_words = remove_stop_words(tokens, stop_words)
+    tokens_freq = calculate_frequencies(tokens_no_stop_words)
+    n = 10
+    top_n_words = get_top_n_words(tokens_freq, n)
+    print('top', n, 'words:', top_n_words)
+    concordance = get_concordance(tokens, 'point', 2, 2)
+    print('concordance of the word "point:"',concordance)
+    adjacent_words = get_adjacent_words(tokens_no_stop_words, 'point', 2, 2)
+    print('adjacent words of the word "point"', adjacent_words)
+    write_to_file(os.path.join(current_dir, 'adjacent_king.txt'), adjacent_words)
+    RESULT = contexts_no_stop_words
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT == [(), ()], 'Concordance not working'
