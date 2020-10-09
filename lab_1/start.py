900d36fb7d004499e65b74662b89ff67b39ea071
egorova-ai19-2 <nastyaegorovasunflower@gmail.com>
2020-10-09 18:04:00 +0300

    modified start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -3,17 +3,38 @@ Concordance implementation starter
 """
 
 import os
-
-from main import read_from_file, tokenize, sort_concordance
+import main
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    data = read_from_file(os.path.join(current_dir, 'data.txt'))
-    tokens = tokenize(data)
+    data = main.read_from_file(os.path.join(current_dir, 'data.txt'))
+    stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words.txt'))
+    stop_words = stop_words.split()
 
     #  here goes your logic: calling methods from concordance.py
-    sorted_concordance = sort_concordance(tokens, 'fitness', 1, 1, True)
+    tokens = main.tokenize(data)
+    print(tokens[:30])
+
+    tokens_clear = main.remove_stop_words(tokens, stop_words)
+    print(tokens_clear[:30])
+
+    freq_dict = main.calculate_frequencies(tokens[:100])
+    print(freq_dict)
+
+    top_7_words = main.get_top_n_words(freq_dict, 7)
+    print(top_7_words)
+
+    concordance = main.get_concordance(tokens,'apple', 3, 4)
+    print(concordance)
+
+    adjacent_words = main.get_adjacent_words(tokens, 'apple', 2, 3)
+    print(adjacent_words)
+
+    sorted_concordance = main.sort_concordance(tokens, 'apple', 1, 2, False)
+    print(sorted_concordance)
+
+    main.write_to_file('report.txt', sorted_concordance)
 
     RESULT = sorted_concordance
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
