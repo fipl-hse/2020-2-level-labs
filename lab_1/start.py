d0e708fa3b14ee66806fb615ef0aec6013023ec8
yukiyukiharu <yellowjaz@yandex.ru>
2020-10-09 23:03:18 +0300

    rewrite start.py for travis-ci

--- lab_1/start.py
+++ lab_1/start.py
@@ -13,24 +13,24 @@ if __name__ == '__main__':
 
     #  use data.txt file to test your program
     tokens = main.tokenize(data)
-    print('tokenize', tokens[:5])
+    print('tokenize', tokens[:4])
 
     tokens = main.remove_stop_words(tokens, stop_words)
-    print('remove_stop_words', tokens[:5])
+    print('remove_stop_words', tokens[:4])
 
     frequencies = main.calculate_frequencies(tokens[:5000])
     print('calculate_frequencies', frequencies[tokens[0]])
 
-    concordance = main.get_concordance(tokens, 'dog', 2, 0)
-    print('get_concordance', concordance[:5])
+    concordance = main.get_concordance(tokens, 'happy', 3, 0)
+    print('get_concordance', concordance[:4])
 
-    adjacent = main.get_adjacent_words(tokens, 'dog', 2, 0)
-    print('get_adjacent_words', adjacent[:5])
+    adjacent = main.get_adjacent_words(tokens, 'happy', 3, 0)
+    print('get_adjacent_words', adjacent[:4])
 
-    sorted_concordance = main.sort_concordance(tokens, 'dog', 2, 0, True)
-    print('sort_concordance', sorted_concordance[:5])
+    sorted_concordance = main.sort_concordance(tokens, 'happy', 3, 0, True)
+    print('sort_concordance', sorted_concordance[:4])
 
-    main.write_to_file('', sorted_concordance)
+    main.write_to_file('report.txt', sorted_concordance)
 
     RESULT = sorted_concordance
     assert RESULT, 'Concordance not working'
