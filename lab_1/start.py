d7b7b04eece7d3646564c8f86c1efb148d91ff57
mcgrozov <65295559+mcgrozov@users.noreply.github.com>
2020-10-10 15:08:21 +0300

    version 420_69_667

--- lab_1/start.py
+++ lab_1/start.py
@@ -14,18 +14,5 @@ if __name__ == '__main__':
     tokens = main.tokenize(text)
     print("tokens:", tokens[:15])
 
-    tokens_stop_words = main.remove_stop_words(tokens, stop_words)
-    print("tokens without stop words:", tokens_stop_words[:7])
-
-    frequencies = main.calculate_frequencies(tokens)
-    print('frequencies for the word: ', frequencies[tokens[2]])
-
-    top_words = main.get_top_n_words(freq_dict, 3)
-    print("top words:", top_words)
-
-    concordance = main.get_concordance(tokens, 'time', 1, 3)
-    print(" concordance:", concordance[:3])
-
-    RESULT = concordance[:3]
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT
