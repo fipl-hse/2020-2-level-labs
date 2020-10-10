b864e9232961c86516ca70df9bfb620292b410bf
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2020-10-10 09:17:52 +0300

    a few changes for start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -17,24 +17,32 @@ if __name__ == '__main__':
     clean_data = main.remove_stop_words(tokenized_data, stop_words)
 
     top_n = main.get_top_n_words(main.calculate_frequencies(clean_data), 13)
-    print(top_n[-1])
+    key_word = top_n[-1]
+    print(f'13th popular word: {key_word}. Let`s use if for further functions')
 
-    closest_words = main.get_adjacent_words(clean_data, top_n[-1], 3, 2)
+    closest_words = main.get_adjacent_words(clean_data, key_word, 3, 2)
     if len(closest_words) > 0:
-        print(f"Third words from the left and second words from the right for "
-              f"the word {top_n[-1]} (first 5 cases) are")
+        print(f"\nThird words from the left and second words from the right for "
+              f"the word '{key_word}' (first 5 cases) are")
         for adjacent_words in closest_words[:5]:
             print('\t', adjacent_words)
 
-    sorted_concordance_left = main.sort_concordance(clean_data, top_n[-1], 2, 2, True)
+    concordances = main.get_concordance(clean_data, key_word, 2, 2)
+    if len(concordances) > 0:
+        print(f"\nThe first three concordances (with 2 word on the left and 2 on the right)"
+              f"for the word '{key_word}' are")
+        for context in concordances[:3]:
+            print('\t', context)
+
+    sorted_concordance_left = main.sort_concordance(clean_data, key_word, 2, 2, True)
     if len(sorted_concordance_left) > 0:
-        print('Concordance sorted by the first left word (first 5 cases):')
+        print('\nConcordance sorted by the first left word (first 5 cases):')
         for concordance in sorted_concordance_left[:5]:
             print('\t', concordance)
 
-    sorted_concordance_right = main.sort_concordance(clean_data, top_n[-1], 2, 2, False)
+    sorted_concordance_right = main.sort_concordance(clean_data, key_word, 2, 2, False)
     if len(sorted_concordance_right) > 0:
-        print('Concordance sorted by the first right word (first 5 cases):')
+        print('\nConcordance sorted by the first right word (first 5 cases):')
         for concordance in sorted_concordance_right[:5]:
             print('\t', concordance)
 
