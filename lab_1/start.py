988b5631199fe34c01fbe99662ea66e8e1e82a53
mcgrozov <65295559+mcgrozov@users.noreply.github.com>
2020-10-10 10:48:43 +0300

    version 420_69_672

--- lab_1/start.py
+++ lab_1/start.py
@@ -1,5 +1,6 @@
 
 import main
+from main import read_from_file
 import os
 
 if __name__ == '__main__':
@@ -11,9 +12,6 @@ if __name__ == '__main__':
 
     #  here goes your logic: calling methods from concordance.py
     tokens = main.tokenize(text)
-    
-    RESULT = None
-    tokens = main.tokenize(text)
     print("tokens:", tokens[:15])
 
     tokens_stop_words = main.remove_stop_words(tokens, stop_words)
@@ -22,12 +20,12 @@ if __name__ == '__main__':
     frequencies = main.calculate_frequencies(tokens)
     print('frequencies for the word: ', frequencies[tokens[2]])
 
-    top_words = main.get_top_n_words(freq_dict, 3)
+    top_words = main.get_top_n_words(frequencies, 3)
     print("top words:", top_words)
 
     concordance = main.get_concordance(tokens, 'time', 1, 3)
     print(" concordance:", concordance[:3])
 
-RESULT = concordance[:3]
+    RESULT = concordance[:3]
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT == [['of', 'time', 'team', 'presented', 'a'], ['in', 'time', 'team', 'over', 'the'], ['the', 'time', 'a', 'stalin', 'directive']]
+    assert RESULT
