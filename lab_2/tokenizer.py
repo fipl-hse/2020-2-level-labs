2139831b6fe2ed07d8609dd5788d063c0e85295a
yudakovaolesya <ovyudakova@edu.hse.ru>
2020-10-12 23:08:02 +0300

    modified tokenizer

--- lab_2/tokenizer.py
+++ lab_2/tokenizer.py
@@ -1,5 +1,15 @@
 """
 Tokenizer out of lab_1 for usage in lab_2
 """
-
+def tokenize(text: str) -> tuple:
+    if not isinstance(text, str):
+        return ()
+    text = text.lower()
+    for symbol in text:
+        if symbol.isalpha() is False and symbol != ' ' and symbol != '\n':
+            text = text.replace(symbol, '')
+        elif symbol == '\n':
+            text = text.replace(symbol, ' ')
+    tokens = text.split()
+    return sentences_tuple
 
