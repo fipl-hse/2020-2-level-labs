e367e09fcda7a2353b1b57752180a4e1a0541850
mcgrozov <65295559+mcgrozov@users.noreply.github.com>
2020-10-14 20:37:19 +0300

    -

--- lab_2/tokenizer.py
+++ lab_2/tokenizer.py
@@ -1,19 +1,6 @@
 """
 Tokenizer out of lab_1 for usage in lab_2
 """
-def tokenize_by_lines(text: str) -> tuple:
-    if not isinstance(text, str):
-        return []
-    tokens = []
-    for token in text.lower().split():
-        variable = ''
-        for character in token:
-            if character.isalpha():
-                variable += character
-        if len(variable) != 0:
-            tokens.append(variable)
-    return tokens
-
 import re
 
 
