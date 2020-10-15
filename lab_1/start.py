f8fd14858ec8da95c2f69a42fbcfd46dba2a0af3
Kate Arbuzova <kate.arbuzova01@yandex.ru>
2020-10-15 17:06:04 +0300

    commit

--- lab_1/start.py
+++ lab_1/start.py
@@ -14,24 +14,31 @@ if __name__ == '__main__':
     #  here goes your logic: calling methods from concordance.py
     tokens = main.tokenize(data)
     print('tokens:', tokens[:10])
+    print('\n-----------------------------\n')
 
-    tokens = main.remove_stop_words(tokens, stop_words)
+    tokens = main.remove_stop_words(tokens, stop_words)   # old: 34 sec, new - 3.4 sec
     print('tokens without stop words:', tokens[:10])
+    print('\n-----------------------------\n')
 
-    frequencies = main.calculate_frequencies(tokens)
+    frequencies = main.calculate_frequencies(tokens)  # old: 116 sec, new: ~81 sec
     print('frequency for the first word:', frequencies[tokens[0]])
+    print('\n-----------------------------\n')
 
     top_10 = main.get_top_n_words(frequencies, 10)
     print('top 10 words:', top_10)
+    print('\n-----------------------------\n')
 
     concordance_cat = main.get_concordance(tokens, 'cat', 2, 3)
-    print('concordance for "cat":', concordance_cat[:5])
+    print('concordance for "cat", left = 2, right = 3:', concordance_cat[:5])
+    print('\n-----------------------------\n')
 
     adjacent_words_cat = main.get_adjacent_words(tokens, 'cat', 2, 3)
-    print('adjacent words for "cat":', adjacent_words_cat[:5])
+    print('adjacent words for "cat" left = 2, right = 3:', adjacent_words_cat[:5])
+    print('\n-----------------------------\n')
 
     sorted_concordance_cat = main.sort_concordance(tokens, 'cat', 2, 3, True)
-    print('sorted concordance for "cat":', sorted_concordance_cat[:5])
+    print('sorted concordance for "cat" left = 2, right = 3:', sorted_concordance_cat[:5])
+    print('\n-----------------------------\n')
 
     main.write_to_file('report.txt', sorted_concordance_cat)
 
