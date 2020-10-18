525788bfa0f0885c36ce3aa5806770e499aa1806
dmitry-uraev <uraev.dima@yandex.ru>
2020-10-18 03:02:26 +0300

    added tests for score 10 functions

--- lab_2/find_lcs_length_optimized_test.py
+++ lab_2/find_lcs_length_optimized_test.py
@@ -4,7 +4,7 @@ Tests find_lcs_optimized function
 
 import timeit
 import unittest
-from lab_2.main import find_lcs_length, find_lcs_length_optimized
+from lab_2.main import find_lcs_length, find_lcs_length_optimized, tokenize_big_file
 from lab_2.tokenizer import tokenize
 from memory_profiler import memory_usage
 
@@ -14,6 +14,62 @@ class FindLcsOptimizedTest(unittest.TestCase):
     Checks for find_lcs_optimized function
     """
 
+    def test_find_lcs_length_optimized_ideal_case(self):
+        """
+        Tests that find_lcs_length_optimized
+            works just fine and not fails with big text
+        """
+        sentence_tokens_first_text = tokenize_big_file('lab_2/data.txt')
+        sentence_tokens_second_text = tokenize_big_file('lab_2/data_2.txt')
+        plagiarism_threshold = 0.3
+        actual = find_lcs_length_optimized(tuple(sentence_tokens_first_text),
+                                           tuple(sentence_tokens_second_text),
+                                           plagiarism_threshold)
+        self.assertTrue(actual)
+
+    def test_find_lcs_length_optimized_quickest_time(self):
+        """
+        Tests that find_lcs_length_optimized
+            works faster than  time reference
+        """
+        reference = 9.101999999927557e-06 + (9.101999999927557e-06 * 0.1)
+        sentence_tokens_first_text = tokenize_big_file('lab_2/data.txt')
+        sentence_tokens_second_text = tokenize_big_file('lab_2/data_2.txt')
+        plagiarism_threshold = 0.3
+
+        start_time = timeit.default_timer()
+        find_lcs_length_optimized(sentence_tokens_first_text,
+                                  sentence_tokens_second_text,
+                                  plagiarism_threshold)
+        end_time = timeit.default_timer()
+        actual = end_time - start_time
+
+        print(f"Actual find_lcs_length_optimized function running time is: {actual}")
+        print(f"Reference find_lcs_length_optimized function running time is: {reference}")
+        self.assertTrue(reference >= actual)
+
+    def test_find_lcs_length_optimized_lowest_memory(self):
+        """
+        Tests that find_lcs_length_optimized
+            works efficiently than given memory reference
+        """
+        reference = 61.53515625 + (61.53515625 * 0.1)
+        sentence_tokens_first_text = tokenize_big_file('lab_2/data.txt')
+        sentence_tokens_second_text = tokenize_big_file('lab_2/data_2.txt')
+        plagiarism_threshold = 0.3
+
+        actual_memory = memory_usage((find_lcs_length_optimized,
+                                      (sentence_tokens_first_text,
+                                       sentence_tokens_second_text,
+                                       plagiarism_threshold)),
+                                     interval=2)
+        actual = sum(actual_memory)/len(actual_memory)
+
+        print(f'Actual find_lcs_length_optimized function memory consuming is: {actual}')
+        print(f'Reference find_lcs_length_optimized function memory consuming is: {reference}')
+        self.assertTrue(reference >= actual)
+
+    @unittest.skip
     def test_find_lcs_length_optimized_works_faster(self):
         """
         Tests find_lcs_length_optimized function
--- lab_2/tokenize_big_file_test.py
+++ lab_2/tokenize_big_file_test.py
@@ -3,6 +3,7 @@ Tests tokenize_big_file function
 """
 
 import unittest
+import timeit
 from memory_profiler import memory_usage
 from lab_2.main import tokenize_big_file, tokenize_by_lines
 
@@ -11,6 +12,43 @@ class TokenizeBigFileTest(unittest.TestCase):
     """
     Checks for tokenize_big_file function
     """
+
+    def test_tokenize_big_file_ideal_case(self):
+        """
+        Tests that tokenize_big_fie
+            works just fine and not fails with big text
+        """
+        actual = tokenize_big_file('lab_2/data.txt')
+        self.assertTrue(actual)
+
+    def test_tokenize_big_file_quickest_time(self):
+        """
+        Tests that tokenize_big_file
+            works faster than  time reference
+        """
+        start_time = timeit.default_timer()
+        tokenize_big_file('lab_2/data.txt')
+        end_time = timeit.default_timer()
+
+        expected = 0.876659336000003 + (0.876659336000003 * 0.1)
+        actual = end_time - start_time
+        print(f'Actual tokenize_big_file function running time is: {actual}')
+        print(f'Reference tokenize_big_file function time is: {expected}')
+        self.assertTrue(expected >= actual)
+
+    def test_tokenize_big_file_lowest_memory(self):
+        """
+        Tests that tokenize_big_file
+            works efficiently than given memory reference
+        """
+        expected = 64.22135416666667 + (64.22135416666667 * 0.1)
+        actual_memory = memory_usage((tokenize_big_file, ('lab_2/data.txt',)),
+                                     interval=2)
+        actual = sum(actual_memory)/len(actual_memory)
+
+        print(f'Actual tokenize_big_file function memory consuming is: {actual}')
+        print(f'Reference tokenize_big_file function memory consuming is: {expected}')
+        self.assertTrue(expected >= actual)
 
     @unittest.skip
     def test_tokenize_big_file_ideal(self):
