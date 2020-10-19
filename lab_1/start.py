ddedcdebc24318e8ad2467cefc9c82b615e6a2d8
psilyusheva <ilyushewa@yandex.ru>
2020-10-19 20:11:55 +0300

    start

--- lab_1/start.py
+++ lab_1/start.py
@@ -4,20 +4,14 @@ Concordance implementation starter
 
 import os
 import main
-<<<<<<< HEAD
-=======
 
->>>>>>> upstream/master
 
 if __name__ == '__main__':
     #  use data.txt file to test your program
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data = main.read_from_file(os.path.join(current_dir, 'data.txt'))
     stop_words = main.read_from_file(os.path.join(current_dir, 'stop_words.txt')).split('\n')
-<<<<<<< HEAD
-=======
 
->>>>>>> upstream/master
     #  here goes your logic: calling methods from concordance.py
     tokenized_data = main.tokenize(data)
     clean_data = main.remove_stop_words(tokenized_data, stop_words)
@@ -52,36 +46,6 @@ if __name__ == '__main__':
         for concordance in sorted_concordance_right[:5]:
             print('\t', concordance)
 
-<<<<<<< HEAD
-    tokens = main.tokenize(data)
-    print(f"first 7 tokens: {tokens[:7]}")
-
-    tokens = main.remove_stop_words(tokens, stop_words)
-    print(f"first 7 tokens without stop words: {tokens[:7]}")
-
-    frequencies = main.calculate_frequencies(tokens)
-    print(f"frequency for the first word: {tokens[0]}")
-
-    top_5 = main.get_top_n_words(frequencies, 3)
-    print(f"top 5 words: {top_5}")
-
-    mountain_concordance = main.get_concordance(tokens, 'mountain', 2, 3)
-    print(f"concordance for 'mountain': {mountain_concordance}")
-
-    mountain_adjacent_words = main.get_adjacent_words(tokens, 'mountain', 1, 1)
-    print(f"adjacent words for 'mountain': {mountain_adjacent_words}")
-
-    mountain_sorted_concordance = main.sort_concordance(tokens, 'mountain', 1, 2, True)
-    print(f"sorted concordance for 'mountain': {mountain_sorted_concordance}")
-
-    main.write_to_file('report.txt', mountain_sorted_concordance)
-    RESULT = mountain_sorted_concordance[:3]
-
-    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT == [['a', 'mountain', 'range', 'running'], ['a', 'mountain', 'killing', 'people'],
-                      ['ascending', 'mountain', 'sermon', 'mount']], 'Concordance not working'
-=======
     RESULT = sorted_concordance_left
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Concordance not working'
->>>>>>> upstream/master
