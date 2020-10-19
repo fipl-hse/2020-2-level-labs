534409fb6af8b621774359777e9111363f327904
dmitry-uraev <uraev.dima@yandex.ru>
2020-10-19 15:01:09 +0300

    10 score functions with references

--- lab_2/tokenize_big_file_test.py
+++ lab_2/tokenize_big_file_test.py
@@ -20,6 +20,8 @@ class TokenizeBigFileTest(unittest.TestCase):
         """
         reference_len = 36970273
         actual = tokenize_big_file('lab_2/data.txt')
+        print(f"Actual tokenize_big_file function length: {len(actual)}")
+        print(f"Reference tokenize_big_file function length: {reference_len}")
         self.assertTrue(actual)
         self.assertEqual(reference_len, len(actual))
 
@@ -32,7 +34,7 @@ class TokenizeBigFileTest(unittest.TestCase):
         tokenize_big_file('lab_2/data.txt')
         end_time = timeit.default_timer()
 
-        expected = 0.876659336000003 * 1.1
+        expected = 17.614083574000006 * 1.1
         actual = end_time - start_time
         print(f'Actual tokenize_big_file function running time is: {actual}')
         print(f'Reference tokenize_big_file function time is: {expected}')
@@ -43,7 +45,7 @@ class TokenizeBigFileTest(unittest.TestCase):
         Tests that tokenize_big_file
             works efficiently than given memory reference
         """
-        expected = 64.22135416666667 * 1.1
+        expected = 224.9296875 * 1.1
         actual_memory = memory_usage((tokenize_big_file, ('lab_2/data.txt',)),
                                      interval=2)
         actual = sum(actual_memory)/len(actual_memory)
