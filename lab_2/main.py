f62a1c3c2ec8730b336880bed46c0c5a5751648d
egorova-ai19-2 <nastyaegorovasunflower@gmail.com>
2020-10-20 22:10:08 +0300

    main tests

--- lab_2/main.py
+++ lab_2/main.py
@@ -38,7 +38,6 @@ def create_zero_matrix(rows: int, columns: int) -> list:
     e.g. rows = 2, columns = 2
     --> [[0, 0], [0, 0]]
     """
-
     is_rows_columns_correct = ((not isinstance(rows, bool)) and (not isinstance(columns, bool)) and
                                isinstance(rows, int) and isinstance(columns, int) and (rows > 0) and (columns > 0))
     if not is_rows_columns_correct:
