f68ace9822cfc1d3d6d6d505f21f0370dba30911
makar-fedorov <65069232+makar-fedorov@users.noreply.github.com>
2020-10-21 17:53:28 +0500

    start is modified

--- lab_2/start.py
+++ lab_2/start.py
@@ -1,3 +1,16 @@
 """
 Longest common subsequence implementation starter
 """
+
+import main
+
+original_text_tokens = (('i', 'have', 'a', 'cat'),
+                                ('its', 'body', 'is', 'covered', 'with', 'bushy', 'white', 'fur'))
+suspicious_text_tokens = (('i', 'have', 'a', 'cat'),
+                                  ('its', 'body', 'is', 'covered', 'with', 'shiny', 'black', 'fur'))
+
+accumulated_diff_stats = main.accumulate_diff_stats(original_text_tokens, suspicious_text_tokens)
+actual = main.create_diff_report(original_text_tokens, suspicious_text_tokens, accumulated_diff_stats).split()
+RESULT = main.create_diff_report(original_text_tokens, suspicious_text_tokens, accumulated_diff_stats).split()
+
+assert RESULT
