358e1ac521295d61503e37068c13ee50b8aaad2d
WhiteJaeger <logerk3@gmail.com>
2020-10-21 19:34:46 +0300

    reset to master

--- lab_1/main.py
+++ lab_1/main.py
@@ -20,18 +20,6 @@ def tokenize(text: str) -> list:
     text_output = re.sub('[^a-z \n]', '', text.lower()).split()
     return text_output
 
-    if not isinstance(text, str):
-        return []
-
-    text = text.lower()
-    text = text.replace('\n', ' ')
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-               'v', 'w', 'x', 'y', 'z']
-    new_text = ''
-    for symbol in text:
-        if symbol in letters or symbol == ' ':
-            new_text += symbol
-    return new_text.split()
 
 def remove_stop_words(tokens: list, stop_words: list) -> list:
     """
@@ -47,16 +35,6 @@ def remove_stop_words(tokens: list, stop_words: list) -> list:
         return []
     list_words = [word for word in tokens if word not in stop_words]
     return list_words
-
-    if not isinstance(tokens, list):
-        return []
-    if not isinstance(stop_words, list):
-        return tokens
-
-    for word in stop_words:
-        while word in tokens:
-            tokens.remove(word)
-    return tokens
 
 
 def calculate_frequencies(tokens: list) -> dict:
@@ -75,16 +53,6 @@ def calculate_frequencies(tokens: list) -> dict:
     dict_freq = {word: tokens.count(word) for word in set_words}
     return dict_freq
 
-    if not isinstance(tokens, list):
-        return {}
-
-    freq_dict = {}
-    for token in tokens:
-        if isinstance(token, str):
-            frequency = tokens.count(token)
-            freq_dict[token] = frequency
-    return freq_dict
-
 
 def get_top_n_words(freq_dict: dict, top_n: int) -> list:
     """
@@ -100,27 +68,6 @@ def get_top_n_words(freq_dict: dict, top_n: int) -> list:
         return []
     list_output = sorted(freq_dict, key=freq_dict.get, reverse=True)
     return list_output[:top_n]
-
-    if not isinstance(freq_dict, dict) or not isinstance(top_n, int):
-        return []
-
-    # the number of words (from the most popular)
-    values_of_words = []
-    for value in freq_dict.values():
-        if value in values_of_words:
-            continue
-        values_of_words.append(value)
-    values_of_words.sort()
-    values_of_words = values_of_words[::-1]
-    # top_values
-    top = values_of_words[:top_n]
-    #
-    top_words = []  # can be words with the same frequency
-    for element in top:
-        for word, frequency in freq_dict.items():
-            if element == frequency:
-                top_words.append(word)
-    return top_words[:top_n]
 
 
 def get_concordance(tokens: list, word: str, left_context_size: int, right_context_size: int) -> list:
@@ -167,31 +114,6 @@ def get_concordance(tokens: list, word: str, left_context_size: int, right_conte
     else:
         list_output = [tokens[ind - left_context_size:ind + 1 + right_context_size] for ind in indexes]
     return list_output
-
-    check_tokens = isinstance(tokens, list)
-    check_word = isinstance(word, str)
-    check_left_context_size = isinstance(left_context_size, int)
-    check_right_context_size = isinstance(right_context_size, int)
-
-    if not check_tokens or not check_word:
-        return []
-    if (not check_left_context_size and not check_right_context_size) or (left_context_size < 1 \
-            and right_context_size < 1):
-        return []
-    if isinstance(left_context_size, bool) or isinstance(right_context_size, bool):
-        return []
-
-    if left_context_size < 1:
-        left_context_size = 0
-    elif right_context_size < 1:
-        right_context_size = 0
-
-    concordance = []
-    for index, token in enumerate(tokens):
-        if token == word:
-            mini_concordance = tokens[index - left_context_size: index + right_context_size + 1]
-            concordance.append(mini_concordance)
-    return concordance
 
 
 def get_adjacent_words(tokens: list, word: str, left_n: int, right_n: int) -> list:
