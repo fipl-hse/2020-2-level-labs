4a925a1b57c390c14fc4aeaab261d8b5e0b63f7b
Yano Erina <blackberrycupcake@yandex.ru>
2020-10-22 18:40:25 +0300

    Minor changes in start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -9,36 +9,45 @@ from main import find_lcs
 from main import calculate_plagiarism_score
 from main import calculate_text_plagiarism_score
 
-text1 = 'Once upon a time there lived a cat named Mary.'
-text2 = 'Not a long time ago there lived an owl named Rena.'
+text1 = 'I have a cat.'
+text2 = 'I have a cat.'
 
-tokenized_text1 = tokenize(text1)
+tokenized_text1 = tokenize_by_lines(text1)
 print('Кортеж предложений с токенами из текста 1:', tokenized_text1)
-tokenized_text2 = tokenize(text2)
+tokenized_text2 = tokenize_by_lines(text2)
 print('Кортеж предложений с токенами из текста 2: ', tokenized_text2)
 
-zero_matrix = create_zero_matrix(len(tokenized_text1), len(tokenized_text2))
+original = tokenized_text1[0]
+suspicious = tokenized_text2[0]
+
+zero_matrix = create_zero_matrix(len(original), len(suspicious))
 print('Матрица из нулей: ')
 for i in range(len(zero_matrix)):
     for j in range(len(zero_matrix[0])):
         print(zero_matrix[i][j], end=' ')
     print()
 
-lcs_matrix = fill_lcs_matrix(tokenized_text1, tokenized_text2)
-print('Longest common subsequence matrix: ', lcs_matrix)
+lcs_matrix = fill_lcs_matrix(original, suspicious)
+print('Longest common subsequence matrix: ')
 for i in range(len(lcs_matrix)):
     for j in range(len(lcs_matrix[0])):
         print(lcs_matrix[i][j], end=' ')
     print()
 
-length = find_lcs_length(tokenized_text1, tokenized_text2, 0.3)
+length = find_lcs_length(original, suspicious, 0.3)
 print('Length of the longest common subsequence: ', length)
 
-lcs = find_lcs(tokenized_text1, tokenized_text2, lcs_matrix)
+lcs = find_lcs(original, suspicious, lcs_matrix)
 print('The longest common subsequence itself: ', lcs)
 
-plagiarism_score = calculate_plagiarism_score(length, tokenized_text2)
+plagiarism_score = calculate_plagiarism_score(length, suspicious)
 print('the plagiarism score: ', plagiarism_score)
 
 score = calculate_text_plagiarism_score(tokenized_text1, tokenized_text2, 0.3)
 print(score)
+
+expected = ('i', 'have', 'a', 'cat')
+actual = find_lcs(original, suspicious, lcs_matrix)
+RESULT = actual
+# DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
+assert RESULT == expected, 'Program works wrong'
