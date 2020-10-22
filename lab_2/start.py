788b32f2ccbfe240cdf7df74090113e960488413
TroublemakerSasha <sashalysuenko@mail.ru>
2020-10-22 19:43:03 +0300

    status

--- lab_2/start.py
+++ lab_2/start.py
@@ -1,4 +1,20 @@
 """
 Longest common subsequence implementation starter
 """
+import main
+
+original_text_tokens = (('I', 'want', 'a', 'dog'),
+                                ('it', 'is', 'a', 'doberman', 'with', 'smooth', 'brown', 'fur'))
+suspicious_text_tokens = (('I', 'want', 'a', 'dog'),
+                                  ('it', 'is', 'a', 'dachshund', 'with', 'smooth', 'black', 'fur'))
+
+accumulated_diff_stats = main.accumulate_diff_stats(original_text_tokens, suspicious_text_tokens)
+actual = main.create_diff_report(original_text_tokens, suspicious_text_tokens, accumulated_diff_stats).split()
+RESULT = main.create_diff_report(original_text_tokens, suspicious_text_tokens, accumulated_diff_stats).split()
+
+assert RESULT == ['-', 'I', 'want', 'a', 'dog', '+', 'I, 'want', 'a', 'dog', 'lcs', '=', '4,', 'plagiarism',
+           '=', '100.0%', '-', 'it', 'is', 'a', '|','doberman', '|','with',  'smooth', '|','brown', '|', 'fur',
+           '+', 'it', 'is', 'a', '|','dachshund', '|', 'with', 'smooth', '|', 'black', '|', 'fur', 'lcs', '=',
+           '6,', 'plagiarism', '=', '75.0%', 'Text', 'average', 'plagiarism', '(words):', '87.5%'], 'diff_report ' \
+                                                                                                    'not working'
 
