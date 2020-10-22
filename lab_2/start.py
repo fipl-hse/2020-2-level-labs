372dc7b4a55068586ddffe0be93b451d644a379c
makar-fedorov <65069232+makar-fedorov@users.noreply.github.com>
2020-10-22 13:18:23 +0500

    srart is modified

--- lab_2/start.py
+++ lab_2/start.py
@@ -13,10 +13,8 @@ accumulated_diff_stats = main.accumulate_diff_stats(original_text_tokens, suspic
 actual = main.create_diff_report(original_text_tokens, suspicious_text_tokens, accumulated_diff_stats).split()
 RESULT = main.create_diff_report(original_text_tokens, suspicious_text_tokens, accumulated_diff_stats).split()
 
-
-RESULT == ['-', 'i', 'have', 'a', 'cat', '+', 'i', 'have', 'a', 'cat', 'lcs', '=', '4,', 'plagiarism',
+assert RESULT == ['-', 'i', 'have', 'a', 'cat', '+', 'i', 'have', 'a', 'cat', 'lcs', '=', '4,', 'plagiarism',
            '=', '100.0%', '-', 'its', 'body', 'is', 'covered', 'with', '|', 'bushy', 'white', '|', 'fur',
            '+', 'its', 'body', 'is', 'covered', 'with', '|', 'shiny', 'black', '|', 'fur', 'lcs', '=',
-           '6,', 'plagiarism', '=', '75.0%', 'Text', 'average', 'plagiarism', '(words):', '87.5%']
-
-assert RESULT
+           '6,', 'plagiarism', '=', '75.0%', 'Text', 'average', 'plagiarism', '(words):', '87.5%'], 'diff_report ' \
+                                                                                                    'not working'
