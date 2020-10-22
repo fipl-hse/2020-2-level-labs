67176e219f0d9297224390c3cef719ae7f564948
Ekaterina <you@example.com>
2020-10-22 20:07:29 +0300

    'start'

--- lab_2/start.py
+++ lab_2/start.py
@@ -15,18 +15,18 @@ if __name__ == '__main__':
     original_text_tokens = tokenize_by_lines(original_text)
     suspicious_text_tokens = tokenize_by_lines(suspicious_text)
 
-    lcs_length = main.find_lcs_length(original_text_tokens, suspicious_text_tokens, plagiarism_threshold=0.3)
+    lcs_length = find_lcs_length(original_text_tokens, suspicious_text_tokens, plagiarism_threshold=0.3)
     print("Length of the longest common subsequence:", lcs_length)
 
-    matrix = main.fill_lcs_matrix(original_text_tokens, suspicious_text_tokens)
+    matrix = fill_lcs_matrix(original_text_tokens, suspicious_text_tokens)
 
-    lcs = main.find_lcs(original_text_tokens, suspicious_text_tokens, matrix)
+    lcs = find_lcs(original_text_tokens, suspicious_text_tokens, matrix)
     print("The longest common subsequence:", lcs)
 
-    plagiarism_score = main.calculate_plagiarism_score(lcs_length, suspicious_text_tokens)
+    plagiarism_score = calculate_plagiarism_score(lcs_length, suspicious_text_tokens)
     print("The plagiarism score:", plagiarism_score)
 
-    text_plagiarism_score = main.calculate_text_plagiarism_score(original_text_tokens, suspicious_text_tokens, plagiarism_score)
+    text_plagiarism_score = calculate_text_plagiarism_score(original_text_tokens, suspicious_text_tokens, plagiarism_score)
     print("Text plagiarism score:", text_plagiarism_score)
 
     RESULT = text_plagiarism_score
