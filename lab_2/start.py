e10ebca30d57251e620336ecb709d4256bdcfae5
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-10-23 10:56:15 +0300

    lints

--- lab_2/start.py
+++ lab_2/start.py
@@ -1,15 +1,16 @@
 """
 Longest common subsequence implementation starter
 """
-from main import *
-
+from main import tokenize_by_lines, create_zero_matrix, fill_lcs_matrix
+from main import find_lcs_length, calculate_plagiarism_score, calculate_text_plagiarism_score
+from main import find_lcs, create_diff_report, find_diff_in_sentence, accumulate_diff_stats
 
 if __name__ == '__main__':
-    original = 'I have a nice dog.'
-    suspicious = 'I have a cute cat.'
+    ORIGINAL = 'I have a nice dog.'
+    SUSPICIOUS = 'I have a cute cat.'
 
-    tokenized_original = tokenize_by_lines(original)
-    tokenized_suspicious = tokenize_by_lines(suspicious)
+    tokenized_original = tokenize_by_lines(ORIGINAL)
+    tokenized_suspicious = tokenize_by_lines(SUSPICIOUS)
     print(f"Original text tokens: {tokenized_original}\nSuspicious text tokens: {tokenized_suspicious}\n")
 
     zero_matrix_first = create_zero_matrix(len(tokenized_original), len(tokenized_suspicious))
@@ -34,8 +35,8 @@ if __name__ == '__main__':
     info = accumulate_diff_stats(tokenized_original, tokenized_suspicious)
     print(f"info about sentences in text\n{info}\n")
 
-    report = create_diff_report(tokenized_original, tokenized_suspicious, info)
-    print(f"The report for two texts:\n{report}")
+    REPORT = create_diff_report(tokenized_original, tokenized_suspicious, info)
+    print(f"The report for two texts:\n{REPORT}")
 
     RESULT = tokenized_original
 
