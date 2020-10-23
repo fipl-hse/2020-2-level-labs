0f2541e8b4f9584dac9b311d6650aa84a5c8764c
mcgrozov <65295559+mcgrozov@users.noreply.github.com>
2020-10-23 20:18:41 +0300

    version 10

--- lab_2/main.py
+++ lab_2/main.py
@@ -36,7 +36,7 @@ def create_zero_matrix(rows: int, columns: int) -> list:
     --> [[0, 0], [0, 0]]
     """
     criterion = [isinstance(rows,int), isinstance(columns,int), not isinstance(rows,bool),
-          not isinstance(columns,bool), (rows > 0), (columns > 0)]
+          not isinstance(columns,bool), rows > 0, columns > 0]
     if all (criterion):
         i = 0
         zero_matrix = [[0 for i in range(columns)] for j in range(rows)]
