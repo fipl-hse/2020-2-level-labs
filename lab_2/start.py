809ae656e1927a1e75c6752a2c267a34c678b7c3
galkinaksenia <ksun2001@rambler.ru>
2020-10-23 18:11:42 +0300

    changed start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -5,14 +5,15 @@ import lab_2.main
 
 
 if __name__ == '__main__':
-    sentence_tokens_first_text = lab_2.main.tokenize_big_file('lab_2/data.txt')[:20000]
-    sentence_tokens_second_text = lab_2.main.tokenize_big_file('lab_2/data_2.txt')[:20000]
-    PLAGIARISM = 0.0001
-    lcs_length = lab_2.main.find_lcs_length_optimized(sentence_tokens_first_text,
-                                                      sentence_tokens_second_text,
-                                                      PLAGIARISM)
-    actual = lcs_length / len(sentence_tokens_second_text)
-    EXPECTED = 2580 / 20000
+    original_text_tokens = (('i', 'have', 'a', 'cat'),
+                            ('its', 'body', 'is', 'covered', 'with', 'bushy', 'white', 'fur'))
+    suspicious_text_tokens = (('i', 'have', 'a', 'cat'),
+                              ('its', 'body', 'is', 'covered', 'with', 'shiny', 'black', 'fur'))
+    accumulated_diff_stats = lab_2.main.accumulate_diff_stats(original_text_tokens, suspicious_text_tokens)
+
+    expected = open('lab_2/diff_report_example.txt', 'r', errors='coerce').read().split()
+    actual = lab_2.main.create_diff_report(original_text_tokens, suspicious_text_tokens, accumulated_diff_stats).split()
+
     RESULT = actual
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT == EXPECTED, 'Results differ'
+    assert RESULT == expected, 'Results differ'
