31b3d8bdb5f6259b8509705c48f304d49bf1e21c
mcgrozov <65295559+mcgrozov@users.noreply.github.com>
2020-10-23 21:58:30 +0300

    version 12 update 2

--- lab_2/main.py
+++ lab_2/main.py
@@ -85,7 +85,10 @@ def find_lcs_length(first_sentence_tokens: tuple, second_sentence_tokens: tuple,
     if len(first_sentence_tokens) == 0 or len(second_sentence_tokens) == 0:
         return 0
     lcs_matrix = fill_lcs_matrix(first_sentence_tokens, second_sentence_tokens)
-    lcs_length = lcs_matrix[-1][-1]
+    if len(first_sentence_tokens) > len(second_sentence_tokens):
+        lcs_length = max(lcs_matrix[len(second_sentence_tokens) - 1])
+    else:
+        lcs_length = max(lcs_matrix[-1])
     if lcs_length / len(second_sentence_tokens) < plagiarism_threshold:
         return 0
     return lcs_length
