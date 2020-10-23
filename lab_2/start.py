ef1772f7ba6f99f70b654ccfee05829b6b729b9b
vchistyakova <65254510+vchistyakova@users.noreply.github.com>
2020-10-23 22:43:25 +0300

    add start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -1,4 +1,26 @@
 """
 Longest common subsequence implementation starter
 """
+from lab_2.main import tokenize_by_lines,create_zero_matrix, fill_lcs_matrix, \
+    find_lcs_length, find_lcs, calculate_plagiarism_score
 
+original_text = '''
+Once upon a time a little cat Meow walked down the street.
+She met her friend, a giant dog Bow.
+And they continued their walk together.
+'''
+ suspicious_text = '''
+ Once upon a time a giant dog Bow walked down the street.
+ He met his friend, a little cat Meow.
+ And they went to the theatre.
+ '''
+
+ tuple_original = tokenize_by_lines(original_text)
+ tuple_suspicious = tokenize_by_lines(suspicious_text)
+ lcs_matrix = fill_lcs_matrix(tuple_original, tuple_suspicious)
+ lcs_length = find_lcs_length(tuple_original,tuple_suspicious,plagiarism_threshold)
+ max_subsequence = find_lcs(tuple_original, tuple_suspicious, lcs_matrix)
+ plagiarism = calculate_plagiarism_score(lcs_length, tuple_suspicious)
+
+ RESULT = plagiarism
+ assert RESULT, 'Calculate plagiarism score not working'
