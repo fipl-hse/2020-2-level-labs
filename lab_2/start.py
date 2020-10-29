2b513df3837ddc5d8fa6cd87c7ef239099999531
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2020-10-29 16:27:56 +0300

    fixing_start

--- lab_2/start.py
+++ lab_2/start.py
@@ -2,3 +2,13 @@
 """
 Longest common subsequence implementation starter
 """
+import main
+
+
+original_text_tokens = (('i', 'have', 'a', 'cat'),
+                                ('its', 'body', 'is', 'covered', 'with', 'bushy', 'white', 'fur'))
+suspicious_text_tokens = (('i', 'have', 'a', 'cat'),
+                                  ('its', 'body', 'is', 'covered', 'with', 'shiny', 'black', 'fur'))
+
+RESULT = main.create_diff_report
+assert RESULT
