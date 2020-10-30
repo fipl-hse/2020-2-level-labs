499f7b451e7ac7b85d2de05409189088c11c53db
ashirka <66332800+ashirka@users.noreply.github.com>
2020-10-30 19:52:22 +0300

    Update start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -1,17 +1,17 @@
 """
 Longest common subsequence implementation starter
 """
+
 from main import tokenize_by_lines, accumulate_diff_stats, create_diff_report
 
-TEXT_ORIGINAL = '''i like small cats'''
+ORIGINAL_TEXT = "i like small cats"
 
-TEXT_SUSPICIOUS = '''i prefer small dogs'''
+SUSPICIOUS_TEXT = "i prefer small dogs"
 
-original_tuple = tokenize_by_lines(TEXT_ORIGINAL)
-suspicious_tuple = tokenize_by_lines(TEXT_SUSPICIOUS)
+original = tokenize_by_lines(ORIGINAL_TEXT)
+suspicious = tokenize_by_lines(SUSPICIOUS_TEXT)
 
-diff_stats = accumulate_diff_stats(original_tuple, suspicious_tuple)
+diff_stats = accumulate_diff_stats(original, suspicious)
 
-RESULT = create_diff_report(original_tuple, suspicious_tuple, diff_stats)
-
-assert RESULT, 'LCS_length not working'
+RESULT = create_diff_report(original, suspicious, diff_stats)
+assert RESULT, "LCS_length not working"
