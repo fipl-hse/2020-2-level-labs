b6eb48ba8a1c9dc7a5411701ac7c83a05611b612
ashirka <66332800+ashirka@users.noreply.github.com>
2020-10-30 15:24:13 +0300

    Update tokenize_by_lines_test.py

--- lab_2/tokenize_by_lines_test.py
+++ lab_2/tokenize_by_lines_test.py
@@ -1,55 +1,40 @@
 """
-Tests tokenize_big_file function
+Tests tokenize_by_lines function
 """
 
 import unittest
-import timeit
-from memory_profiler import memory_usage
-from lab_2.main import tokenize_big_file
+from lab_2.main import tokenize_by_lines
 
 
-class TokenizeBigFileTest(unittest.TestCase):
+class TokenizeByLinesTest(unittest.TestCase):
     """
-    Checks for tokenize_big_file function
+    Checks for tokenize_bby_lines function
     """
 
-    def test_tokenize_big_file_ideal_case(self):
+    def test_tokenize_by_lines_ideal(self):
         """
-        Tests that tokenize_big_fie
-            works just fine and not fails with big text
+        Tests that tokenize_by_lines function
+            can handle ...
         """
-        reference_len = 36815824
-        actual = tokenize_big_file('lab_2/data.txt')
-        print(f"Actual tokenize_big_file function length: {len(actual)}")
-        print(f"Reference tokenize_big_file function length: {reference_len}")
-        self.assertTrue(actual)
-        self.assertEqual(reference_len, len(actual))
+        text = 'I have a cat.\nHis name is Bruno'
 
-    def test_tokenize_big_file_quickest_time(self):
-        """
-        Tests that tokenize_big_file
-            works faster than  time reference
-        """
-        start_time = timeit.default_timer()
-        tokenize_big_file('lab_2/data.txt')
-        end_time = timeit.default_timer()
+        expected = (('i', 'have', 'a', 'cat'), ('his', 'name', 'is', 'bruno'))
+        actual = tokenize_by_lines(text)
+        self.assertEqual(expected, actual)
 
-        expected = 17.614083574000006 * 1.1
-        actual = end_time - start_time
-        print(f'Actual tokenize_big_file function running time is: {actual}')
-        print(f'Reference tokenize_big_file function time is: {expected}')
-        self.assertGreater(expected, actual)
-
-    def test_tokenize_big_file_lowest_memory(self):
+    def test_tokenize_by_lines_more_lines(self):
         """
-        Tests that tokenize_big_file
-            works efficiently than given memory reference
+        Tests that tokenize_by_lines function
+            can handle ...
         """
-        expected = 224.9296875 * 1.1
-        actual_memory = memory_usage((tokenize_big_file, ('lab_2/data.txt',)),
-                                     interval=2)
-        actual = sum(actual_memory)/len(actual_memory)
+        text = """
+        I have a cat.\nHis name is Bruno.\n
+        He is white and beautiful.\n
+        """
 
-        print(f'Actual tokenize_big_file function memory consuming is: {actual}')
-        print(f'Reference tokenize_big_file function memory consuming is: {expected}')
-        self.assertGreater(expected, actual)
+        expected = (('i', 'have', 'a', 'cat'),
+                    ('his', 'name', 'is', 'bruno'),
+                    ('he', 'is', 'white', 'and', 'beautiful'))
+
+        actual = tokenize_by_lines(text)
+        self.assertEqual(expected, actual)
