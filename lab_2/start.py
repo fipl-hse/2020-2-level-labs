89de7da972d002b4c28d6c502a53dcaa4c8d53e3
ashirka <66332800+ashirka@users.noreply.github.com>
2020-10-30 15:44:31 +0300

    Update start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -1,30 +1,13 @@
 """
 Longest common subsequence implementation starter
 """
-<<<<<<< Updated upstream
 import lab_2.main
 
-if __name__ == '__main__':
-    original_text_tokens = (('i', 'have', 'a', 'cat'),
-                            ('its', 'body', 'is', 'covered', 'with', 'bushy', 'white', 'fur'))
-    suspicious_text_tokens = (('i', 'have', 'a', 'cat'),
-                              ('its', 'body', 'is', 'covered', 'with', 'shiny', 'black', 'fur'))
-    accumulated_diff_stats = lab_2.main.accumulate_diff_stats(original_text_tokens, suspicious_text_tokens)
+original = 'the cat is sleeping'
+suspicious = 'the little cat is running'
 
-    expected = open('lab_2/diff_report_example.txt', 'r', errors='coerce').read().split()
-    actual = lab_2.main.create_diff_report(original_text_tokens, suspicious_text_tokens, accumulated_diff_stats).split()
-
-    RESULT = actual
-    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT == expected, 'Results differ'
-=======
-import main
-
-ORIGINAL_TEXT = 'the cat is sleeping'
-SUSPICIOUS_TEXT = 'the little cat is running'
-
-original_tokens = main.tokenize_by_lines(ORIGINAL_TEXT)
-suspicious_tokens = main.tokenize_by_lines(SUSPICIOUS_TEXT)
+original_tokens = main.tokenize_by_lines(original)
+suspicious_tokens = main.tokenize_by_lines(suspicious)
 
 print("Tokenized original text:", original_tokens)
 print("Tokenized suspicious text:", suspicious_tokens)
@@ -45,4 +28,3 @@ print("Text average plagiarism:", text_plagiarism_score)
 
 RESULT = text_plagiarism_score
 assert RESULT, 'Not working'
->>>>>>> Stashed changes
