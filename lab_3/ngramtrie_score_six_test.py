5abd8b98c6fa6891b85f93639cc83634d23f86a8
dmitry-uraev <uraev.dima@yandex.ru>
2020-11-01 17:05:21 +0300

    added lab_3 project structure with starting files

---
+++ lab_3/ngramtrie_score_six_test.py
@@ -0,0 +1,185 @@
+# pylint: skip-file
+"""
+Tests for NGramTrie class
+"""
+
+import math
+import unittest
+from main import NGramTrie
+
+
+class BiGramTest(unittest.TestCase):
+    """
+    check NGramTrie class functionality on BiGrams.
+        All tests should pass for 6 score or above
+    """
+
+    def test_ngram_trie_check_creation(self):
+        ngram = NGramTrie(2)
+        self.assertEqual(ngram.size, 2)
+        self.assertEqual(ngram.n_grams, ())
+        self.assertEqual(ngram.n_gram_frequencies, {})
+        self.assertEqual(ngram.n_gram_log_probabilities, {})
+
+# -----------------------------------------------------------
+
+    def test_fill_n_grams_ideal(self):
+        ngram = NGramTrie(2)
+        text = (((1, 2, 3, 4, 5), (2, 3, 4, 5)),)
+        expected = (
+            (
+                ((1, 2), (2, 3), (3, 4), (4, 5)),
+                ((2, 3), (3, 4), (4, 5))
+            ),
+        )
+        actual = ngram.fill_n_grams(text)
+        self.assertEqual(0, actual)
+        self.assertEqual(ngram.n_grams, expected)
+
+    def test_fill_n_grams_duplcicates_ideal(self):
+        ngram = NGramTrie(2)
+        sentences = (((1, 2, 1, 2, 1, 2), (10, 11, 12)),)
+        expected = (
+            (
+                ((1, 2), (2, 1), (1, 2), (2, 1), (1, 2)),
+                ((10, 11), (11, 12))
+            ),
+        )
+        actual = ngram.fill_n_grams(sentences)
+        self.assertEqual(0, actual)
+        self.assertEqual(ngram.n_grams, expected)
+
+    def test_fill_n_grams_empty(self):
+        ngram = NGramTrie(2)
+        sentences = ()
+        expected = ()
+        actual = ngram.fill_n_grams(sentences)
+        self.assertEqual(0, actual)
+        self.assertEqual(ngram.n_grams, expected)
+
+    def test_fill_n_grams_none(self):
+        ngram = NGramTrie(2)
+        sentences = None
+        expected = ()
+        actual = ngram.fill_n_grams(sentences)
+        self.assertEqual(1, actual)
+        self.assertEqual(ngram.n_grams, expected)
+
+    def test_fill_n_grams_not_tuple(self):
+        ngram = NGramTrie(2)
+        sentences = [(1, 2, 3, 4, 5)]
+        expected = ()
+        actual = ngram.fill_n_grams(sentences)
+        self.assertEqual(1, actual)
+        self.assertEqual(ngram.n_grams, expected)
+
+# -------------------------------------------------------------
+
+    def test_calculate_n_grams_frequencies_ideal(self):
+        ngram = NGramTrie(2)
+        sentences = (((1, 2, 3, 4, 5),),)
+        ngram.fill_n_grams(sentences)
+
+        expected = {(1, 2): 1, (2, 3): 1, (3, 4): 1, (4, 5): 1}
+        actual = ngram.calculate_n_grams_frequencies()
+        self.assertEqual(expected, ngram.n_gram_frequencies)
+        self.assertEqual(0, actual)
+
+    def test_calculate_n_grams_frequencies_duplcicates_ideal(self):
+        ngram = NGramTrie(2)
+        sentences = (((1, 2, 1, 2, 1, 2), (1, 2)),)
+        ngram.fill_n_grams(sentences)
+
+        expected = {(1, 2): 4, (2, 1): 2}
+        actual = ngram.calculate_n_grams_frequencies()
+        self.assertEqual(expected, ngram.n_gram_frequencies)
+        self.assertEqual(0, actual)
+
+    def test_calculate_n_grams_frequencies_empty(self):
+        ngram = NGramTrie(2)
+        sentences = ()
+        ngram.fill_n_grams(sentences)
+
+        expected = {}
+        actual = ngram.calculate_n_grams_frequencies()
+        self.assertEqual(expected, ngram.n_gram_frequencies)
+        self.assertEqual(1, actual)
+
+    def test_get_ngrams_frequencies_from_sentence_none(self):
+        ngram = NGramTrie(2)
+        sentences = None
+        ngram.fill_n_grams(sentences)
+
+        expected = {}
+        actual = ngram.calculate_n_grams_frequencies()
+        self.assertEqual(expected, ngram.n_gram_frequencies)
+        self.assertEqual(1, actual)
+
+    def test_get_ngrams_frequencies_from_sentence_not_tuple(self):
+        ngram = NGramTrie(2)
+        sentences = [(1, 2, 3, 4, 5)]
+        ngram.fill_n_grams(sentences)
+
+        expected = {}
+        actual = ngram.calculate_n_grams_frequencies()
+        self.assertEqual(expected, ngram.n_gram_frequencies)
+        self.assertEqual(1, actual)
+
+# ----------------------------------------------------------
+
+    def test_top_n_grams_ideal(self):
+        ngram = NGramTrie(2)
+        top_n = 2
+        ngram.n_gram_frequencies = {(1, 2): 100,
+                                    (2, 3): 123,
+                                    (3, 4): 12345}
+        expected = ((3, 4), (2, 3))
+        actual = ngram.top_n_grams(top_n)
+        self.assertEqual(expected, actual)
+
+    def test_top_n_grams_more(self):
+        ngram = NGramTrie(2)
+        top_n = 2000000
+        ngram.n_gram_frequencies = {(1, 2): 100,
+                                    (2, 3): 123,
+                                    (3, 4): 12345}
+        expected = ((3, 4), (2, 3), (1, 2))
+        actual = ngram.top_n_grams(top_n)
+        self.assertEqual(expected, actual)
+
+    def test_top_n_grams_inappropriate(self):
+        ngram = NGramTrie(2)
+        bad_inputs = [[], (), {}, None, True, '', -1, 0, 9.22]
+        expected = ()
+        for bad_input in bad_inputs:
+            actual = ngram.top_n_grams(bad_input)
+            self.assertEqual(expected, actual)
+
+# -----------------------------------------------------------
+
+    def test_calculate_log_probabilities_ideal(self):
+        ngram = NGramTrie(2)
+        ngram.n_gram_frequencies = {(1, 2): 10, (1, 3): 2, (2, 5): 5}
+        first_prob = math.log(10 / 12)
+        second_prob = math.log(2 / 12)
+
+        actual = ngram.calculate_log_probabilities()
+        self.assertEqual(ngram.n_gram_log_probabilities[(1, 2)], first_prob)
+        self.assertEqual(ngram.n_gram_log_probabilities[(1, 3)], second_prob)
+        self.assertEqual(0, actual)
+
+    def test_calculate_log_probabilities_one_bi_gram(self):
+        ngram = NGramTrie(2)
+        ngram.n_gram_frequencies = {(1, 2): 10}
+
+        actual = ngram.calculate_log_probabilities()
+        self.assertEqual(ngram.n_gram_log_probabilities[(1, 2)], 0.0)
+        self.assertEqual(0, actual)
+
+    def test_calculate_log_probabilities_empty_frequencies(self):
+        ngram = NGramTrie(2)
+        ngram.n_gram_frequencies = {}
+
+        actual = ngram.calculate_log_probabilities()
+        self.assertEqual(ngram.n_gram_log_probabilities, {})
+        self.assertEqual(1, actual)
---
+++ lab_3/tokenize_score_four_test.py
@@ -0,0 +1,116 @@
+# pylint: skip-file
+"""
+Tests for tokenize_by_sentence_function
+"""
+
+import unittest
+from main import tokenize_by_sentence
+
+
+class TokenizeBySentenceTest(unittest.TestCase):
+    """
+    checks for tokenize_by_sentence function.
+        All tests should pass for score 4 or above
+    """
+    def test_tokenize_by_sentence_ideal(self):
+        """
+        Tests that tokenize_by_sentence function
+            can handle ideal two sentence input
+        """
+        text = 'She is happy. He is happy.'
+        expected = (
+            (('_', 's', 'h', 'e', '_'), ('_', 'i', 's', '_'), ('_', 'h', 'a', 'p', 'p', 'y', '_')),
+            (('_', 'h', 'e', '_'), ('_', 'i', 's', '_'), ('_', 'h', 'a', 'p', 'p', 'y', '_'))
+        )
+        actual = tokenize_by_sentence(text)
+        self.assertEqual(expected, actual)
+
+    def test_tokenize_by_sentence_punctuation_marks(self):
+        """
+        Tests that tokenize_by_sentence function
+            can process and ignore different punctuation marks
+        """
+        text = 'The, first sentence - nice. The second sentence: bad!'
+        expected = (
+            (('_', 't', 'h', 'e', '_'), ('_', 'f', 'i', 'r', 's', 't', '_'),
+             ('_', 's', 'e', 'n', 't', 'e', 'n', 'c', 'e', '_'), ('_', 'n', 'i', 'c', 'e', '_')),
+            (('_', 't', 'h', 'e', '_'), ('_', 's', 'e', 'c', 'o', 'n', 'd', '_'),
+             ('_', 's', 'e', 'n', 't', 'e', 'n', 'c', 'e', '_'), ('_', 'b', 'a', 'd', '_'))
+        )
+        actual = tokenize_by_sentence(text)
+        self.assertEqual(expected, actual)
+
+    def test_tokenize_by_sentence_dirty_text(self):
+        """
+        Tests that tokenize_by_sentence function
+            can handle text filled with inappropriate characters
+        """
+        text = 'The first% sentence><. The sec&*ond sent@ence #.'
+        expected = (
+            (('_', 't', 'h', 'e', '_'), ('_', 'f', 'i', 'r', 's', 't', '_'),
+             ('_', 's', 'e', 'n', 't', 'e', 'n', 'c', 'e', '_')),
+            (('_', 't', 'h', 'e', '_'), ('_', 's', 'e', 'c', 'o', 'n', 'd', '_'),
+             ('_', 's', 'e', 'n', 't', 'e', 'n', 'c', 'e', '_'))
+        )
+        actual = tokenize_by_sentence(text)
+        self.assertEqual(expected, actual)
+
+    def test_tokenize_by_sentence_incorrect_input(self):
+        """
+        Tests that tokenize_by_sentence function
+            can handle incorrect input cases
+        """
+        bad_inputs = [[], {}, (), None, 9, 9.34, True]
+        expected = ()
+        for bad_input in bad_inputs:
+            actual = tokenize_by_sentence(bad_input)
+            self.assertEqual(expected, actual)
+
+    def test_tokenize_by_sentence_complex(self):
+        """
+        Tests that tokenize_by_sentence function
+            can handle complex split case
+        """
+        text = 'Mar#y wa$nted, to swim. However, she was afraid of sharks.'
+        expected = (
+            (('_', 'm', 'a', 'r', 'y', '_'), ('_', 'w', 'a', 'n', 't', 'e', 'd', '_'),
+             ('_', 't', 'o', '_'), ('_', 's', 'w', 'i', 'm', '_')),
+            (('_', 'h', 'o', 'w', 'e', 'v', 'e', 'r', '_'), ('_', 's', 'h', 'e', '_'),
+             ('_', 'w', 'a', 's', '_'), ('_', 'a', 'f', 'r', 'a', 'i', 'd', '_'),
+             ('_', 'o', 'f', '_'), ('_', 's', 'h', 'a', 'r', 'k', 's', '_'))
+        )
+        actual = tokenize_by_sentence(text)
+        self.assertEqual(expected, actual)
+
+    def test_tokenize_by_sentence_empty_sentence(self):
+        """
+        Tests that tokenize_by_sentence function
+            can handle empty sentence input
+        """
+        text = ''
+
+        expected = ()
+        actual = tokenize_by_sentence(text)
+        self.assertEqual(expected, actual)
+
+    def test_tokenize_by_sentence_inappropriate_sentence(self):
+        """
+        Tests that tokenize_by_sentence function
+            can handle inappropriate sentence input
+        """
+        text = '$#&*@#$*#@)'
+
+        expected = ()
+        actual = tokenize_by_sentence(text)
+        self.assertEqual(expected, actual)
+
+    def test_tokenize_by_sentence_not_sentence(self):
+        """
+        Tests that tokenize_by_sentence function
+            can handle not sentence input
+        """
+        text = 'Mary'
+
+        expected = ()
+        actual = tokenize_by_sentence(text)
+        self.assertEqual(expected, actual)
