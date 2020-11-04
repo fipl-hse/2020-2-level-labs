e8fafcdcf42074d761076513d5e72943f5eaafc8
AnastasiaZheleznyakova <66490710+AnastasiaZheleznyakova@users.noreply.github.com>
2020-11-04 10:01:17 +0300

    Update start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     accumulated_diff_stats = lab_2.main.accumulate_diff_stats(original_text_tokens, suspicious_text_tokens)
 
     expected = open('lab_2/diff_report_example.txt', 'r', errors='coerce').read().split()
-    actual = lab_2.main.create_diff_report(original_text_tokens, suspicious_text_tokens, accumulated_diff_stats).split()
+    #actual = lab_2.main.create_diff_report(original_text_tokens, suspicious_text_tokens, accumulated_diff_stats).split()
 
     RESULT = actual
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
