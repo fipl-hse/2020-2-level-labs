92bab1c23657331731bf245c517228faacc100ac
vngrudkina <dolgorubka@gmail.com>
2020-11-06 02:46:35 +0300

    delete all in start.py

--- lab_3/start.py
+++ lab_3/start.py
@@ -2,12 +2,6 @@
 Language detector implementation starter
 """
 
-import lab_3.main
 
-if __name__ == '__main__':
 
-    # here goes your function calls
 
-    RESULT = ''
-    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, ''
