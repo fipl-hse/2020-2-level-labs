0a9a7e2112e51f014f07c3580374281d064abeb4
vngrudkina <dolgorubka@gmail.com>
2020-11-06 06:16:06 +0300

    ff

---
+++ lab_3/start.py
@@ -0,0 +1,9 @@
+#import lab_3.main
+
+if __name__ == '__main__':
+
+    # here goes your function calls
+
+    RESULT = ''
+    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
+#    assert RESULT, ''
