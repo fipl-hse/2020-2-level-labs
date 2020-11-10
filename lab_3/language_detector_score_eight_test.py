3ae16742996634f807bc5c9e4e0fd72f996942d1
dmitry-uraev <uraev.dima@yandex.ru>
2020-11-11 00:41:27 +0300

    refactored inputs for some tests - Note! now encode function takes tuple of sentences as described in description

--- lab_3/language_detector_score_eight_test.py
+++ lab_3/language_detector_score_eight_test.py
@@ -117,7 +117,7 @@ class LanguageDetectorTest(unittest.TestCase):
         first_n_grams = ((1, 2), (3, 4), (7, 8), (9, 10), (5, 6), (13, 14))
         second_n_grams = ((1, 2), (5, 6), (7, 8), (3, 4), (11, 12), (15, 16))
 
-        expected = 5
+        expected = 17
         actual = language_detector._calculate_distance(first_n_grams, second_n_grams)
         self.assertEqual(expected, actual)
 
