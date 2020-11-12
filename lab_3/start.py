e21c875dbb19681c06ad828388c517d8c51a94ff
alenashuvar <alenashuvar@users.noreply.github.com>
2020-11-12 19:25:10 +0300

    fix start.py

--- lab_3/start.py
+++ lab_3/start.py
@@ -2,19 +2,19 @@
 Language detector implementation starter
 """
 
-from main import tokenize_by_sentence
-from main import encode_corpus
-from main import NGramTrie
-from main import LetterStorage
-from main import ProbabilityLanguageDetector
+from lab_3.main import tokenize_by_sentence
+from lab_3.main import encode_corpus
+from lab_3.main import NGramTrie
+from lab_3.main import LetterStorage
+from lab_3.main import ProbabilityLanguageDetector
 
 
 if __name__ == '__main__':
 
     # here goes your function calls
-    unknown_file = open('unknown_Arthur_Conan_Doyle.txt', encoding='utf-8')
-    german_file = open('Thomas_Mann.txt', encoding='utf-8')
-    english_file = open('Frank_Baum.txt', encoding='utf-8')
+    unknown_file = open('lab_3/unknown_Arthur_Conan_Doyle.txt', encoding='utf-8')
+    german_file = open('lab_3/Thomas_Mann.txt', encoding='utf-8')
+    english_file = open('lab_3/Frank_Baum.txt', encoding='utf-8')
 
     text_unk = tokenize_by_sentence(unknown_file.read())
     text_ger = tokenize_by_sentence(german_file.read())
