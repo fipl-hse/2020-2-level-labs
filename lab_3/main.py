31f62052355004e2bea7752691b8b18abd4bd3ed
katherinekh <nerupineapple@gmail.com>
2020-11-14 19:55:31 +0300

    It should be the last commit

--- lab_3/main.py
+++ lab_3/main.py
@@ -17,7 +17,8 @@ def tokenize_by_sentence(text: str) -> tuple:
          (('_', 'h', 'e', '_'), ('_', 'i', 's', '_'), ('_', 'h', 'a', 'p', 'p', 'y', '_'))
          )
     """
-    pass
+    if not isinstance(text, str):
+        return ()
 
 
 # 4
