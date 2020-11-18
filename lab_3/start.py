3d9194109dff9ba3280897a44df3b23a8abc2d85
ffmiil <65333072+ffmiil@users.noreply.github.com>
2020-11-18 10:26:00 +0300

    start

--- lab_3/start.py
+++ lab_3/start.py
@@ -2,12 +2,29 @@
 Language detector implementation starter
 """
 
-import lab_3.main
+import main
 
 if __name__ == '__main__':
+    text = 'It aspires to being a modernized fairy tale, in which the wonderment and joy are retained. The heartaches and nightmares are left out.'
+    tokens = main.tokenize_by_sentence(text)
+    print(tokens)
+
+    storage = main.LetterStorage()
+    storage.update(tokens)
+    print(storage.storage)
+
+    encoded_corpus = main.encode_corpus(storage, tokens)
+    print(encoded_corpus)
+
+    bi_gram = main.NGramTrie(2)
+    fille = bi_gram.fill_n_grams(encoded_corpus)
+    frequencies = bi_gram.calculate_n_grams_frequencies()
+    top = bi_gram.top_n_grams(5)
+    print('Frequencies: ', frequencies)
+    print('Top 5: ', top)
 
     # here goes your function calls
 
-    RESULT = ''
+    RESULT = top
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, ''
+    assert RESULT, 'Not working'
