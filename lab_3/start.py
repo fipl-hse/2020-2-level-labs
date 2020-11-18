997666d95f5a92f01bdaffc29cdd87ef66e32e37
katyleo <65247432+katyleo@users.noreply.github.com>
2020-11-18 22:27:38 +0300

    result

--- lab_3/start.py
+++ lab_3/start.py
@@ -49,4 +49,4 @@ if __name__ == '__main__':
 
     RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT == 'english', 'Not working'
+    assert RESULT, 'Not working'
