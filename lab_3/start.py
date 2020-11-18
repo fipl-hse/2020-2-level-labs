0804846a9eb29e67e5b7c417efb24074b1dc8c03
Anastasia <avisaichkina@edu.hse.ru>
2020-11-18 10:31:27 +0300

    fixed start.py

--- lab_3/start.py
+++ lab_3/start.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
 
     # here goes your function calls
 
-    english_file = open('lab_3/Frank_Baum.txt', encoding='utf-8')
-    german_file = open('lab_3/Thomas_Mann.txt', encoding='utf-8')
-    unknown_file = open('lab_3/unknown_Arthur_Conan_Doyle.txt', encoding='utf-8')
+    english_file = open('Frank_Baum.txt', encoding='utf-8')
+    german_file = open('Thomas_Mann.txt', encoding='utf-8')
+    unknown_file = open('unknown_Arthur_Conan_Doyle.txt', encoding='utf-8')
 
     english_text = lab_3.main.tokenize_by_sentence(english_file.read())
     german_text = lab_3.main.tokenize_by_sentence(german_file.read())
@@ -33,11 +33,10 @@ if __name__ == '__main__':
     language_detector.new_language(encoded_english, 'english')
     language_detector.new_language(encoded_german, 'german')
 
-    ngram_unknown = lab_3.main.NGramTrie(3)
+    ngram_unknown = lab_3.main.NGramTrie(4)
     ngram_unknown.fill_n_grams(encoded_unknown)
 
     actual = language_detector.detect_language(ngram_unknown.n_grams)
-    print(actual)
 
     RESULT = actual['german'] > actual['english']
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
