691f8374956694e8f776c823fb3aef00450008bc
SoullessDark <65155304+SoullessDark@users.noreply.github.com>
2020-11-19 20:41:31 +0300

    nthng

--- lab_3/start.py
+++ lab_3/start.py
@@ -5,25 +5,25 @@ Language detector implementation starter
 import main
 
 if __name__ == '__main__':
-    text = 'Horse is beautiful. Dog is beautiful.'
-    tokens = main.tokenize_by_sentence(text)
-    print(tokens)
+    TEXT = 'Horse is beautiful. Dog is beautiful.'
+    TOKENS = main.tokenize_by_sentence(TEXT)
+    print(TOKENStokens)
 
     storage = main.LetterStorage()
-    storage.update(tokens)
+    storage.update(TOKENS)
     print(storage.storage)
 
-    encoded_corpus = main.encode_corpus(storage, tokens)
+    encoded_corpus = main.encode_corpus(storage, TOKENS)
     print(encoded_corpus)
 
     three_gram = main.NGramTrie(3)
-    filles = three_gram.fill_n_grams(encoded_corpus)
-    frequencies = three_gram.calculate_n_grams_frequencies()
-    top = three_gram.top_n_grams(5)
-    print('Frequencies: ', frequencies)
-    print('Top 5: ', top)
+    FILLED = three_gram.fill_n_grams(encoded_corpus)
+    FREQS = three_gram.calculate_n_grams_frequencies()
+    TOP = three_gram.top_n_grams(5)
+    print('Frequencies: ', FREQS)
+    print('Top 5: ', TOP)
 
-    RESULT = top
+    RESULT = TOP
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, ((1, 7, 5), (7, 5, 1), (1, 8, 6), (8, 6, 9), (6, 9, 10))
     'Not working'
