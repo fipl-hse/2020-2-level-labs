6c727e042d33510dc83769692a2875b6ab47987e
Yano Erina <blackberrycupcake@yandex.ru>
2020-11-19 11:30:22 +0300

    start.py is done

--- lab_3/start.py
+++ lab_3/start.py
@@ -8,6 +8,23 @@ if __name__ == '__main__':
 
     # here goes your function calls
 
-    RESULT = ''
+    text = 'All emotions, and that one particularly, were abhorrent to his cold, precise but admirably balanced mind. '\
+           'He was, I take it, the most perfect reasoning and observing machine that the world has seen, ' \
+           'but as a lover he would have placed himself in a false position. '
+    tokenized_text = lab_3.main.tokenize_by_sentence(text)
+    print(tokenized_text)
+
+    letter_storage = lab_3.main.LetterStorage()
+    letter_storage.update(tokenized_text)
+    encoded_text = lab_3.main.encode_corpus(letter_storage, tokenized_text)
+    print(encoded_text)
+
+    bi_gram = lab_3.main.NGramTrie(2)
+    filled_bi_grams = bi_gram.fill_n_grams(encoded_text)
+    bi_grams_frequencies = bi_gram.calculate_n_grams_frequencies()
+    top_bi_grams = bi_gram.top_n_grams(5)
+    print('Top 5 bi_grams: ', top_bi_grams)
+
+    RESULT = top_bi_grams
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, ''
+    assert RESULT == ((8, 5), (5, 6), (15, 5), (11, 5), (5, 11)), 'Not working'
