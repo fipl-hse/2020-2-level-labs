f7c81f5643d72bc2dc247b60003fcbd6d6e975f5
SoullessDark <65155304+SoullessDark@users.noreply.github.com>
2020-11-19 20:44:01 +0300

    start again and again

--- lab_3/start.py
+++ lab_3/start.py
@@ -7,7 +7,7 @@ import main
 if __name__ == '__main__':
     TEXT = 'Horse is beautiful. Dog is beautiful.'
     TOKENS = main.tokenize_by_sentence(TEXT)
-    print(TOKENStokens)
+    print(TOKENS)
 
     storage = main.LetterStorage()
     storage.update(TOKENS)
@@ -25,5 +25,4 @@ if __name__ == '__main__':
 
     RESULT = TOP
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, ((1, 7, 5), (7, 5, 1), (1, 8, 6), (8, 6, 9), (6, 9, 10))
-    'Not working'
+    assert RESULT == ((1, 7, 5), (7, 5, 1), (1, 8, 6), (8, 6, 9), (6, 9, 10)), 'Not working'
