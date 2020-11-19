bbeece694333504ff5fd7c0b371b6d9396fd172d
ashirka <66332800+ashirka@users.noreply.github.com>
2020-11-19 16:04:11 +0300

    Update start.py

--- lab_3/start.py
+++ lab_3/start.py
@@ -6,8 +6,23 @@ import lab_3.main
 
 if __name__ == '__main__':
 
-    # here goes your function calls
+    sentence = 'I love you to the moon and back'
+    tokenized_sentence = lab_3.main.tokenize_by_sentence(sentence)
+    print(f'TOKENS: {tokenized_sentence}')
 
-    RESULT = ''
+    storage = lab_3.main.LetterStorage()
+    storage.update(tokenized_sentence)
+    print(f'STORAGE: {storage.storage}')
+
+    encoded_corpus = lab_3.main.encode_corpus(storage, tokenized_sentence)
+    print(f'ENCODED_CORPUS: {encoded_corpus}')
+
+    n_gram = lab_3.main.NGramTrie(2)
+    fill_n_gram = n_gram.fill_n_grams(encoded_corpus)
+    frequencies = n_gram.calculate_n_grams_frequencies()
+    top = n_gram.top_n_grams(5)
+    print(f'TOP_5: {top}')
+
+    RESULT = top
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, ''
+    assert RESULT == ((4, 1), (1, 8), (8, 4), (12, 1), (4, 9)), 'Not working'
