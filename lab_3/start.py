d313b5bb6b57627eb5eec92215512489ac8ba4ab
SoullessDark <65155304+SoullessDark@users.noreply.github.com>
2020-11-19 18:58:25 +0300

    start again

--- lab_3/start.py
+++ lab_3/start.py
@@ -2,52 +2,42 @@
 Language detector implementation starter
 """
 
-from lab_3.main import tokenize_by_sentence
-from lab_3.main import encode_corpus
-from lab_3.main import NGramTrie
-from lab_3.main import LetterStorage
-from lab_3.main import LanguageDetector
-
+import lab_3.main
 
 if __name__ == '__main__':
 
     # here goes your function calls
 
-    unknown_file = open('lab_3/unknown_Arthur_Conan_Doyle.txt', encoding='utf-8')
-    german_file = open('lab_3/Thomas_Mann.txt', encoding='utf-8')
-    english_file = open('lab_3/Frank_Baum.txt', encoding='utf-8')
+    eng_file = open('Frank_Baum.txt', encoding='utf-8')
+    germ_file = open('Thomas_Mann.txt', encoding='utf-8')
+    unk_file = open('unknown_Arthur_Conan_Doyle.txt', encoding='utf-8')
 
-    unknown_text = tokenize_by_sentence(unknown_file.read())
-    german_text = tokenize_by_sentence(german_file.read())
-    english_text = tokenize_by_sentence(english_file.read())
-    english_file.close()
-    german_file.close()
-    unknown_file.close()
+    eng_text = lab_3.main.tokenize_by_sentence(eng_file.read())
+    germ_text = lab_3.main.tokenize_by_sentence(germ_file.read())
+    unk_text = lab_3.main.tokenize_by_sentence(unk_file.read())
 
-    letter_storage = LetterStorage()
-    letter_storage.update(english_text)
-    letter_storage.update(german_text)
-    letter_storage.update(unknown_text)
+    eng_file.close()
+    germ_file.close()
+    unk_file.close()
 
-    eng_encoded = encode_corpus(letter_storage, english_text)
-    unk_encoded = encode_corpus(letter_storage, unknown_text)
-    ger_encoded = encode_corpus(letter_storage, german_text)
+    letter_storage = lab_3.main.LetterStorage()
+    letter_storage.update(eng_text)
+    letter_storage.update(germ_text)
+    letter_storage.update(unk_text)
 
-    language_detector = LanguageDetector((3, 4, 5), 1000)
-    language_detector.new_language(eng_encoded, 'english')
-    language_detector.new_language(ger_encoded, 'german')
+    encoded_eng = lab_3.main.encode_corpus(letter_storage, eng_text)
+    encoded_germ = lab_3.main.encode_corpus(letter_storage, germ_text)
+    encoded_unk = lab_3.main.encode_corpus(letter_storage, unk_text)
 
-    ngram_unknown = NGramTrie(4)
-    ngram_unknown.fill_n_grams(unk_encoded)
+    language_detector = lab_3.main.LanguageDetector((3, 4, 5), 1000)
+    language_detector.new_language(encoded_eng, 'english')
+    language_detector.new_language(encoded_germ, 'german')
 
-    language_log_probability_dict = language_detector.detect_language(ngram_unknown.n_grams)
+    ngram_unknown = lab_3.main.NGramTrie(3)
+    ngram_unknown.fill_n_grams(encoded_unk)
 
-    if language_log_probability_dict['german'] > language_log_probability_dict['english']:
-        RESULT = 'english'
-    else:
-        RESULT = 'german'
+    actual = language_detector.detect_language(ngram_unknown.n_grams)
 
-    print('It is a {} text.'.format(RESULT))
-
+    RESULT = actual['german'] > actual['english']
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT == 'english', 'Not working'
+    assert RESULT == 1, "Detector does not work"
