58684a60f4e3b275381a02e1d9dcf844854df25b
Kate Arbuzova <kate.arbuzova01@yandex.ru>
2020-11-20 22:10:38 +0300

    got better

--- lab_3/start.py
+++ lab_3/start.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     with open('lab_3/Frank_Baum.txt', encoding='utf-8') as english_file:
         english_text = tokenize_by_sentence(english_file.read())
 
-    with open('lab_3/Thomas_Mann.txt', encoding='utf-8')   as german_file:
+    with open('lab_3/Thomas_Mann.txt', encoding='utf-8') as german_file:
         german_text = tokenize_by_sentence(german_file.read())
 
     with open('lab_3/unknown_Vorleser.txt', encoding='utf-8') as unknown_file:
@@ -39,17 +39,17 @@ Unknown text: {unknown_text[0]}
 
     print('Creating language detector with different n-gram tries and top_k-s')
 
-    for top_k in (600, 1000, 2500):
-        for level in (2, 5, 10):
-            ngram_unknown = NGramTrie(level)
-            ngram_unknown.fill_n_grams(unknown_encoded)
+    for level in (2, 5, 10):
+        ngram_unknown = NGramTrie(level)
+        ngram_unknown.fill_n_grams(unknown_encoded)
+        for top_k in (600, 1000, 2500):
             language_detector = ProbabilityLanguageDetector((level,), top_k)
             language_detector.new_language(english_encoded, 'english')
             language_detector.new_language(german_encoded, 'german')
 
             actual = language_detector.detect_language(ngram_unknown.n_grams)
 
-            print(top_k, level, actual)
+            print('top_k =', top_k, 'level =', level, actual)
             if top_k == 600 and level == 5:
                 RESULT = actual
 
@@ -57,4 +57,4 @@ Unknown text: {unknown_text[0]}
           'by level = 2 it works incorrectly.\nTop_k doesn\'t change the result')
 
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT['english'] > RESULT['german'] , 'The detector doesn\'t work.'
+    assert RESULT['english'] > RESULT['german'], 'The detector doesn\'t work.'
