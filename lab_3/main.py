f704cd8213f3bbb46cd6893b006636ceb585b19a
vngrudkina <dolgorubka@gmail.com>
2020-11-24 20:18:00 +0300

    Delete ) in main.py

--- lab_3/main.py
+++ lab_3/main.py
@@ -96,7 +96,7 @@ def encode_corpus(storage: LetterStorage, corpus: tuple) -> tuple:
             encoded_token = []
             for letter in token:
                 encoded_token.append(storage.get_id_by_letter(letter))
-            encoded_sentence.append(tuple(encoded_token)
+            encoded_sentence.append(tuple(encoded_token))
         encoded_corpus.append(tuple(encoded_sentence))
     return tuple(encoded_corpus)
 
