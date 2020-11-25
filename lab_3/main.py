8c8175dc567d8139669aa95ac05fca36d3a7ecd5
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2020-11-25 13:28:02 +0300

    p

--- lab_3/main.py
+++ lab_3/main.py
@@ -343,30 +343,3 @@ class ProbabilityLanguageDetector(LanguageDetector):
         :return: a dictionary with language_name: probability
         """
         pass
-
-
-letter_storage = LetterStorage()
-language_detector = LanguageDetector((3,), 100)
-
-file_first = open('Frank_Baum.txt', 'r', encoding='utf-8')
-file_second = open('Thomas_Mann.txt', 'r', encoding='utf-8')
-file_third = open('unknown_Arthur_Conan_Doyle.txt', 'r', encoding='utf-8')
-
-text_english = tokenize_by_sentence(file_first.read())
-text_german = tokenize_by_sentence(file_second.read())
-text_unknown = tokenize_by_sentence(file_third.read())
-letter_storage.update(text_english)
-letter_storage.update(text_german)
-letter_storage.update(text_unknown)
-encoded_english = encode_corpus(letter_storage, text_english)
-encoded_german = encode_corpus(letter_storage, text_german)
-encoded_unknown = encode_corpus(letter_storage, text_unknown)
-file_first.close()
-file_second.close()
-file_third.close()
-
-language_detector.new_language(encoded_english, 'english')
-language_detector.new_language(encoded_german, 'german')
-
-actual = language_detector.detect_language(encoded_unknown)
-print(actual)
