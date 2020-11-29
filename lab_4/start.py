3ed03a47f6f3a39dbca05563e9d34b22b794a1ed
ffmiil <65333072+ffmiil@users.noreply.github.com>
2020-11-29 14:21:55 +0300

    marsh!

--- lab_4/start.py
+++ lab_4/start.py
@@ -7,7 +7,7 @@ from lab_4.main import tokenize_by_sentence, encode_text
 
 if __name__ == '__main__':
     # here goes your function calls
-    first_text = open('../lab_3/Frank_Baum.txt', encoding="utf-8")
+    first_text = open('lab_3/Frank_Baum.txt', encoding="utf-8")
     first_text_tokenized = tokenize_by_sentence(first_text.read())
 
     word_storage = WordStorage()
