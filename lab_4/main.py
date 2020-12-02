f4af4b181e59a20a4916f4bf7fb96cdd0fe7773d
alenashuvar <alenashuvar@users.noreply.github.com>
2020-12-02 22:55:23 +0300

    add functions score 4

--- lab_4/main.py
+++ lab_4/main.py
@@ -3,36 +3,67 @@ Lab 4
 """
 
 from ngrams.ngram_trie import NGramTrie
+import re
 
 
 def tokenize_by_sentence(text: str) -> tuple:
-    pass
+    if not isinstance(text, str):
+        raise ValueError
+
+    sentences = re.split('[!?.] |[!?.]\n', text)
+    list_tokens = []
+    for sentence in sentences:
+        tokens = re.sub('[^a-z \n]', '', sentence.lower()).split()
+        if tokens:
+            list_tokens.extend(tokens + ['<END>'])
+
+    return tuple(list_tokens)
 
 
 class WordStorage:
     def __init__(self):
-        pass
+        self.storage = {}
 
     def _put_word(self, word: str):
-        pass
+        if not isinstance(word, str) or not word:
+            raise ValueError
+        if word not in self.storage:
+            self.storage[word] = len(self.storage) + 1
+        return self.storage[word]
 
     def get_id(self, word: str) -> int:
-        pass
+        if not isinstance(word, str) or not word:
+            raise ValueError
+        if word not in self.storage:
+            raise KeyError
+        return self.storage[word]
 
     def get_word(self, word_id: int) -> str:
-        pass
+        if not isinstance(word_id, int) or not word_id:
+            raise ValueError
+        if word_id not in self.storage.values():
+            raise KeyError
+        for word, w_id in self.storage.items():
+            if w_id == word_id:
+                return word
 
     def update(self, corpus: tuple):
-        pass
+        if not isinstance(corpus, tuple):
+            raise ValueError
+        for word in corpus:
+            self._put_word(word)
 
 
 def encode_text(storage: WordStorage, text: tuple) -> tuple:
-    pass
+    if not isinstance(storage, WordStorage) or not isinstance(text, tuple):
+        raise ValueError
+    return tuple(storage.get_id(word) for word in text)
 
 
 class NGramTextGenerator:
     def __init__(self, word_storage: WordStorage, n_gram_trie: NGramTrie):
-        pass
+        self._word_storage = word_storage
+        self._n_gram_trie = n_gram_trie
 
     def _generate_next_word(self, context: tuple) -> int:
         pass
