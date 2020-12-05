f8c7c90bd3e667b5bb62a9067c072ee1f510cd89
marina-kaz <isthismeowgoingfrommyheart@gmail.com>
2020-12-05 14:47:34 +0300

    build for 4

--- lab_4/main.py
+++ lab_4/main.py
@@ -3,31 +3,112 @@ Lab 4
 """
 
 from ngrams.ngram_trie import NGramTrie
+import re
+
+def split_into_sentences(text: str) -> tuple:
+    if not isinstance(text, str):
+        return ()
+    for sign in ['?', '!']:
+        text = text.replace(sign, '.')   # unify ending symbols
+    potential_sentences = re.split('\.\\s', text)
+    proven_sentences = []
+    solved_indexes = []  # for cases with ambuguous separation
+    for index, sentence in enumerate(potential_sentences):
+        if index in solved_indexes:
+            continue
+        try:
+            if potential_sentences[index + 1][0].isupper():
+                proven_sentences.append(sentence)
+                continue
+            solved = False
+            while not solved:
+                index += 1
+                sentence += potential_sentences[index]
+                try:
+                    if potential_sentences[index + 1][0].isupper():
+                        sentence = ' ' + sentence  # чтобы потом было удобно парсить слова
+                        proven_sentences.append(sentence)
+                        solved = True
+                except IndexError:
+                    proven_sentences.append(sentence)
+                    solved = True
+                solved_indexes.append(index)
+        except IndexError:
+            proven_sentences.append(sentence)
+    return tuple(proven_sentences)
+
+
+def tokenize_by_words(text: str) -> tuple:
+    """
+    Splits sentences into tokens, converts the tokens into lowercase, removes punctuation
+    :param text: the initial text
+    :return: a list of lowercased tokens without punctuation
+    e.g. text = 'The weather is sunny, the man is happy.'
+    --> ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
+    """
+    for sign in ['?', '!', '.']:
+        text = text.replace(sign, ' ')
+    text_output = re.sub('[^a-z \n]', '', text.lower()).split() + ['<END>']
+    return text_output
 
 
 def tokenize_by_sentence(text: str) -> tuple:
-    pass
+    if not isinstance(text, str):
+        raise ValueError
+    if not len(re.findall('[A-Za-z]', text)) > 0:
+        return ()
+    sentences = split_into_sentences(text)
+    print(sentences)
+    tokenized = [tokenize_by_words(sentence) for sentence in sentences]
+    print(tokenized)
+    result = []
+    for sentence in tokenized:
+        result += sentence
+    return tuple(result)
 
 
 class WordStorage:
     def __init__(self):
-        pass
+        self.storage = {}
 
     def _put_word(self, word: str):
-        pass
+        if not isinstance(word, str) or len(word) < 1:
+            raise ValueError
+        if word not in self.storage:
+            self.storage[word] = len(self.storage) + 1
+            return len(self.storage)
+        return self.storage[word]
 
     def get_id(self, word: str) -> int:
-        pass
+        if not isinstance(word, str):
+            raise ValueError
+        if word not in self.storage:
+            raise KeyError
+        return self.storage[word]
 
     def get_word(self, word_id: int) -> str:
-        pass
+        if not isinstance(word_id, int):
+            raise ValueError
+        if word_id not in self.storage.values():
+            raise KeyError
+        for key in self.storage.keys():
+            if self.storage[key] == word_id:
+                return key
 
     def update(self, corpus: tuple):
-        pass
+        if not isinstance(corpus, tuple):
+            raise ValueError
+        for token in corpus:
+            self._put_word(token)
 
 
 def encode_text(storage: WordStorage, text: tuple) -> tuple:
-    pass
+    if not isinstance(text, tuple) or not isinstance(storage, WordStorage):
+        raise ValueError
+    encoded = [storage.get_id(word) for word in text]
+    return tuple(encoded)
+
+
 
 
 class NGramTextGenerator:
