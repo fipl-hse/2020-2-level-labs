21443128e5d4c1c03e58c0176bacee0b2da06e36
alenashuvar <alenashuvar@users.noreply.github.com>
2020-12-07 11:38:45 +0300

    fix lint again

--- lab_4/ngrams/ngram_trie.py
+++ lab_4/ngrams/ngram_trie.py
@@ -34,3 +34,9 @@ class NGramTrie:
                 self.n_gram_frequencies[n_gram] += 1
             else:
                 self.n_gram_frequencies[n_gram] = 1
+
+    def public_method(self):
+        pass
+
+    def public_method_2(self):
+        pass
