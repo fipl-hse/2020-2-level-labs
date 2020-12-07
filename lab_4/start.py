7a59cb5445a4f65978100d5b1b63a2312b43c7e8
standfromunder <polinapletneva2001@gmail.com>
2020-12-07 16:54:33 +0300

    #8

---
+++ lab_4/start.py
@@ -0,0 +1,31 @@
+import unittest
+from lab_4.main import encode_text, WordStorage, LikelihoodBasedTextGenerator, decode_text
+from lab_4.ngrams.ngram_trie import NGramTrie
+
+if __name__ == '__main__':
+    corpus = ('i', 'have', 'a', 'cat', '<END>',
+              'his', 'name', 'is', 'bruno', '<END>',
+              'i', 'have', 'a', 'dog', 'too', '<END>',
+              'his', 'name', 'is', 'rex', '<END>',
+              'her', 'name', 'is', 'rex', 'too', '<END>')
+
+    storage = WordStorage()
+    storage.update(corpus)
+
+    encoded = encode_text(storage, corpus)
+
+    trie = NGramTrie(3, encoded)
+
+    context = (storage.get_id('name'),
+               storage.get_id('is'),)
+    end = storage.get_id('<END>')
+
+    generator = LikelihoodBasedTextGenerator(storage, trie)
+
+    to_decode = generator.generate_text(context, 2)
+
+    expected = ('Name is rex', 'Her name is rex')
+    RESULT = decode_text(storage, to_decode)
+    print(RESULT)
+
+    assert RESULT == expected, 'doesnt work'
