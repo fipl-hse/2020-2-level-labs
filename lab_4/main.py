497626a7cc3ad91853c9fa3f92e887d087a127fa
idontunderstandtbh <65286701+idontunderstandtbh@users.noreply.github.com>
2020-12-08 12:59:25 +0300

    1 and 2 func

--- lab_4/main.py
+++ lab_4/main.py
@@ -3,31 +3,66 @@ Lab 4
 """
 
 from ngrams.ngram_trie import NGramTrie
-
+import re
 
 def tokenize_by_sentence(text: str) -> tuple:
-    pass
+    if not isinstance(text, str):
+        raise ValueError
+    sentences_list = re.split('[.!?]|\n', text)
+    tokens_list = []
+    for sentence in sentences_list:
+        separ = re.sub('[^a-z \n]', '', sentence.lower()).split()
+        if not separ:
+            continue
+        separ.append('<END>')
+        for word in separ:
+            tokens_list.append(word)
+    return tuple(tokens_list)
 
 
 class WordStorage:
     def __init__(self):
-        pass
+        self.storage = {}
+        self.value = 1
 
     def _put_word(self, word: str):
-        pass
+        if not isinstance(word, str) or len(word) == 0:
+            raise ValueError
+        if word not in self.storage:
+            self.storage[word] = self.value
+            self.value += 1
+            return 1
+        if word in self.storage:
+            return self.storage[word]
 
     def get_id(self, word: str) -> int:
-        pass
+        if not isinstance(word, str) or not word:
+            raise ValueError
+        return self.storage[word]
 
     def get_word(self, word_id: int) -> str:
-        pass
+        if not isinstance(word_id, int) or not word_id:
+            raise ValueError
+        for key, value in self.storage.items():
+            if word_id == value:
+                return key
+            else:
+                raise KeyError
 
     def update(self, corpus: tuple):
-        pass
+        if not isinstance(corpus, tuple):
+            raise ValueError
+        for word in corpus:
+            self._put_word(word)
 
 
 def encode_text(storage: WordStorage, text: tuple) -> tuple:
-    pass
+    if not isinstance(storage, WordStorage) or not isinstance(text, tuple):
+        raise ValueError
+    encode_text = []
+    for word in text:
+        encode_text.append(storage.get_id(word))
+    return tuple(encode_text)
 
 
 class NGramTextGenerator:
