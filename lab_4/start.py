02512e4b6b1eb4267e0f466b389888c77728737e
SoullessDark <65155304+SoullessDark@users.noreply.github.com>
2020-12-09 23:09:59 +0300

    start

---
+++ lab_4/start.py
@@ -0,0 +1,25 @@
+"""
+Text generator implementation starter
+"""
+
+from lab_4.main import WordStorage, NGramTrie, NGramTextGenerator
+from lab_4.main import tokenize_by_sentence, encode_text
+
+if __name__ == '__main__':
+    text = 'I have a cat. His name is Bruno. I have a dog. Her name is Rex. Her name is Rex too.'
+    text_tokenized = tokenize_by_sentence(text)
+
+    word_storage = WordStorage()
+    word_storage.update(text_tokenized)
+
+    encoded = encode_text(word_storage, text_tokenized)
+
+    trie = NGramTrie(2, encoded)
+    context = (word_storage.get_id('i'),)
+    generator = NGramTextGenerator(word_storage, trie)
+
+    RESULT = generator.generate_text(context, 4)
+
+    print(RESULT)
+
+    assert RESULT, "Not working"
