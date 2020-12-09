92a62210b7b89415a822e151a682a8f18a9ab213
SoullessDark <65155304+SoullessDark@users.noreply.github.com>
2020-12-09 15:11:54 +0300

    ngram generator and encoder

--- lab_4/main.py
+++ lab_4/main.py
@@ -72,16 +72,58 @@ def encode_text(storage: WordStorage, text: tuple) -> tuple:
 
 class NGramTextGenerator:
     def __init__(self, word_storage: WordStorage, n_gram_trie: NGramTrie):
-        pass
+        self._word_storage = word_storage
+        self._n_gram_trie = n_gram_trie
 
     def _generate_next_word(self, context: tuple) -> int:
-        pass
+        if not isinstance(context, tuple) or len(context) + 1 != self._n_gram_trie.size:
+            raise ValueError
+
+        ctxt_n_grams = {}
+        for n_gram, freq in self._n_gram_trie.n_gram_frequencies.items():
+            if context == n_gram[:self._n_gram_trie.size - 1]:
+                ctxt_n_grams[n_gram] = freq
+
+        if not ctxt_n_grams:
+            finding_top_word = sorted(self._n_gram_trie.uni_grams, key=self._n_gram_trie.uni_grams.get, reverse=True)
+            return finding_top_word[0][0]
+
+        finding_top_context_n_gram = sorted(ctxt_n_grams, key=ctxt_n_grams.get, reverse=True)
+        return finding_top_context_n_gram[0][-1]
 
     def _generate_sentence(self, context: tuple) -> tuple:
-        pass
+        if not isinstance(context, tuple):
+            raise ValueError
+
+        sent_ctxt = []
+        sent_ctxt.extend(list(context))
+        counter = 0
+        while counter < 20:
+            counter += 1
+            sent_ctxt.append(self._generate_next_word(tuple(sent_ctxt[-len(context):])))
+            if sent_ctxt[-1] == self._word_storage.storage['<END>']:
+                break
+        else:
+            sent_ctxt.append(self._word_storage.storage['<END>'])
+
+        return tuple(sent_ctxt)
 
     def generate_text(self, context: tuple, number_of_sentences: int) -> tuple:
-        pass
+        if not isinstance(context, tuple) or not isinstance(number_of_sentences, int) \
+                or isinstance(number_of_sentences, bool):
+            raise ValueError
+
+        text = []
+        while number_of_sentences:
+            number_of_sentences -= 1
+            new_sent = self._generate_sentence(context)
+
+            if new_sent[len(context) - 1] == self._word_storage.storage['<END>']:
+                new_sent = new_sent[len(context):]
+            text.extend(new_sent)
+            context = tuple(text[-len(context):])
+
+        return tuple(text)
 
 
 class LikelihoodBasedTextGenerator(NGramTextGenerator):
@@ -103,7 +145,22 @@ class BackOffGenerator(NGramTextGenerator):
 
 
 def decode_text(storage: WordStorage, encoded_text: tuple) -> tuple:
-    pass
+    if not isinstance(storage, WordStorage) or not isinstance(encoded_text, tuple) or not encoded_text:
+        raise ValueError
+
+    decoded_text = [[]]
+
+    for encoded_word in encoded_text:
+        decoded_word = storage.get_word(encoded_word)
+        if decoded_word == '<END>':
+            decoded_text.append([])
+        else:
+            decoded_text[-1].append(decoded_word)
+
+    decoded_text = [sentence[0][0].upper() + sentence[0][1:] + ' ' + ' '.join(sentence[1:])
+                    for sentence in decoded_text if sentence]
+
+    return tuple(decoded_text)
 
 
 def save_model(model: NGramTextGenerator, path_to_saved_model: str):
