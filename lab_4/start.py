f0b4633586c27c18beaaf7d2b330685c9596f80b
alenashuvar <alenashuvar@users.noreply.github.com>
2020-12-09 20:24:54 +0300

    changes in start.py

--- lab_4/start.py
+++ lab_4/start.py
@@ -2,31 +2,48 @@
 Text generation implementation starter
 """
 
-from lab_4.main import WordStorage, encode_text, LikelihoodBasedTextGenerator, decode_text
-from lab_4.ngrams.ngram_trie import NGramTrie
+from main import WordStorage, encode_text, LikelihoodBasedTextGenerator, decode_text, BackOffGenerator
+from ngrams.ngram_trie import NGramTrie
 
 if __name__ == '__main__':
-    corpus = ('i', 'have', 'a', 'parrot', '<END>',
-              'his', 'name', 'is', 'john', '<END>',
-              'i', 'have', 'a', 'chinchilla', 'too', '<END>',
-              'his', 'name', 'is', 'taylor', '<END>',
-              'her', 'name', 'is', 'taylor', 'too', '<END>')
+    corpus = ('i', 'have', 'a', 'colourful', 'dog', '<END>',
+              'i', 'have', 'colourful', 'pets', 'too', '<END>',
+              'they', 'have', 'beautiful', 'dogs', '<END>',
+              'i', 'havent', 'a', 'cat', '<END>',
+              'i', 'havent', 'a', 'cat', 'too', '<END>',
+              'we', 'havent', 'a', 'cat', 'too', '<END>')
 
     storage = WordStorage()
     storage.update(corpus)
 
     encoded = encode_text(storage, corpus)
 
+    trie = NGramTrie(4, encoded)
+
+    context = (storage.get_id('i'),
+               storage.get_id('have'),
+               storage.get_id('a'))
+
+    generator_likelihood = LikelihoodBasedTextGenerator(storage, trie)
+
+    generated_text = generator_likelihood.generate_text(context, 3)
+    decoded_gen_text = decode_text(storage, generated_text)
+    print('Likelihood generator generates sentences:')
+    print(*decoded_gen_text, sep='. ', end='.\n')
+
+    two = NGramTrie(2, encoded)
     trie = NGramTrie(3, encoded)
 
     context = (storage.get_id('i'),
                storage.get_id('have'),)
 
-    generator = LikelihoodBasedTextGenerator(storage, trie)
+    generator_backoff = BackOffGenerator(storage, trie, two)
 
-    generated_text = generator.generate_text(context, 3)
-    RESULT = decode_text(storage, generated_text)
+    actual = generator_backoff.generate_text(context, 3)
+    RESULT = decode_text(storage, actual)
+    print('Backoff generator generates sentences:')
+    print(*RESULT, sep='. ', end='.\n')
 
-    assert RESULT == ('I have a parrot',
-                      'His name is taylor',
-                      'Her name is taylor'), 'Text generator does not work'
+    assert RESULT == ('I have a colourful dog',
+                      'I havent a cat too',
+                      'They have beautiful dogs'), 'Text generator does not work'
