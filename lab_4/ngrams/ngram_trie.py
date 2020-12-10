7e97e41b1c86c5c33903ebd304dd93e112cac83a
SoullessDark <65155304+SoullessDark@users.noreply.github.com>
2020-12-10 14:05:26 +0300

    lint again

--- lab_4/ngrams/ngram_trie.py
+++ lab_4/ngrams/ngram_trie.py
@@ -18,14 +18,17 @@ class NGramTrie:
         if not isinstance(self.encoded_text, tuple):
             raise ValueError
 
-        n_grams = []
-        for i in range(len(self.encoded_text) - self.size + 1):
-            n_grams.append(tuple(self.encoded_text[i:i + self.size]))
-        self.n_grams = tuple(n_grams)
-
+        self.n_grams = self.tupling_n_grams()
         for word in self.encoded_text:
             if (word, ) not in self.uni_grams:
                 self.uni_grams[(word, )] = self.encoded_text.count(word)
+
+    def tupling_n_grams(self):
+
+        n_grams = []
+        for i in range(len(self.encoded_text) - self.size + 1):
+            n_grams.append(tuple(self.encoded_text[i:i + self.size]))
+        return tuple(n_grams)
 
     def _calculate_n_grams_frequencies(self):
 
