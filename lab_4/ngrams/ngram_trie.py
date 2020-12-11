14b00dcbfb28035f527eab2b7384dbc1f703f944
WhiteJaeger <logerk3@gmail.com>
2020-12-11 14:41:01 +0300

    restore tests

--- lab_4/ngrams/ngram_trie.py
+++ lab_4/ngrams/ngram_trie.py
@@ -1,7 +1,8 @@
-# pylint: skip-file
 """
 N-gram model
 """
+
+
 class NGramTrie:
     def __init__(self, n_gram_size: int, encoded_text: tuple):
         self.size = n_gram_size
@@ -11,22 +12,20 @@ class NGramTrie:
         self.uni_grams = {}
         self._fill_n_grams()
         self._calculate_n_grams_frequencies()
+
     def _fill_n_grams(self):
+
         if not isinstance(self.encoded_text, tuple):
             raise ValueError
-
-
-        self.n_grams = self.tupling_n_grams()
-        for word in self.encoded_text:
-            if (word, ) not in self.uni_grams:
-                self.uni_grams[(word, )] = self.encoded_text.count(word)
-
-    def tupling_n_grams(self):
 
         n_grams = []
         for i in range(len(self.encoded_text) - self.size + 1):
             n_grams.append(tuple(self.encoded_text[i:i + self.size]))
-        return tuple(n_grams)
+        self.n_grams = tuple(n_grams)
+
+        for word in self.encoded_text:
+            if (word, ) not in self.uni_grams:
+                self.uni_grams[(word, )] = self.encoded_text.count(word)
 
     def _calculate_n_grams_frequencies(self):
 
