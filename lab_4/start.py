76e056b6ddc6f3ee3f3f7845a0365d4ce910568e
mcgrozov <65295559+mcgrozov@users.noreply.github.com>
2020-12-12 00:36:27 +0300

    Allah blesses this code

--- lab_4/start.py
+++ lab_4/start.py
@@ -1,31 +1,25 @@
 """
  Text generator
  """
+import main
+import ngrams
 
-from lab_4.ngrams.ngram_trie import NGramTrie
-from lab_4.main import WordStorage
-from lab_4.main import encode_text
-from lab_4.main import NGramTextGenerator
 
 if __name__ == '__main__':
-    corpus = ('i', 'have', 'a', 'cat', '<END>',
-                'his', 'name', 'is', 'bruno', '<END>',
-                'i', 'have', 'a', 'dog', 'too', '<END>',
-                'his', 'name', 'is', 'rex', '<END>',
-                'her', 'name', 'is', 'rex', 'too', '<END>')
-
-    word_storage = WordStorage()
+    text = 'I want to pass exams. I do not want study morphology'
+    corpus = main.tokenize_by_sentence(text)
+    word_storage = main.WordStorage()
     word_storage.update(corpus)
 
-    encoded_text = encode_text(word_storage, corpus)
+    encoded_text = main.encode_text(word_storage, corpus)
 
-    n_gram_trie = NGramTrie(2, encoded_text)
+    n_gram_trie = ngrams.NGramTrie(2, encoded_text)
 
-    n_gram_text_generator = NGramTextGenerator(word_storage, n_gram_trie)
+    n_gram_text_generator = main.NGramTextGenerator(word_storage, n_gram_trie)
 
-    context = (word_storage.get_id('Butter'), word_storage.get_id('unterheben'))
+    context = (word_storage.get_id('want'), word_storage.get_id('study'))
 
     text_generated = n_gram_text_generator.generate_text(context, 2)
-    output_text = []
-    
-    assert RESULT == ''
+    RESULT = text_generated
+
+    assert RESULT, ''
