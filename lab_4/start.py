42b8a23347fd409ceac547355dbcc9c988159096
OlesyaGanina <65288843+OlesyaGanina@users.noreply.github.com>
2020-12-11 08:09:05 +0300

    Update start.py

--- lab_4/start.py
+++ lab_4/start.py
@@ -3,7 +3,7 @@ from lab_4.main import WordStorage
 from lab_4.main import encode_text
 
 if __name__ == '__main__':
-    text = 'His name is leon. He is happy.'
+    text = 'He is leon. He is happy.'
     corpus = tokenize_by_sentence(text)
 
     word_storage = WordStorage()
