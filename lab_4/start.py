37444b66ada4a6ecbbc7d2e1f1d26383a1f1288f
mcgrozov <65295559+mcgrozov@users.noreply.github.com>
2020-12-12 00:56:31 +0300

    No church in the wild

--- lab_4/start.py
+++ lab_4/start.py
@@ -7,18 +7,11 @@ from ngrams.ngram_trie import NGramTrie
 if __name__ == '__main__':
     text = 'I want to pass exams. I do not want study morphology'
     corpus = main.tokenize_by_sentence(text)
+
     word_storage = main.WordStorage()
     word_storage.update(corpus)
 
     encoded_text = main.encode_text(word_storage, corpus)
-
-    n_gram_trie = NGramTrie(2, encoded_text)
-
-    n_gram_text_generator = main.NGramTextGenerator(word_storage, n_gram_trie)
-
-    context = (word_storage.get_id('want'), word_storage.get_id('study'))
-
-    text_generated = n_gram_text_generator.generate_text(context, 2)
-    RESULT = text_generated
+    RESULT = encoded_text
 
     assert RESULT,''
