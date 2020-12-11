39af05775a8aa7f451f5f2b5899f5d0cb250d4dc
Ekaterina <ekrivenkova2001@mail.ru>
2020-12-11 13:31:53 +0300

    new

--- lab_4/start.py
+++ lab_4/start.py
@@ -1,9 +1,9 @@
-from lab_4.main import tokenize_by_sentence, WordStorage, encode_text, NGramTextGenerator, NGramTrie
+from lab_4.main import tokenize_by_sentence, WordStorage, encode_text, NGramTextGenerator
+from lab_4.ngrams.ngram_trie import NGramTrie
 
 if __name__ == '__main__':
-    text = 'I have a friend. \n Her name is Beth.'
-
-    corpus = tokenize_by_sentence(text)
+    TEXT = 'I have a friend. \n Her name is Beth.'
+    corpus = tokenize_by_sentence(TEXT)
 
     word_storage = WordStorage()
     word_storage.update(corpus)
@@ -12,12 +12,12 @@ if __name__ == '__main__':
 
     ngrams = NGramTrie(2, encoded_corpus)
 
-    generator = NGramTextGenerator(word_storage, ngrams)
+    text_generator = NGramTextGenerator(word_storage, ngrams)
 
     context = (word_storage.get_id('name'),
                word_storage.get_id('is'))
 
-    text = generator.generate_text(context, 2)
+    gen_text = text_generator.generate_text(context, 2)
 
-    RESULT = text
-    assert RESULT, 'Not working'
+    RESULT = gen_text
+    assert RESULT == (5,5), 'Not working'
