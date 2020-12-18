7e2d8af0903f057042c5995e1bdb06f12c9a45aa
AnastasiaZheleznyakova <66490710+AnastasiaZheleznyakova@users.noreply.github.com>
2020-12-18 13:50:22 +0300

    Update letter_storage_score_four_test.py

--- lab_3/letter_storage_score_four_test.py
+++ lab_3/letter_storage_score_four_test.py
@@ -13,7 +13,7 @@ class LetterStorageTest(unittest.TestCase):
     check LetterStorage class functionality.
         All tests should pass for score 4 or above
     """
-    @unittest.skip('')
+    
     def test_letter_storage_correct_instance_creation(self):
         """
         letter storage instance creates with correct attributes
@@ -23,7 +23,7 @@ class LetterStorageTest(unittest.TestCase):
         self.assertEqual(letter_storage.storage, expected)
 
 # --------------------------------------------------------
-    @unittest.skip('')
+   
     def test_letter_storage_put_letter_ideal(self):
         """
         letter is added to storage
@@ -34,7 +34,7 @@ class LetterStorageTest(unittest.TestCase):
         actual = letter_storage._put_letter(letter)
         self.assertTrue(letter in letter_storage.storage)
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+   
     def test_letter_storage_put_letter_none(self):
         """
         none is not added to storage
@@ -45,7 +45,7 @@ class LetterStorageTest(unittest.TestCase):
         actual = letter_storage._put_letter(letter)
         self.assertEqual(letter_storage.storage, {})
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+
     def test_letter_storage_put_letter_not_str(self):
         """
         non string letter is not added to storage
@@ -56,7 +56,7 @@ class LetterStorageTest(unittest.TestCase):
         actual = letter_storage._put_letter(letter)
         self.assertEqual(letter_storage.storage, {})
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+   
     def test_letter_storage_put_letter_existing(self):
         """
         existing letter is not added to storage
@@ -70,7 +70,7 @@ class LetterStorageTest(unittest.TestCase):
         self.assertEqual(expected, actual)
 
 # -----------------------------------------------------------------
-    @unittest.skip('')
+  
     def test_letter_storage_get_id_by_letter_ideal(self):
         """
         ideal case for get_id_by_letter
@@ -80,7 +80,7 @@ class LetterStorageTest(unittest.TestCase):
         expected = 1
         actual = letter_storage.get_id_by_letter('w')
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+
     def test_letter_storage_get_id_by_letter_none(self):
         """
         get_id_by_letter none
@@ -90,7 +90,7 @@ class LetterStorageTest(unittest.TestCase):
         expected = -1
         actual = letter_storage.get_id_by_letter(None)
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+
     def test_letter_storage_get_id_by_letter_not_str(self):
         """
         id is not str  get_id_by_letter
@@ -100,7 +100,7 @@ class LetterStorageTest(unittest.TestCase):
         expected = -1
         actual = letter_storage.get_id_by_letter(123)
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+    
     def test_letter_storage_get_id_by_letter_not_in_storage(self):
         """
         letter not in storage
@@ -112,7 +112,7 @@ class LetterStorageTest(unittest.TestCase):
         self.assertEqual(expected, actual)
 
 # -----------------------------------------------------------
-    @unittest.skip('')
+    
     def test_letter_storage_update_ideal(self):
         """
         ideal case for update
@@ -125,7 +125,7 @@ class LetterStorageTest(unittest.TestCase):
         actual = letter_storage.update(sentences)
         self.assertEqual(len(letter_storage.storage), 4)
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+   
     def test_letter_storage_update_duplicates(self):
         """
         ideal case for update
@@ -139,7 +139,7 @@ class LetterStorageTest(unittest.TestCase):
         actual = letter_storage.update(sentences)
         self.assertEqual(len(letter_storage.storage), 4)
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+   
     def test_letter_storage_update_empty(self):
         """
         ideal case for update
@@ -150,7 +150,7 @@ class LetterStorageTest(unittest.TestCase):
         actual = letter_storage.update(sentences)
         self.assertEqual(letter_storage.storage, {})
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+   
     def test_letter_storage_update_none(self):
         """
         ideal case for update
@@ -161,7 +161,7 @@ class LetterStorageTest(unittest.TestCase):
         actual = letter_storage.update(sentences)
         self.assertEqual(letter_storage.storage, {})
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+ 
     def test_letter_storage_update_not_tuple(self):
         """
         ideal case for update
@@ -175,9 +175,9 @@ class LetterStorageTest(unittest.TestCase):
         actual = letter_storage.update(sentences)
         self.assertEqual(letter_storage.storage, {})
         self.assertEqual(expected, actual)
-    @unittest.skip('')
+
     @patch('lab_3.main.LetterStorage._put_letter', side_effect=LetterStorage()._put_letter)
-    @unittest.skip('')
+  
     def test_letter_storage_update_calls_required_function(self, mock):
         """
         ideal case for update calling put_letter method
