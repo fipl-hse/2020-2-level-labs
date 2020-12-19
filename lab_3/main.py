ff0afa7da977e20de426d7e78d4ee46576c14519
AnastasiaZheleznyakova <66490710+AnastasiaZheleznyakova@users.noreply.github.com>
2020-12-19 21:05:09 +0300

    Update main.py

--- lab_3/main.py
+++ lab_3/main.py
@@ -19,7 +19,7 @@ def tokenize_by_sentence(text: str) -> tuple:
          (('_', 'h', 'e', '_'), ('_', 'i', 's', '_'), ('_', 'h', 'a', 'p', 'p', 'y', '_'))
          )
     """
-    words = [word.lower() for word in re.findall(r"[a-zA-Z]+"), text]
+    words = [word.lower() for word in re.findall(r"[a-zA-Z]+", text)]
     return tuple(('_', ) + tuple(letter for letter in word) + ('_', ))
 
 
