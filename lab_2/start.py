74efe626f7f8488f9deaeed9efab4d9e452867b9
aphexqueen <kristinasheshmeneva@yandex.ru>
2020-12-20 20:05:34 +0300

    fixed lab_2 start.py bc lint asked me

--- lab_2/start.py
+++ lab_2/start.py
@@ -8,14 +8,14 @@ import main
 if __name__ == '__main__':
     with open('lab_2/diff_report_example.txt', 'r', encoding='utf-8') as file:
         report_example = file.read()
-    original_text = 'I have a cat. \nIts body is covered with bushy white fur.'
-    suspicious_text = 'I have a cat. \nIts body is covered with shiny black fur.'
-    print('\tOriginal text:\n' + original_text)
-    print('\n\tSuspicious text:\n' + suspicious_text)
+    ORIGINAL_TEXT = 'I have a cat. \nIts body is covered with bushy white fur.'
+    SUSPICIOUS_TEXT = 'I have a cat. \nIts body is covered with shiny black fur.'
+    print('\tOriginal text:\n' + ORIGINAL_TEXT)
+    print('\n\tSuspicious text:\n' + SUSPICIOUS_TEXT)
     print('\n\t...tokenizing original text...')
-    original_text_tokens = main.tokenize_by_lines(original_text)
+    original_text_tokens = main.tokenize_by_lines(ORIGINAL_TEXT)
     print('\t...tokenizing suspicious text...')
-    suspicious_text_tokens = main.tokenize_by_lines(suspicious_text)
+    suspicious_text_tokens = main.tokenize_by_lines(SUSPICIOUS_TEXT)
     print('\t...creating a line-by-line diff report...\n\n')
     report = (main.create_diff_report(original_text_tokens, suspicious_text_tokens,
                                       main.accumulate_diff_stats(original_text_tokens, suspicious_text_tokens)))
